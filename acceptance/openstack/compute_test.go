f370dc7c6c1058ce97357b7f650123aa1ef53aac
Samuel A. Falvo II <kc5tja@arrl.net>
2014-02-13 15:05:34 -0800

    Convert compute acceptance tests to go test form

--- acceptance/openstack/compute_test.go
+++ acceptance/openstack/compute_test.go
@@ -1,33 +1,39 @@
-package main
+package openstack
 
 import (
+	"testing"
 	"fmt"
 	"github.com/rackspace/gophercloud/openstack/compute/servers"
+	"github.com/rackspace/gophercloud/openstack/compute/images"
 	"github.com/rackspace/gophercloud/openstack/identity"
 	"github.com/rackspace/gophercloud/openstack/utils"
 	"os"
 	"text/tabwriter"
 )
 
-func main() {
+func TestListServers(t *testing.T) {
 	ao, err := utils.AuthOptions()
 	if err != nil {
-		panic(err)
+		t.Error(err)
+		return
 	}
 
 	a, err := identity.Authenticate(ao)
 	if err != nil {
-		panic(err)
+		t.Error(err)
+		return
 	}
 
 	sc, err := identity.GetServiceCatalog(a)
 	if err != nil {
-		panic(err)
+		t.Error(err)
+		return
 	}
 
 	eps, err := findAllComputeEndpoints(sc)
 	if err != nil {
-		panic(err)
+		t.Error(err)
+		return
 	}
 
 	w := new(tabwriter.Writer)
@@ -45,12 +51,14 @@ func main() {
 
 		listResults, err := servers.List(client)
 		if err != nil {
-			panic(err)
+			t.Error(err)
+			return
 		}
 
 		svrs, err := servers.GetServers(listResults)
 		if err != nil {
-			panic(err)
+			t.Error(err)
+			return
 		}
 
 		n = n + len(svrs)
@@ -77,3 +85,64 @@ func findAllComputeEndpoints(sc *identity.ServiceCatalog) ([]identity.Endpoint, 
 
 	return nil, fmt.Errorf("Compute endpoint not found.")
 }
+
+func TestListImages(t *testing.T) {
+	ao, err := utils.AuthOptions()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	a, err := identity.Authenticate(ao)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	sc, err := identity.GetServiceCatalog(a)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	eps, err := findAllComputeEndpoints(sc)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 2, 8, 2, ' ', 0)
+	fmt.Fprintln(w, "ID\tRegion\tName\tStatus\tCreated\t")
+
+	region := os.Getenv("OS_REGION_NAME")
+	n := 0
+	for _, ep := range eps {
+		if (region != "") && (region != ep.Region) {
+			continue
+		}
+
+		client := images.NewClient(ep.PublicURL, a, ao)
+
+		listResults, err := images.List(client)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		imgs, err := images.GetImages(listResults)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		n = n + len(imgs)
+
+		for _, i := range imgs {
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t\n", i.Id, ep.Region, i.Name, i.Status, i.Created)
+		}
+	}
+	w.Flush()
+	fmt.Printf("--------\n%d images listed.\n", n)
+}
+
