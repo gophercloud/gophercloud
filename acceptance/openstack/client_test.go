33c85f4e5e10d0e97cc6d39a1a5c3a92f415ea7c
Ash Wilson <smashwilson@gmail.com>
2014-09-02 14:09:14 -0400

    Acceptance test for creating authenticated clients.

---
+++ acceptance/openstack/client_test.go
@@ -0,0 +1,27 @@
+package openstack
+
+import (
+	"testing"
+
+	"github.com/rackspace/gophercloud/openstack"
+	"github.com/rackspace/gophercloud/openstack/utils"
+)
+
+func TestAuthenticatedClient(t *testing.T) {
+	// Obtain credentials from the environment.
+	ao, err := utils.AuthOptions()
+	if err != nil {
+		t.Fatalf("Unable to acquire credentials: %v", err)
+	}
+
+	client, err := openstack.AuthenticatedClient(ao)
+	if err != nil {
+		t.Fatalf("Unable to authenticate: %v", err)
+	}
+
+	if client.TokenID == "" {
+		t.Errorf("No token ID assigned to the client")
+	}
+
+	t.Logf("Client successfully acquired a token: %v", client.TokenID)
+}
