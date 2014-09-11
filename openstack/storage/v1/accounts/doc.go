3845eeeba1a3f9079555338d51f23799eed0f225
Jon Perritt <jrperritt@gmail.com>
2014-09-11 10:42:47 -0500

    add doc.go files to subpackages

---
+++ openstack/storage/v1/accounts/doc.go
@@ -0,0 +1,5 @@
+/* The accounts package defines operations performed on an object-storage account.
+
+Reference: http://developer.openstack.org/api-ref-objectstorage-v1.html#storage_account_services
+*/
+package accounts
