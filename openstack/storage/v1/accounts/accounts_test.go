519729f8874ef5373d375076f1a32d95042cfb5d
Jon Perritt <jrperritt@gmail.com>
2014-09-14 13:37:25 -0500

    use reflect.DeepEqual

--- openstack/storage/v1/accounts/accounts_test.go
+++ openstack/storage/v1/accounts/accounts_test.go
@@ -2,19 +2,18 @@ package accounts
 
 import (
 	"net/http"
+	"reflect"
 	"testing"
 )
 
-func TestExtractMetadata(t *testing.T) {
+func TestExtractAccountMetadata(t *testing.T) {
 	getResult := &http.Response{}
 
 	expected := map[string]string{}
 
 	actual := ExtractMetadata(getResult)
 
-	for key, value := range expected {
-		if value != actual[key] {
-			t.Errorf("Expected: %+v\nActual:%+v", expected, actual)
-		}
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected: %+v\nActual:%+v", expected, actual)
 	}
 }
