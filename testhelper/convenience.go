2964aed5ba7f0129e916362dbe092c8a97b4cd93
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-09-15 12:20:02 +0200

    Add comments

--- testhelper/convenience.go
+++ testhelper/convenience.go
@@ -2,12 +2,15 @@ package testhelper
 
 import "testing"
 
+// This function compares two arbitrary values and performs a comparison. If the
+// comparison fails, a fatal error is raised that will fail the test
 func Compare(t *testing.T, expected interface{}, actual interface{}) {
 	if expected != actual {
 		t.Fatalf("Expected [%#v] but got [%#v]", expected, actual)
 	}
 }
 
+// A convenience function for checking whether an error value is an actual error
 func CheckErr(t *testing.T, e error) {
 	if e != nil {
 		t.Fatalf("Unexpected error: %#v", e)
