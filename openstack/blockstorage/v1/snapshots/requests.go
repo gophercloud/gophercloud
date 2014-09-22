dfff9977b7bf3d693c179a47055d140a8413dabe
Jon Perritt <jrperritt@gmail.com>
2014-09-22 01:14:54 -0500

    rename snapshots.go; start rewrite of create snapshot

---
+++ openstack/blockstorage/v1/snapshots/requests.go
@@ -0,0 +1,56 @@
+package snapshots
+
+import (
+	"github.com/racker/perigee"
+	"github.com/rackspace/gophercloud"
+	"github.com/rackspace/gophercloud/openstack/utils"
+)
+
+type CreateOpts struct {
+	Description string
+	Force       bool
+	Metadata    map[string]interface{}
+	Name        string
+	VolumeID    string
+}
+
+func Create(client *gophercloud.ServiceClient, opts CreateOpts) (*Snapshot, error) {
+	type snapshot struct {
+		Description *string                `json:"display_description,omitempty"`
+		Force       *bool                  `json:"force,omitempty"`
+		Metadata    map[string]interface{} `json:"metadata,omitempty"`
+		Name        *string                `json:"display_name,omitempty"`
+		VolumeID    *string                `json:"volume_id,omitempty"`
+	}
+
+	type request struct {
+		Snapshot snapshot `json:"snapshot"`
+	}
+
+	reqBody := request{
+		Snapshot: snapshot{},
+	}
+
+	reqBody.Snapshot.Description = utils.MaybeString(opts.Description)
+	reqBody.Snapshot.Force = utils.MaybeString(opts.Force)
+	reqBody.Snapshot.Name = utils.MaybeString(opts.Name)
+	reqBody.Snapshot.VolumeID = utils.MaybeString(opts.VolumeID)
+
+	type response struct {
+		Snapshot Snapshot `json:"snapshot"`
+	}
+
+	var respBody response
+
+	_, err := perigee.Request("POST", snapshotsURL(client), perigee.Options{
+		MoreHeaders: client.Provider.AuthenticatedHeaders(),
+		OkCodes:     []int{201},
+		ReqBody:     &reqBody,
+		Results:     &respBody,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &respBody.Snapshot, nil
+}
---
+++ openstack/blockstorage/v1/snapshots/results.go
@@ -0,0 +1,12 @@
+package snapshots
+
+type Snapshot struct {
+	CreatedAt   string
+	Description string
+	ID          string
+	Metadata    map[string]interface{}
+	Name        string
+	Size        int
+	Status      string
+	VolumeID    string
+}
