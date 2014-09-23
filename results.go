4baa1236420df05bb4c232e47dd71b83b716582d
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-09-23 15:23:04 +0200

    Shifting to root pkg

--- results.go
+++ results.go
@@ -1,7 +1,7 @@
-package openstack
+package gophercloud
 
 // CommonResult acts as a base struct that other results can embed. It contains
-// the deserialized JSON structured returned from the server (Resp), and any
+// the deserialized JSON structure returned from the server (Resp), and any
 // errors that might have occurred during transport or deserialization.
 type CommonResult struct {
 	Resp map[string]interface{}
