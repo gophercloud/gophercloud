fb9716834702fdfc62ed220a4d1b1c3258ad4ba8
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-09-25 10:48:27 +0200

    Removing quotas since the docs are patchy

--- openstack/networking/v2/extensions/quotas/urls.go
+++
@@ -1,9 +0,0 @@
-package quotas
-
-import "github.com/rackspace/gophercloud"
-
-const version = "v2.0"
-
-func rootURL(c *gophercloud.ServiceClient) string {
-	return c.ServiceURL(version, "quotas")
-}
