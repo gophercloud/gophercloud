9b75ba22255f59dd950e2c4bb324874a4083f44a
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-09-26 11:18:22 +0200

    Moving URLs into their own file for consistency

--- openstack/networking/v2/extensions/layer3/routers/requests.go
+++ openstack/networking/v2/extensions/layer3/routers/requests.go
@@ -10,27 +10,6 @@ import (
 	"github.com/rackspace/gophercloud/pagination"
 )
 
-const (
-	version      = "v2.0"
-	resourcePath = "routers"
-)
-
-func rootURL(c *gophercloud.ServiceClient) string {
-	return c.ServiceURL(version, resourcePath)
-}
-
-func resourceURL(c *gophercloud.ServiceClient, id string) string {
-	return c.ServiceURL(version, resourcePath, id)
-}
-
-func addInterfaceURL(c *gophercloud.ServiceClient, id string) string {
-	return c.ServiceURL(version, resourcePath, id, "add_router_interface")
-}
-
-func removeInterfaceURL(c *gophercloud.ServiceClient, id string) string {
-	return c.ServiceURL(version, resourcePath, id, "remove_router_interface")
-}
-
 type ListOpts struct {
 	ID           string
 	Name         string
