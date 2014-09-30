82522ca128b00a4de8d027fd04f664a3a1bed794
Jon Perritt <jrperritt@gmail.com>
2014-09-29 22:31:31 -0500

    urls unit tests

--- openstack/blockStorage/v1/snapshots/urls_test.go
+++ openstack/blockStorage/v1/snapshots/urls_test.go
@@ -1,1 +1,26 @@
 package snapshots
+
+import (
+	"testing"
+
+	"github.com/rackspace/gophercloud"
+	th "github.com/rackspace/gophercloud/testhelper"
+)
+
+const endpoint = "http://localhost:57909"
+
+func endpointClient() *gophercloud.ServiceClient {
+	return &gophercloud.ServiceClient{Endpoint: endpoint}
+}
+
+func TestSnapshotsURL(t *testing.T) {
+	actual := snapshotsURL(endpointClient())
+	expected := endpoint + "snapshots"
+	th.AssertEquals(t, expected, actual)
+}
+
+func TestSnapshotURL(t *testing.T) {
+	actual := snapshotURL(endpointClient(), "foo")
+	expected := endpoint + "snapshots/foo"
+	th.AssertEquals(t, expected, actual)
+}
--- openstack/blockStorage/v1/volumes/urls_test.go
+++ openstack/blockStorage/v1/volumes/urls_test.go
@@ -1,1 +1,26 @@
 package volumes
+
+import (
+	"testing"
+
+	"github.com/rackspace/gophercloud"
+	th "github.com/rackspace/gophercloud/testhelper"
+)
+
+const endpoint = "http://localhost:57909"
+
+func endpointClient() *gophercloud.ServiceClient {
+	return &gophercloud.ServiceClient{Endpoint: endpoint}
+}
+
+func TestVolumesURL(t *testing.T) {
+	actual := volumesURL(endpointClient())
+	expected := endpoint + "volumes"
+	th.AssertEquals(t, expected, actual)
+}
+
+func TestVolumeURL(t *testing.T) {
+	actual := volumeURL(endpointClient(), "foo")
+	expected := endpoint + "volumes/foo"
+	th.AssertEquals(t, expected, actual)
+}
