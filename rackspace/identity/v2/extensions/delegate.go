56d97a642b6a67930451f6d8a3abe35488988a91
Ash Wilson <smashwilson@gmail.com>
2014-10-13 12:07:47 -0400

    Use delegated fixtures in Rackspace identity.

--- rackspace/identity/v2/extensions/delegate.go
+++ rackspace/identity/v2/extensions/delegate.go
@@ -3,49 +3,22 @@ package extensions
 import (
 	"github.com/rackspace/gophercloud"
 	common "github.com/rackspace/gophercloud/openstack/common/extensions"
-	os "github.com/rackspace/gophercloud/openstack/identity/v2/extensions"
 	"github.com/rackspace/gophercloud/pagination"
 )
 
-// ExtensionPage is the page returned by a pager when traversing over a collection of extensions.
-type ExtensionPage struct {
-	common.ExtensionPage
-}
-
-// IsEmpty checks whether an ExtensionPage struct is empty.
-func (r ExtensionPage) IsEmpty() (bool, error) {
-	is, err := ExtractExtensions(r)
-	if err != nil {
-		return true, err
-	}
-	return len(is) == 0, nil
-}
-
 // ExtractExtensions accepts a Page struct, specifically an ExtensionPage struct, and extracts the
 // elements into a slice of os.Extension structs.
-func ExtractExtensions(page pagination.Page) ([]os.Extension, error) {
-	exts, err := common.ExtractExtensions(page.(ExtensionPage).ExtensionPage)
-	if err != nil {
-		return nil, err
-	}
-	results := make([]os.Extension, len(exts))
-	for i, ext := range exts {
-		results[i] = os.Extension{Extension: ext}
-	}
-	return results, nil
+func ExtractExtensions(page pagination.Page) ([]common.Extension, error) {
+	return common.ExtractExtensions(page)
 }
 
 // Get retrieves information for a specific extension using its alias.
-func Get(c *gophercloud.ServiceClient, alias string) os.GetResult {
-	return os.Get(c, alias)
+func Get(c *gophercloud.ServiceClient, alias string) common.GetResult {
+	return common.Get(c, alias)
 }
 
 // List returns a Pager which allows you to iterate over the full collection of extensions.
 // It does not accept query parameters.
 func List(c *gophercloud.ServiceClient) pagination.Pager {
-	return os.List(c).WithPageCreator(func(r pagination.LastHTTPResponse) pagination.Page {
-		return ExtensionPage{
-			ExtensionPage: common.ExtensionPage{SinglePageBase: pagination.SinglePageBase(r)},
-		}
-	})
+	return common.List(c)
 }
--- rackspace/identity/v2/extensions/delegate_test.go
+++ rackspace/identity/v2/extensions/delegate_test.go
@@ -1,12 +1,9 @@
 package extensions
 
 import (
-	"fmt"
-	"net/http"
 	"testing"
 
 	common "github.com/rackspace/gophercloud/openstack/common/extensions"
-	os "github.com/rackspace/gophercloud/openstack/identity/v2/extensions"
 	"github.com/rackspace/gophercloud/pagination"
 	th "github.com/rackspace/gophercloud/testhelper"
 	fake "github.com/rackspace/gophercloud/testhelper/client"
@@ -15,28 +12,7 @@ import (
 func TestList(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
-
-	th.Mux.HandleFunc("/extensions", func(w http.ResponseWriter, r *http.Request) {
-		th.TestMethod(t, r, "GET")
-		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
-
-		w.Header().Add("Content-Type", "application/json")
-
-		fmt.Fprintf(w, `
-{
-  "extensions": [
-    {
-      "updated": "2013-01-20T00:00:00-00:00",
-      "name": "Neutron Service Type Management",
-      "links": [],
-      "namespace": "http://docs.openstack.org/ext/neutron/service-type/api/v1.0",
-      "alias": "service-type",
-      "description": "API for retrieving service providers for Neutron advanced services"
-    }
-  ]
-}
-    `)
-	})
+	common.HandleListExtensionsSuccessfully(t)
 
 	count := 0
 
@@ -44,21 +20,7 @@ func TestList(t *testing.T) {
 		count++
 		actual, err := ExtractExtensions(page)
 		th.AssertNoErr(t, err)
-
-		expected := []os.Extension{
-			os.Extension{
-				Extension: common.Extension{
-					Updated:     "2013-01-20T00:00:00-00:00",
-					Name:        "Neutron Service Type Management",
-					Links:       []interface{}{},
-					Namespace:   "http://docs.openstack.org/ext/neutron/service-type/api/v1.0",
-					Alias:       "service-type",
-					Description: "API for retrieving service providers for Neutron advanced services",
-				},
-			},
-		}
-
-		th.AssertDeepEquals(t, expected, actual)
+		th.AssertDeepEquals(t, common.ExpectedExtensions, actual)
 
 		return true, nil
 	})
@@ -69,34 +31,9 @@ func TestList(t *testing.T) {
 func TestGet(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
+	common.HandleGetExtensionSuccessfully(t)
 
-	th.Mux.HandleFunc("/extensions/agent", func(w http.ResponseWriter, r *http.Request) {
-		th.TestMethod(t, r, "GET")
-		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
-
-		w.Header().Add("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-
-		fmt.Fprintf(w, `
-{
-    "extension": {
-        "updated": "2013-02-03T10:00:00-00:00",
-        "name": "agent",
-        "links": [],
-        "namespace": "http://docs.openstack.org/ext/agent/api/v2.0",
-        "alias": "agent",
-        "description": "The agent management extension."
-    }
-}
-    `)
-
-		ext, err := Get(fake.ServiceClient(), "agent").Extract()
-		th.AssertNoErr(t, err)
-
-		th.AssertEquals(t, ext.Updated, "2013-02-03T10:00:00-00:00")
-		th.AssertEquals(t, ext.Name, "agent")
-		th.AssertEquals(t, ext.Namespace, "http://docs.openstack.org/ext/agent/api/v2.0")
-		th.AssertEquals(t, ext.Alias, "agent")
-		th.AssertEquals(t, ext.Description, "The agent management extension.")
-	})
+	actual, err := Get(fake.ServiceClient(), "agent").Extract()
+	th.AssertNoErr(t, err)
+	th.CheckDeepEquals(t, common.SingleExtension, actual)
 }
