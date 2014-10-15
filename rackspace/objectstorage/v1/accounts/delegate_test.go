0e28b1178d2ecd9229276c43ee4b9a593d211789
Jon Perritt <jrperritt@gmail.com>
2014-10-14 20:49:31 -0500

    rax cloud files delegation requests

---
+++ rackspace/objectstorage/v1/accounts/delegate_test.go
@@ -0,0 +1,15 @@
+package accounts
+
+import (
+	"testing"
+	//th "github.com/rackspace/gophercloud/testhelper"
+	//fake "github.com/rackspace/gophercloud/testhelper/client"
+)
+
+func TestGetAccounts(t *testing.T) {
+
+}
+
+func TestUpdateAccounts(t *testing.T) {
+
+}
