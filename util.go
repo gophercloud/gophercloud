3ac3aa735cf2cfdc88848a1ed8134f89f72061fd
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-10-23 17:30:49 +0200

    Rejig ordering of timeout

--- util.go
+++ util.go
@@ -14,6 +14,11 @@ func WaitFor(timeout int, predicate func() (bool, error)) error {
 		// Force a 1s sleep
 		time.Sleep(1 * time.Second)
 
+		// If a timeout is set, and that's been exceeded, shut it down
+		if timeout >= 0 && time.Now().Second()-start >= timeout {
+			return errors.New("A timeout occurred")
+		}
+
 		// Execute the function
 		satisfied, err := predicate()
 		if err != nil {
@@ -21,11 +26,6 @@ func WaitFor(timeout int, predicate func() (bool, error)) error {
 		}
 		if satisfied {
 			return nil
-		}
-
-		// If a timeout is set, and that's been exceeded, shut it down
-		if timeout > 0 && time.Now().Second()-start >= timeout {
-			return errors.New("A timeout occurred")
 		}
 	}
 }
