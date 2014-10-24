bdc8697f837cd32b350dca50f8c8173abfe29c86
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-10-24 17:12:34 +0200

    Adding doc.go files for packages

--- openstack/identity/v3/tokens/doc.go
+++ openstack/identity/v3/tokens/doc.go
@@ -1,6 +1,6 @@
-/*
-Package tokens defines operations performed on the token resource.
-
-Documentation: http://developer.openstack.org/api-ref-identity-v3.html#tokens-v3
-*/
+// Package tokens provides information and interaction with the token API
+// resource for the OpenStack Identity service.
+//
+// For more information, see:
+// http://developer.openstack.org/api-ref-identity-v3.html#tokens-v3
 package tokens
