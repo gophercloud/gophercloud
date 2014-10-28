fe1b4975388436b2f686adfe61544a7f82a1d309
jrperritt <jrperritt@gmail.com>
2014-10-28 10:27:58 -0500

    use th.AssertNoErr

--- acceptance/openstack/blockstorage/v1/volumetypes_test.go
+++ acceptance/openstack/blockstorage/v1/volumetypes_test.go
@@ -8,13 +8,12 @@ import (
 
 	"github.com/rackspace/gophercloud/openstack/blockstorage/v1/volumetypes"
 	"github.com/rackspace/gophercloud/pagination"
+	th "github.com/rackspace/gophercloud/testhelper"
 )
 
 func TestVolumeTypes(t *testing.T) {
 	client, err := newClient()
-	if err != nil {
-		t.Fatalf("Failed to create Block Storage v1 client: %v", err)
-	}
+	th.AssertNoErr(t, err)
 
 	vt, err := volumetypes.Create(client, &volumetypes.CreateOpts{
 		ExtraSpecs: map[string]interface{}{
@@ -23,10 +22,7 @@ func TestVolumeTypes(t *testing.T) {
 		},
 		Name: "gophercloud-test-volumeType",
 	}).Extract()
-	if err != nil {
-		t.Error(err)
-		return
-	}
+	th.AssertNoErr(t, err)
 	defer func() {
 		time.Sleep(10000 * time.Millisecond)
 		err = volumetypes.Delete(client, vt.ID).ExtractErr()
@@ -38,10 +34,7 @@ func TestVolumeTypes(t *testing.T) {
 	t.Logf("Created volume type: %+v\n", vt)
 
 	vt, err = volumetypes.Get(client, vt.ID).Extract()
-	if err != nil {
-		t.Error(err)
-		return
-	}
+	th.AssertNoErr(t, err)
 	t.Logf("Got volume type: %+v\n", vt)
 
 	err = volumetypes.List(client).EachPage(func(page pagination.Page) (bool, error) {
@@ -52,7 +45,5 @@ func TestVolumeTypes(t *testing.T) {
 		t.Logf("Listing volume types: %+v\n", volTypes)
 		return true, err
 	})
-	if err != nil {
-		t.Errorf("Error trying to list volume types: %v", err)
-	}
+	th.AssertNoErr(t, err)
 }
