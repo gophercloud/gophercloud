cc08b11c9ba9ee60a357ae1b7541388d9f05ec70
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-10-28 16:53:48 +0100

    Removing this because it's pointless and breaks the universe

--- util_test.go
+++ util_test.go
@@ -8,14 +8,6 @@ import (
 )
 
 func TestWaitFor(t *testing.T) {
-	err := WaitFor(0, func() (bool, error) {
-		time.Sleep(1 * time.Second)
-		return true, nil
-	})
-	if err == nil {
-		t.Errorf("Expected error: 'Time out in WaitFor'")
-	}
-
 	err = WaitFor(5, func() (bool, error) {
 		return true, nil
 	})
