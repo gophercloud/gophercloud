b319d47d1a41cded74a7d08ee81c57307bf7c7f7
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-10-29 10:57:22 +0100

    Adding URLs for users

--- openstack/identity/v2/users/urls.go
+++ openstack/identity/v2/users/urls.go
@@ -1,1 +1,13 @@
 package users
+
+import "github.com/rackspace/gophercloud"
+
+const path = "users"
+
+func resourceURL(c *gophercloud.ServiceClient, id string) string {
+	return c.ServiceURL(path, id)
+}
+
+func rootURL(c *gophercloud.ServiceClient) string {
+	return c.ServiceURL(path)
+}
