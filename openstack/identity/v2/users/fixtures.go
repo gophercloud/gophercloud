6a076d89b2a077208362ef401ead657eaf76406a
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-10-29 10:57:36 +0100

    Adding fixtures file

--- openstack/identity/v2/users/fixtures.go
+++ openstack/identity/v2/users/fixtures.go
@@ -1,1 +1,39 @@
 package users
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	th "github.com/rackspace/gophercloud/testhelper"
+	fake "github.com/rackspace/gophercloud/testhelper/client"
+)
+
+func MockListResponse(t *testing.T) {
+	th.Mux.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, `
+{
+    "users":[
+        {
+            "id": "u1000",
+            "username": "jqsmith",
+            "email": "john.smith@example.org",
+            "enabled": true
+        },
+        {
+            "id": "u1001",
+            "username": "jqsmith",
+            "email": "john.smith@example.org",
+            "enabled": true
+        }
+    ]
+}
+  `)
+	})
+}
