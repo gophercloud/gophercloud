46e07ae6467a0679561668d0794ad3c9cc9537f6
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-10-30 13:53:43 +0100

    Abstracting url paths a bit better

--- openstack/identity/v2/extensions/admin/roles/urls.go
+++ openstack/identity/v2/extensions/admin/roles/urls.go
@@ -2,16 +2,19 @@ package roles
 
 import "github.com/rackspace/gophercloud"
 
-const extPath = "OS-KSADMN/roles"
+const (
+	ExtPath  = "OS-KSADMN/roles"
+	UserPath = "users"
+)
 
 func resourceURL(c *gophercloud.ServiceClient, id string) string {
-	return c.ServiceURL(extPath, id)
+	return c.ServiceURL(ExtPath, id)
 }
 
 func rootURL(c *gophercloud.ServiceClient) string {
-	return c.ServiceURL(extPath)
+	return c.ServiceURL(ExtPath)
 }
 
 func userRoleURL(c *gophercloud.ServiceClient, tenantID, userID, roleID string) string {
-	return c.ServiceURL("tenants", tenantID, "users", userID, extPath, roleID)
+	return c.ServiceURL("tenants", tenantID, UserPath, userID, ExtPath, roleID)
 }
