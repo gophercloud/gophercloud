c8cc45e65907deec70fc5ef1457d10ed475ff704
Jon Perritt <jrperritt@gmail.com>
2014-12-22 22:19:03 -0700

    move template to common.go

--- acceptance/openstack/orchestration/v1/stacks_test.go
+++ acceptance/openstack/orchestration/v1/stacks_test.go
@@ -11,32 +11,6 @@ import (
 	th "github.com/rackspace/gophercloud/testhelper"
 )
 
-const template = `
-{
-		"heat_template_version": "2013-05-23",
-		"description": "Simple template to test heat commands",
-		"parameters": {
-				"flavor": {
-						"default": "m1.tiny",
-						"type": "string"
-				}
-		},
-		"resources": {
-				"hello_world": {
-						"type":"OS::Nova::Server",
-						"properties": {
-								"key_name": "heat_key",
-								"flavor": {
-										"get_param": "flavor"
-								},
-								"image": "ad091b52-742f-469e-8f3c-fd81cadf0743",
-								"user_data": "#!/bin/bash -xv\necho \"hello world\" &gt; /root/hello-world.txt\n"
-						}
-				}
-		}
-}
-`
-
 func TestStacks(t *testing.T) {
 	// Create a provider client for making the HTTP requests.
 	// See common.go in this directory for more information.
