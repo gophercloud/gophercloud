9a9c386a5f0a03c45b2a5eb65ce4bc78dd45b51c
Jon Perritt <jrperritt@gmail.com>
2015-01-19 17:16:22 -0700

    openstack cdn base unit tests

---
+++ openstack/cdn/v1/base/requests_test.go
@@ -0,0 +1,45 @@
+package base
+
+import (
+	"testing"
+
+	th "github.com/rackspace/gophercloud/testhelper"
+	fake "github.com/rackspace/gophercloud/testhelper/client"
+)
+
+func TestGetContainer(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	HandleGetSuccessfully(t)
+
+	actual, err := Get(fake.ServiceClient()).Extract()
+	th.CheckNoErr(t, err)
+
+	expected := HomeDocument{
+		"rel/cdn": `{
+        "href-template": "services{?marker,limit}",
+        "href-vars": {
+            "marker": "param/marker",
+            "limit": "param/limit"
+        },
+        "hints": {
+            "allow": [
+                "GET"
+            ],
+            "formats": {
+                "application/json": {}
+            }
+        }
+    }`,
+	}
+	th.CheckDeepEquals(t, expected, *actual)
+}
+
+func TestPingContainer(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	HandlePingSuccessfully(t)
+
+	err := Ping(fake.ServiceClient()).ExtractErr()
+	th.CheckNoErr(t, err)
+}
