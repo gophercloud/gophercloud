1d3bc4b794b3457961f4471485ce52546d3062d0
Jon Perritt <jrperritt@gmail.com>
2015-02-04 12:06:19 -0700

    rackspace adopt stack op and unit test

--- rackspace/orchestration/v1/stacks/delegate.go
+++ rackspace/orchestration/v1/stacks/delegate.go
@@ -10,3 +10,9 @@ import (
 func Create(c *gophercloud.ServiceClient, opts os.CreateOptsBuilder) os.CreateResult {
 	return os.Create(c, opts)
 }
+
+// Adopt accepts an os.AdoptOpts struct and creates a new stack from existing stack
+// resources using the values provided.
+func Adopt(c *gophercloud.ServiceClient, opts os.AdoptOptsBuilder) os.AdoptResult {
+	return os.Adopt(c, opts)
+}
