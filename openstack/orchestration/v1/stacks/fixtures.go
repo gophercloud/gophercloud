91246b533955b5befbd938eb20cf503a61a1f55e
Jon Perritt <jrperritt@gmail.com>
2015-02-11 13:10:35 -0700

    remove unneeded comment

--- openstack/orchestration/v1/stacks/fixtures.go
+++ openstack/orchestration/v1/stacks/fixtures.go
@@ -278,33 +278,6 @@ func HandlePreviewSuccessfully(t *testing.T, output string) {
 	})
 }
 
-/*
-`
-{
-"heat_template_version": "2013-05-23",
-"description": "Simple template to test heat commands",
-"parameters": {
-"flavor": {
-"default": "m1.tiny",
-"type": "string"
-}
-},
-"resources": {
-"hello_world": {
-"type": "OS::Nova::Server",
-"properties": {
-"key_name": "heat_key",
-"flavor": {
-"get_param": "flavor"
-},
-"image": "ad091b52-742f-469e-8f3c-fd81cadf0743",
-"user_data": "#!/bin/bash -xv\necho \"hello world\" &gt; /root/hello-world.txt\n"
-}
-}
-}
-}`
-*/
-
 // AbandonExpected represents the expected object from an Abandon request.
 var AbandonExpected = &AbandonedStack{
 	Status: "COMPLETE",
