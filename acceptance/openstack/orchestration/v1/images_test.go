a8e664b7c3d442147e2bfbee1f493f77e2ff8b71
James Fisher <jhf@trifork.com>
2015-06-01 15:09:07 +0100

    dummy test

---
+++ acceptance/openstack/orchestration/v1/images_test.go
@@ -0,0 +1,13 @@
+// +build acceptance
+
+package v2
+
+import (
+	"testing"
+
+	th "github.com/rackspace/gophercloud/testhelper"
+)
+
+func TestImages(t *testing.T) {
+	th.AssertEquals(true, true)
+}
