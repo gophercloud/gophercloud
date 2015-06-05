150a922af6314a72851ba05e80a54297a7af3011
Daniel Speichert <daniel_speichert@cable.comcast.com>
2015-06-05 10:32:27 -0400

    initial telemetry v2

---
+++ openstack/telemetry/v2/meters/requests.go
@@ -0,0 +1,88 @@
+package meters
+
+import "github.com/rackspace/gophercloud"
+
+// ListOptsBuilder allows extensions to add additional parameters to the
+// List request.
+type ListOptsBuilder interface {
+	ToMeterListQuery() (string, error)
+}
+
+// ListOpts allows the filtering and sorting of collections through
+// the API. Filtering is achieved by passing in struct field values that map to
+// the server attributes you want to see returned.
+type ListOpts struct {
+}
+
+// ToServerListQuery formats a ListOpts into a query string.
+func (opts ListOpts) ToMeterListQuery() (string, error) {
+	q, err := gophercloud.BuildQueryString(opts)
+	if err != nil {
+		return "", err
+	}
+	return q.String(), nil
+}
+
+// List makes a request against the API to list meters accessible to you.
+func List(client *gophercloud.ServiceClient, opts ListOptsBuilder) (res listResult, err error) {
+	url := listURL(client)
+
+	if opts != nil {
+		query, err := opts.ToMeterListQuery()
+		if err != nil {
+			return res, err
+		}
+		url += query
+	}
+
+	_, err = client.Get(url, &res.Body, &gophercloud.RequestOpts{})
+
+	return
+}
+
+// StatisticsOptsBuilder allows extensions to add additional parameters to the
+// List request.
+type MeterStatisticsOptsBuilder interface {
+	ToMeterStatisticsQuery() (string, error)
+}
+
+// StatisticsOpts allows the filtering and sorting of collections through
+// the API. Filtering is achieved by passing in struct field values that map to
+// the server attributes you want to see returned.
+type MeterStatisticsOpts struct {
+	QueryField string `q:"q.field"`
+	QueryOp    string `q:"q.op"`
+	QueryValue string `q:"q.value"`
+
+	// Optional group by
+	GroupBy string `q:"groupby"`
+
+	// Optional number of seconds in a period
+	Period int `q:"period"`
+}
+
+// ToStatisticsQuery formats a StatisticsOpts into a query string.
+func (opts MeterStatisticsOpts) ToMeterStatisticsQuery() (string, error) {
+	q, err := gophercloud.BuildQueryString(opts)
+	if err != nil {
+		return "", err
+	}
+	return q.String(), nil
+}
+
+// List makes a request against the API to list meters accessible to you.
+func MeterStatistics(client *gophercloud.ServiceClient, n string, opts MeterStatisticsOptsBuilder) (res statisticsResult, err error) {
+	url := statisticsURL(client, n)
+
+	if opts != nil {
+		query, err := opts.ToMeterStatisticsQuery()
+		if err != nil {
+			return res, err
+		}
+		url += query
+	}
+
+	_, err = client.Get(url, &res.Body, &gophercloud.RequestOpts{})
+
+	return
+}
---
+++ openstack/telemetry/v2/meters/results.go
@@ -0,0 +1,99 @@
+package meters
+
+import (
+	"reflect"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/rackspace/gophercloud"
+)
+
+type MeterListResult struct {
+	MeterId    string `json:"meter_id"`
+	Name       string `json:"name"`
+	ProjectId  string `json:"project_id"`
+	ResourceId string `json:"resource_id"`
+	Source     string `json:"source"`
+	Type       string `json:"type"`
+	Unit       string `json:"user"`
+	UserId     string `json:"user_id"`
+}
+
+type listResult struct {
+	gophercloud.Result
+}
+
+// Extract interprets any serverResult as a Server, if possible.
+func (r listResult) Extract() (*[]MeterListResult, error) {
+	if r.Err != nil {
+		return nil, r.Err
+	}
+
+	var response []MeterListResult
+
+	config := &mapstructure.DecoderConfig{
+		DecodeHook: toMapFromString,
+		Result:     &response,
+	}
+	decoder, err := mapstructure.NewDecoder(config)
+	if err != nil {
+		return nil, err
+	}
+
+	err = decoder.Decode(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
+
+type MeterStatisticsResult struct {
+	Avg           float32 `json:"avg"`
+	Count         int     `json:"count"`
+	Duration      float32 `json:"duration"`
+	DurationEnd   string  `json:"duration_end"`
+	DurationStart string  `json:"duration_start"`
+	Max           float32 `json:"max"`
+	Min           float32 `json:"min"`
+	Period        int     `json:"user_id"`
+	PeriodEnd     string  `json:"period_end"`
+	PeriodStart   string  `json:"period_start"`
+	Sum           float32 `json:"sum"`
+	Unit          string  `json:"unit"`
+}
+
+type statisticsResult struct {
+	gophercloud.Result
+}
+
+// Extract interprets any serverResult as a Server, if possible.
+func (r statisticsResult) Extract() (*[]MeterStatisticsResult, error) {
+	if r.Err != nil {
+		return nil, r.Err
+	}
+
+	var response []MeterStatisticsResult
+
+	config := &mapstructure.DecoderConfig{
+		DecodeHook: toMapFromString,
+		Result:     &response,
+	}
+	decoder, err := mapstructure.NewDecoder(config)
+	if err != nil {
+		return nil, err
+	}
+
+	err = decoder.Decode(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
+
+func toMapFromString(from reflect.Kind, to reflect.Kind, data interface{}) (interface{}, error) {
+	if (from == reflect.String) && (to == reflect.Map) {
+		return map[string]interface{}{}, nil
+	}
+	return data, nil
+}
