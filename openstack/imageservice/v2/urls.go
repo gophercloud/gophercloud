920bd1396a59490d7bfa91f2533aceff37b2063b
James Fisher <jhf@trifork.com>
2015-06-09 10:01:13 +0000

    prefix urls with /images

--- openstack/imageservice/v2/urls.go
+++ openstack/imageservice/v2/urls.go
@@ -5,17 +5,17 @@ import "github.com/rackspace/gophercloud"
 // `listURL` is a pure function. `listURL(c)` is a URL for which a GET
 // request will respond with a list of images in the service `c`.
 func listURL(c *gophercloud.ServiceClient) string {
-	return c.Endpoint
+	return c.ServiceURL("images")
 }
 
 func createURL(c *gophercloud.ServiceClient) string {
-	return c.Endpoint
+	return c.ServiceURL("images")
 }
 
 // `imageURL(c,i)` is the URL for the image identified by ID `i` in
 // the service `c`.
 func imageURL(c *gophercloud.ServiceClient, imageID string) string {
-	return c.ServiceURL(imageID)
+	return c.ServiceURL("images", imageID)
 }
 
 // `getURL(c,i)` is a URL for which a GET request will respond with
@@ -30,7 +30,7 @@ var deleteURL = imageURL
 // `imageDataURL(c,i)` is the URL for the binary image data for the
 // image identified by ID `i` in the service `c`.
 func imageDataURL(c *gophercloud.ServiceClient, imageID string) string {
-	return c.ServiceURL(imageID, "file")
+	return c.ServiceURL("images", imageID, "file")
 }
 
 var getDataURL = imageDataURL
@@ -38,7 +38,7 @@ var getDataURL = imageDataURL
 var updateDataURL = imageDataURL
 
 func imageTagURL(c *gophercloud.ServiceClient, imageID string, tag string) string {
-	return c.ServiceURL(imageID, "tags", tag)
+	return c.ServiceURL("images", imageID, "tags", tag)
 }
 
 var createTagURL = imageTagURL
@@ -46,7 +46,7 @@ var createTagURL = imageTagURL
 var deleteTagURL = imageTagURL
 
 func imageMembersURL(c *gophercloud.ServiceClient, imageID string) string {
-	return c.ServiceURL(imageID, "members")
+	return c.ServiceURL("images", imageID, "members")
 }
 
 var listMembersURL = imageMembersURL
@@ -54,7 +54,7 @@ var listMembersURL = imageMembersURL
 var createMemberURL = imageMembersURL
 
 func imageMemberURL(c *gophercloud.ServiceClient, imageID string, memberID string) string {
-	return c.ServiceURL(imageID, "members", memberID)
+	return c.ServiceURL("images", imageID, "members", memberID)
 }
 
 var getMemberURL = imageMemberURL
@@ -64,9 +64,9 @@ var updateMemberURL = imageMemberURL
 var deleteMemberURL = imageMemberURL
 
 func reactivateImageURL(c *gophercloud.ServiceClient, imageID string) string {
-	return c.ServiceURL(imageID, "actions", "reactivate")
+	return c.ServiceURL("images", imageID, "actions", "reactivate")
 }
 
 func deactivateImageURL(c *gophercloud.ServiceClient, imageID string) string {
-	return c.ServiceURL(imageID, "actions", "deactivate")
+	return c.ServiceURL("images", imageID, "actions", "deactivate")
 }
--- openstack/imageservice/v2/urls_test.go
+++ openstack/imageservice/v2/urls_test.go
@@ -7,12 +7,12 @@ import (
 	th "github.com/rackspace/gophercloud/testhelper"
 )
 
-const endpoint = "http://localhost:57909/v2"
+const endpoint = "http://localhost:57909/v2/"
 
 func endpointClient() *gophercloud.ServiceClient {
 	return &gophercloud.ServiceClient{Endpoint: endpoint}
 }
 
 func TestListURL(t *testing.T) {
-	th.AssertEquals(t, endpoint, listURL(endpointClient()))
+	th.AssertEquals(t, endpoint + "images", listURL(endpointClient()))
 }
