c3d49b79485edd7f2bae90bdffc7065a5e15f313
Rickard von Essen <rickard.von.essen@gmail.com>
2016-02-16 20:59:18 +0100

    Added test for servers.GetPasswordResult and fixed spelling.

--- openstack/compute/v2/servers/requests.go
+++ openstack/compute/v2/servers/requests.go
@@ -862,7 +862,7 @@ func IDFromName(client *gophercloud.ServiceClient, name string) (string, error) 
 	}
 }
 
-// GetPassword makes a request agains the nova API to get the encrypted administrative password.
+// GetPassword makes a request against the nova API to get the encrypted administrative password.
 func GetPassword(client *gophercloud.ServiceClient, serverId string) GetPasswordResult {
 	var res GetPasswordResult
 	_, res.Err = client.Request("GET", passwordURL(client, serverId), gophercloud.RequestOpts{
