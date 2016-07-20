ddb2c25d62aba591fc6af755df517985e2948590
Carolyn Van Slyck <me@carolynvanslyck.com>
2016-07-20 16:45:50 -0500

    Support listing Magnum bays

--- openstack/containerorchestration/v1/bays/requests.go
+++ openstack/containerorchestration/v1/bays/requests.go
@@ -2,7 +2,48 @@ package bays
 
 import (
 	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/pagination"
 )
+
+// ListOptsBuilder allows extensions to add additional parameters to the
+// List request.
+type ListOptsBuilder interface {
+	ToBayListQuery() (string, error)
+}
+
+// ListOpts allows the sorting of paginated collections through
+// the API. SortKey allows you to sort by a particular bay attribute.
+// SortDir sets the direction, and is either `asc' or `desc'.
+// Marker and Limit are used for pagination.
+type ListOpts struct {
+	Marker  string `q:"marker"`
+	Limit   int    `q:"limit"`
+	SortKey string `q:"sort_key"`
+	SortDir string `q:"sort_dir"`
+}
+
+// ToBayListQuery formats a ListOpts into a query string.
+func (opts ListOpts) ToBayListQuery() (string, error) {
+	q, err := gophercloud.BuildQueryString(opts)
+	return q.String(), err
+}
+
+// List returns a Pager which allows you to iterate over a collection of
+// bays. It accepts a ListOpts struct, which allows you to sort
+// the returned collection for greater efficiency.
+func List(c *gophercloud.ServiceClient, opts ListOptsBuilder) pagination.Pager {
+	url := listURL(c)
+	if opts != nil {
+		query, err := opts.ToBayListQuery()
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		url += query
+	}
+	return pagination.NewPager(c, url, func(r pagination.PageResult) pagination.Page {
+		return BayPage{pagination.LinkedPageBase{PageResult: r}}
+	})
+}
 
 // Get retrieves a specific bay based on its unique ID.
 func Get(c *gophercloud.ServiceClient, id string) (r GetResult) {
