c9fc0ea3f108abe635283f65ff98ef0a187b6c52
Edouard Bouvet <ehdouh@gmail.com>
2016-10-18 10:59:00 +0300

    sfs: Add acceptance tests for share type Show Access

--- acceptance/openstack/sharedfilesystems/v2/sharetypes_test.go
+++ acceptance/openstack/sharedfilesystems/v2/sharetypes_test.go
@@ -113,3 +113,25 @@ func TestShareTypeExtraSpecs(t *testing.T) {
 
 	defer DeleteShareType(t, client, shareType)
 }
+
+func TestShareTypeShowAccess(t *testing.T) {
+	client, err := clients.NewSharedFileSystemV2Client()
+	if err != nil {
+		t.Fatalf("Unable to create shared file system client: %v", err)
+	}
+
+	shareType, err := CreateShareType(t, client)
+	if err != nil {
+		t.Fatalf("Unable to create share type: %v", err)
+	}
+
+	_, err = sharetypes.ShowAccess(client, shareType.ID).Extract()
+	if err != nil {
+		t.Fatalf("Unable to retrieve the access details for a share type: %v", err)
+	}
+
+	PrintShareType(t, shareType)
+
+	defer DeleteShareType(t, client, shareType)
+
+}
