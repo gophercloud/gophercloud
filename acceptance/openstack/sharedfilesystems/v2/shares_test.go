a9522fa69236a3658591ecbcc71ffc4f0147e05a
Mikko Valkonen <mikko.valkonen@ericsson.com>
2016-10-18 14:50:36 +0300

    sfs/shares: acceptance test for short listing

--- acceptance/openstack/sharedfilesystems/v2/shares_test.go
+++ acceptance/openstack/sharedfilesystems/v2/shares_test.go
@@ -13,7 +13,7 @@ func TestShareCreate(t *testing.T) {
 		t.Fatalf("Unable to create a sharedfs client: %v", err)
 	}
 
-	share, err := CreateShare(t, client)
+	share, err := CreateShare(t, client, "my test share")
 	if err != nil {
 		t.Fatalf("Unable to create a share: %v", err)
 	}
@@ -25,4 +25,34 @@ func TestShareCreate(t *testing.T) {
 		t.Errorf("Unable to retrieve share: %v", err)
 	}
 	PrintShare(t, created)
+}
+
+func TestShareList(t *testing.T) {
+	client, err := clients.NewSharedFileSystemV2Client()
+	if err != nil {
+		t.Fatalf("Unable to create a sharedfs client: %v", err)
+	}
+
+	names := []string{"share_one", "share_two", "share_three"}
+
+	created, err := CreateShares(t, client, names)
+	if err != nil {
+		t.Fatalf("Unable to create a share: %v", err)
+	}
+
+	defer DeleteShares(t, client, created)
+
+	pages, err := shares.List(client, &shares.ListOpts{}, false).AllPages()
+	if err != nil {
+		t.Fatalf("Failed to list shares: %v", err)
+	}
+
+	shares, err := shares.ExtractShares(pages)
+	if err != nil {
+		t.Fatalf("Unable to extract shares: %v", err)
+	}
+
+	for _, share := range shares {
+		PrintShare(t, &share)
+	}
 }
