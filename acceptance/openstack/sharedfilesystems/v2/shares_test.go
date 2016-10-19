c1f2e6f2c7f2b7754b342e44ea63d3ae4d8f7b10
Mikko Valkonen <mikko.valkonen@ericsson.com>
2016-10-19 16:01:22 +0300

    sfs/shares: list pagination acceptance test

--- acceptance/openstack/sharedfilesystems/v2/shares_test.go
+++ acceptance/openstack/sharedfilesystems/v2/shares_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gophercloud/gophercloud/acceptance/clients"
 	"github.com/gophercloud/gophercloud/openstack/sharedfilesystems/v2/shares"
+	"github.com/gophercloud/gophercloud/pagination"
 )
 
 func TestShareCreate(t *testing.T) {
@@ -85,4 +86,27 @@ func TestShareListDetail(t *testing.T) {
 	for _, share := range shares {
 		PrintShare(t, &share)
 	}
+}
+
+func TestShareListDetailPaginate(t *testing.T) {
+	client, err := clients.NewSharedFileSystemV2Client()
+	if err != nil {
+		t.Fatalf("Unable to create a sharedfs client: %v", err)
+	}
+
+	names := []string{"share_one", "share_two", "share_three"}
+
+	created, err := CreateShares(t, client, names)
+	if err != nil {
+		t.Fatalf("Unable to create a share: %v", err)
+	}
+
+	defer DeleteShares(t, client, created)
+
+	err = shares.List(client, &shares.ListOpts{}, true).EachPage(func(page pagination.Page) (bool, error) {
+		l, _ := shares.ExtractShares(page)
+		t.Logf("Got some shares: %+v\n", l)
+		return true, nil
+	})
+
 }
