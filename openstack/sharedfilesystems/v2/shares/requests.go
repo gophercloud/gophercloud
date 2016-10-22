15556972ed8b6cac9abc4df0bf7d6e2ffd136276
Mikko Valkonen <mikko.valkonen@ericsson.com>
2016-10-22 11:36:23 +0300

    sfs/shares: fixes according to comments

--- openstack/sharedfilesystems/v2/shares/requests.go
+++ openstack/sharedfilesystems/v2/shares/requests.go
@@ -10,22 +10,41 @@ type CreateOptsBuilder interface {
 	ToShareCreateMap() (map[string]interface{}, error)
 }
 
-// CreateOpts used for ...
+// CreateOpts contains the options for create a Share. This object is
+// passed to shares.Create(). For more information about these parameters,
+// please refer to the Share object, or the shared file systems API v2
+// documentation
 type CreateOpts struct {
-	ShareProto         string            `json:"share_proto"`
-	Size               int               `json:"size"`
-	Name               string            `json:"name,omitempty"`
-	Description        string            `json:"description,omitempty"`
-	DisplayName        string            `json:"display_name,omitempty"`
-	DisplayDescription string            `json:"display_description,omitempty"`
-	ShareType          string            `json:"share_type,omitempty"`
-	VolumeType         string            `json:"volume_type,omitempty"`
-	SnapshotID         string            `json:"snapshot_id,omitempty"`
-	IsPublic           bool              `json:"is_public,omitempty"`
-	MetaData           map[string]string `json:"metadata,omitempty"`
-	ShareNetworkID     string            `json:"share_network_id,omitempty"`
-	ConsistencyGroupID string            `json:"consistency_group_id,omitempty"`
-	AvailabilityZone   string            `json:"availability_zone,omitempty"`
+	// Defines the share protocol to use
+	ShareProto string `json:"share_proto" required:"true"`
+	// Size in GB
+	Size int `json:"size" required:"true"`
+	// Defines the share name
+	Name string `json:"name,omitempty"`
+	// Share description
+	Description string `json:"description,omitempty"`
+	// DisplayName is equivalent to Name. The API supports using both
+	// This is an inherited attribute from the block storage API
+	DisplayName string `json:"display_name,omitempty"`
+	// DisplayDescription is equivalent to Description. The API supports using bot
+	// This is an inherited attribute from the block storage API
+	DisplayDescription string `json:"display_description,omitempty"`
+	// ShareType defines the sharetype. If omitted, a default share type is used
+	ShareType string `json:"share_type,omitempty"`
+	// VolumeType is deprecated but supported. Either ShareType or VolumeType can be used
+	VolumeType string `json:"volume_type,omitempty"`
+	// The UUID from which to create a share
+	SnapshotID string `json:"snapshot_id,omitempty"`
+	// Determines whether or not the share is public
+	IsPublic *bool `json:"is_public,omitempty"`
+	// Key value pairs of user defined metadata
+	Metadata map[string]string `json:"metadata,omitempty"`
+	// The UUID of the share network to which the share belongs to
+	ShareNetworkID string `json:"share_network_id,omitempty"`
+	// The UUID of the consistency group to which the share belongs to
+	ConsistencyGroupID string `json:"consistency_group_id,omitempty"`
+	// The availability zone of the share
+	AvailabilityZone string `json:"availability_zone,omitempty"`
 }
 
 // ToShareCreateMap assembles a request body based on the contents of a
