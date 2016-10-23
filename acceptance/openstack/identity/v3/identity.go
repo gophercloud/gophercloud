47c96b983727165b1a03386c2b54ce12d6a958aa
Joe Topjian <joe@topjian.net>
2016-10-23 18:17:25 +0000

    Identity v3 Projects List

--- acceptance/openstack/identity/v3/identity.go
+++ acceptance/openstack/identity/v3/identity.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/gophercloud/gophercloud/openstack/identity/v3/endpoints"
+	"github.com/gophercloud/gophercloud/openstack/identity/v3/projects"
 	"github.com/gophercloud/gophercloud/openstack/identity/v3/services"
 	"github.com/gophercloud/gophercloud/openstack/identity/v3/tokens"
 )
@@ -16,6 +17,17 @@ func PrintEndpoint(t *testing.T, endpoint *endpoints.Endpoint) {
 	t.Logf("Region: %s", endpoint.Region)
 	t.Logf("ServiceID: %s", endpoint.ServiceID)
 	t.Logf("URL: %s", endpoint.URL)
+}
+
+// PrintProject will print a project and all of its attributes.
+func PrintProject(t *testing.T, project *projects.Project) {
+	t.Logf("ID: %s", project.ID)
+	t.Logf("IsDomain: %t", project.IsDomain)
+	t.Logf("Description: %s", project.Description)
+	t.Logf("DomainID: %s", project.DomainID)
+	t.Logf("Enabled: %t", project.Enabled)
+	t.Logf("Name: %s", project.Name)
+	t.Logf("ParentID: %s", project.ParentID)
 }
 
 // PrintService will print a service and all of its attributes.
