6be8c4247d29435e773753d98626bfe0b0b0d03c
jrperritt <jrperritt@gmail.com>
2016-11-09 13:08:35 -0600

    comments for tenantattr pkg

--- openstack/blockstorage/extensions/tenantattr/results.go
+++ openstack/blockstorage/extensions/tenantattr/results.go
@@ -1,5 +1,7 @@
 package tenantattr
 
+// An extension to the base Volume object
 type VolumeExt struct {
+	// TenantID is the id of the project that owns the volume.
 	TenantID string `json:"os-vol-tenant-attr:tenant_id"`
 }
