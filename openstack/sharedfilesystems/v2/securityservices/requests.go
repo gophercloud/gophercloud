c94fe64628bf0f46f5b35f34116a1c8607de0f81
Edouard Bouvet <edouard.bouvet.dit.marechal@ericsson.com>
2016-11-10 09:14:27 +0200

    sfs: Fix comments

--- openstack/sharedfilesystems/v2/securityservices/requests.go
+++ openstack/sharedfilesystems/v2/securityservices/requests.go
@@ -2,6 +2,15 @@ package securityservices
 
 import "github.com/gophercloud/gophercloud"
 
+type SecurityServiceType string
+
+// Valid security service types
+const (
+	LDAP            SecurityServiceType = "ldap"
+	Kerberos        SecurityServiceType = "kerberos"
+	ActiveDirectory SecurityServiceType = "active_directory"
+)
+
 // CreateOptsBuilder allows extensions to add additional parameters to the
 // Create request.
 type CreateOptsBuilder interface {
@@ -12,12 +21,12 @@ type CreateOptsBuilder interface {
 // passed to the securityservices.Create function. For more information about
 // these parameters, see the SecurityService object.
 type CreateOpts struct {
+	// The security service type. A valid value is ldap, kerberos, or active_directory
+	Type SecurityServiceType `json:"type" required:"true"`
 	// The security service name
-	Name string `json:"name"`
+	Name string `json:"name,omitempty"`
 	// The security service description
 	Description string `json:"description,omitempty"`
-	// The security service type. A valid value is ldap, kerberos, or active_directory
-	Type string `json:"type" required:"true"`
 	// The DNS IP address that is used inside the tenant network
 	DNSIP string `json:"dns_ip,omitempty"`
 	// The security service user or group name that is used by the tenant
