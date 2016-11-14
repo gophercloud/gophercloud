4bfa025ae1aeb8f726281f8f05f97f31a07a5d5d
jrperritt <jrperritt@gmail.com>
2016-11-14 14:49:46 -0600

    add volumetenants.VolumeExt.Unmarshal

--- openstack/blockstorage/extensions/volumetenants/results.go
+++ openstack/blockstorage/extensions/volumetenants/results.go
@@ -1,7 +1,13 @@
 package volumetenants
 
+import "encoding/json"
+
 // An extension to the base Volume object
 type VolumeExt struct {
 	// TenantID is the id of the project that owns the volume.
 	TenantID string `json:"os-vol-tenant-attr:tenant_id"`
+}
+
+func (s *VolumeExt) UnmarshalJSON(b []byte) error {
+	return json.Unmarshal(b, &s)
 }
