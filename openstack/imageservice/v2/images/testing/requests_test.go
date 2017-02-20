2aaed7fc5684e8cd90e4b5e112f06c7f1769da2c
jrperritt <jrperritt@gmail.com>
2017-02-20 16:19:24 -0600

    fix images 'AllPages' paging (#266)
    * fix images AllPages paging; make AllPages more robust
    
    * need to keep using the local page when iterating in AllPages

--- openstack/imageservice/v2/images/testing/requests_test.go
+++ openstack/imageservice/v2/images/testing/requests_test.go
@@ -44,6 +44,19 @@ func TestListImage(t *testing.T) {
 	th.AssertEquals(t, 3, count)
 }
 
+func TestAllPagesImage(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	HandleImageListSuccessfully(t)
+
+	pages, err := images.List(fakeclient.ServiceClient(), nil).AllPages()
+	th.AssertNoErr(t, err)
+	images, err := images.ExtractImages(pages)
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, 3, len(images))
+}
+
 func TestCreateImage(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
