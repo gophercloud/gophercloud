68911ff35c70f4b434bcc099c4628530c2bb59bc
Joe Topjian <joe@topjian.net>
2017-03-03 03:11:50 +0000

    Changing acceptance test for DNS v2 to use AllPages

--- acceptance/openstack/dns/v2/zones_test.go
+++ acceptance/openstack/dns/v2/zones_test.go
@@ -1,4 +1,5 @@
 // +build acceptance dns zones
+
 package v2
 
 import (
@@ -7,7 +8,6 @@ import (
 	"github.com/gophercloud/gophercloud/acceptance/clients"
 	"github.com/gophercloud/gophercloud/acceptance/tools"
 	"github.com/gophercloud/gophercloud/openstack/dns/v2/zones"
-	"github.com/gophercloud/gophercloud/pagination"
 )
 
 func TestZonesList(t *testing.T) {
@@ -16,34 +16,16 @@ func TestZonesList(t *testing.T) {
 		t.Fatalf("Unable to create a DNS client: %v", err)
 	}
 
-	// This works
 	var allZones []zones.Zone
-	pager := zones.List(client)
-	err = pager.EachPage(func(page pagination.Page) (bool, error) {
-		zones, err := zones.ExtractZones(page)
-		if err != nil {
-			return false, err
-		}
+	allPages, err := zones.List(client).AllPages()
+	if err != nil {
+		t.Fatalf("Unable to retrieve zones: %v", err)
+	}
 
-		for _, i := range zones {
-			allZones = append(allZones, i)
-		}
-
-		return true, nil
-	})
-
-	// This doesn't
-	/*
-		allPages, err := zones.List(client).AllPages()
-		if err != nil {
-			t.Fatalf("Unable to retrieve zones: %v", err)
-		}
-
-		allZones, err = zones.ExtractZones(allPages)
-		if err != nil {
-			t.Fatalf("Unable to extract zones: %v", err)
-		}
-	*/
+	allZones, err = zones.ExtractZones(allPages)
+	if err != nil {
+		t.Fatalf("Unable to extract zones: %v", err)
+	}
 
 	for _, zone := range allZones {
 		tools.PrintResource(t, &zone)
