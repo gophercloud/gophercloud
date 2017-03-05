06b8fb31b8d7373a49dc312a81720d2e944040fe
Joe Topjian <joe@topjian.net>
2017-03-05 07:12:37 +0000

    DNS v2: RecordSet Create, Update, Delete unit tests

--- openstack/dns/v2/recordsets/testing/fixtures.go
+++ openstack/dns/v2/recordsets/testing/fixtures.go
@@ -229,3 +229,149 @@ func HandleGetSuccessfully(t *testing.T) {
 			fmt.Fprintf(w, GetOutput)
 		})
 }
+
+// CreateRecordSetRequest is a sample request to create a resource record.
+const CreateRecordSetRequest = `
+{
+  "name" : "example.org.",
+  "description" : "This is an example record set.",
+  "type" : "A",
+  "ttl" : 3600,
+  "records" : [
+      "10.1.0.2"
+  ]
+}
+`
+
+// CreateRecordSetResponse is a sample response to a create request.
+const CreateRecordSetResponse = `
+{
+    "description": "This is an example record set.",
+    "links": {
+        "self": "https://127.0.0.1:9001/v2/zones/2150b1bf-dee2-4221-9d85-11f7886fb15f/recordsets/f7b10e9b-0cae-4a91-b162-562bc6096648"
+    },
+    "updated_at": null,
+    "records": [
+        "10.1.0.2"
+    ],
+    "ttl": 3600,
+    "id": "f7b10e9b-0cae-4a91-b162-562bc6096648",
+    "name": "example.org.",
+    "project_id": "4335d1f0-f793-11e2-b778-0800200c9a66",
+    "zone_id": "2150b1bf-dee2-4221-9d85-11f7886fb15f",
+    "zone_name": "example.com.",
+    "created_at": "2014-10-24T19:59:44.000000",
+    "version": 1,
+    "type": "A",
+    "status": "PENDING",
+    "action": "CREATE"
+}
+`
+
+// CreatedRecordSet is the expected created resource record.
+var CreatedRecordSet = FirstRecordSet
+
+// HandleZoneCreationSuccessfully configures the test server to respond to a Create request.
+func HandleCreateSuccessfully(t *testing.T) {
+	th.Mux.HandleFunc("/zones/2150b1bf-dee2-4221-9d85-11f7886fb15f/recordsets",
+		func(w http.ResponseWriter, r *http.Request) {
+			th.TestMethod(t, r, "POST")
+			th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
+			th.TestJSONRequest(t, r, CreateRecordSetRequest)
+
+			w.WriteHeader(http.StatusCreated)
+			w.Header().Add("Content-Type", "application/json")
+			fmt.Fprintf(w, CreateRecordSetResponse)
+		})
+}
+
+// UpdateRecordSetRequest is a sample request to update a record set.
+const UpdateRecordSetRequest = `
+{
+  "description" : "Updated description",
+  "ttl" : null,
+  "records" : [
+      "10.1.0.2",
+      "10.1.0.3"
+  ]
+}
+`
+
+// UpdateRecordSetResponse is a sample response to an update request.
+const UpdateRecordSetResponse = `
+{
+    "description": "Updated description",
+    "links": {
+        "self": "https://127.0.0.1:9001/v2/zones/2150b1bf-dee2-4221-9d85-11f7886fb15f/recordsets/f7b10e9b-0cae-4a91-b162-562bc6096648"
+    },
+    "updated_at": null,
+    "records": [
+        "10.1.0.2",
+        "10.1.0.3"
+    ],
+    "ttl": 3600,
+    "id": "f7b10e9b-0cae-4a91-b162-562bc6096648",
+    "name": "example.org.",
+    "project_id": "4335d1f0-f793-11e2-b778-0800200c9a66",
+    "zone_id": "2150b1bf-dee2-4221-9d85-11f7886fb15f",
+    "zone_name": "example.com.",
+    "created_at": "2014-10-24T19:59:44.000000",
+    "version": 2,
+    "type": "A",
+    "status": "PENDING",
+    "action": "UPDATE"
+}
+`
+
+// HandleUpdateSuccessfully configures the test server to respond to an Update request.
+func HandleUpdateSuccessfully(t *testing.T) {
+	th.Mux.HandleFunc("/zones/2150b1bf-dee2-4221-9d85-11f7886fb15f/recordsets/f7b10e9b-0cae-4a91-b162-562bc6096648",
+		func(w http.ResponseWriter, r *http.Request) {
+			th.TestMethod(t, r, "PUT")
+			th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
+			th.TestJSONRequest(t, r, UpdateRecordSetRequest)
+
+			w.WriteHeader(http.StatusOK)
+			w.Header().Add("Content-Type", "application/json")
+			fmt.Fprintf(w, UpdateRecordSetResponse)
+		})
+}
+
+// DeleteRecordSetResponse is a sample response to a delete request.
+const DeleteRecordSetResponse = `
+{
+    "description": "Updated description",
+    "links": {
+        "self": "https://127.0.0.1:9001/v2/zones/2150b1bf-dee2-4221-9d85-11f7886fb15f/recordsets/f7b10e9b-0cae-4a91-b162-562bc6096648"
+    },
+    "updated_at": null,
+    "records": [
+        "10.1.0.2",
+        "10.1.0.3",
+    ],
+    "ttl": null,
+    "id": "f7b10e9b-0cae-4a91-b162-562bc6096648",
+    "name": "example.org.",
+    "project_id": "4335d1f0-f793-11e2-b778-0800200c9a66",
+    "zone_id": "2150b1bf-dee2-4221-9d85-11f7886fb15f",
+    "zone_name": "example.com.",
+    "created_at": "2014-10-24T19:59:44.000000",
+    "version": 2,
+    "type": "A",
+    "status": "PENDING",
+    "action": "UPDATE"
+}
+`
+
+// HandleDeleteSuccessfully configures the test server to respond to an Delete request.
+func HandleDeleteSuccessfully(t *testing.T) {
+	th.Mux.HandleFunc("/zones/2150b1bf-dee2-4221-9d85-11f7886fb15f/recordsets/f7b10e9b-0cae-4a91-b162-562bc6096648",
+		func(w http.ResponseWriter, r *http.Request) {
+			th.TestMethod(t, r, "DELETE")
+			th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
+
+			w.WriteHeader(http.StatusAccepted)
+			//w.Header().Add("Content-Type", "application/json")
+			//fmt.Fprintf(w, DeleteZoneResponse)
+		})
+}
