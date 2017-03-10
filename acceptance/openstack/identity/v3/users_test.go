881a93bcfe05e870f98b072116af37047dacbb83
Joe Topjian <joe@topjian.net>
2017-03-10 03:49:28 +0000

    Identity v3: User Create acceptance tests

--- acceptance/openstack/identity/v3/users_test.go
+++ acceptance/openstack/identity/v3/users_test.go
@@ -60,3 +60,31 @@ func TestUsersGet(t *testing.T) {
 
 	tools.PrintResource(t, p)
 }
+
+func TestUserCRUD(t *testing.T) {
+	client, err := clients.NewIdentityV3Client()
+	if err != nil {
+		t.Fatalf("Unable to obtain an identity client: %v", err)
+	}
+
+	project, err := CreateProject(t, client, nil)
+	if err != nil {
+		t.Fatalf("Unable to create project: %v", err)
+	}
+	defer DeleteProject(t, client, project.ID)
+
+	tools.PrintResource(t, project)
+
+	createOpts := users.CreateOpts{
+		DefaultProjectID: project.ID,
+		Password:         "foobar",
+		DomainID:         "default",
+	}
+
+	user, err := CreateUser(t, client, &createOpts)
+	if err != nil {
+		t.Fatalf("Unable to create user: %v", err)
+	}
+
+	tools.PrintResource(t, user)
+}
