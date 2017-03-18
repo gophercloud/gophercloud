0eba7ebc5c2ce80b8a1ec9d9cd43cc1eaba7e55c
Joe Topjian <joe@topjian.net>
2017-03-18 17:47:58 +0000

    Network v2: Provider Extension unit tests

--- openstack/networking/v2/networks/testing/fixtures.go
+++ openstack/networking/v2/networks/testing/fixtures.go
@@ -16,7 +16,10 @@ const ListResponse = `
             "admin_state_up": true,
             "tenant_id": "4fd44f30292945e481c7b8a0c8908869",
             "shared": true,
-            "id": "d32019d3-bc6e-4319-9c1d-6722fc136a22"
+            "id": "d32019d3-bc6e-4319-9c1d-6722fc136a22",
+            "provider:segmentation_id": 9876543210,
+            "provider:physical_network": null,
+            "provider:network_type": "local"
         },
         {
             "status": "ACTIVE",
@@ -27,7 +30,10 @@ const ListResponse = `
             "admin_state_up": true,
             "tenant_id": "26a7980765d0414dbc1fc1f88cdb7e6e",
             "shared": false,
-            "id": "db193ab3-96e3-4cb3-8fc5-05f4296d0324"
+            "id": "db193ab3-96e3-4cb3-8fc5-05f4296d0324",
+            "provider:segmentation_id": 1234567890,
+            "provider:physical_network": null,
+            "provider:network_type": "local"
         }
     ]
 }`
@@ -43,7 +49,10 @@ const GetResponse = `
         "admin_state_up": true,
         "tenant_id": "4fd44f30292945e481c7b8a0c8908869",
         "shared": true,
-        "id": "d32019d3-bc6e-4319-9c1d-6722fc136a22"
+        "id": "d32019d3-bc6e-4319-9c1d-6722fc136a22",
+        "provider:segmentation_id": 9876543210,
+        "provider:physical_network": null,
+        "provider:network_type": "local"
     }
 }`
 
@@ -64,7 +73,10 @@ const CreateResponse = `
         "admin_state_up": true,
         "tenant_id": "26a7980765d0414dbc1fc1f88cdb7e6e",
         "shared": false,
-        "id": "db193ab3-96e3-4cb3-8fc5-05f4296d0324"
+        "id": "db193ab3-96e3-4cb3-8fc5-05f4296d0324",
+        "provider:segmentation_id": 9876543210,
+        "provider:physical_network": null,
+        "provider:network_type": "local"
     }
 }`
 
@@ -96,7 +108,10 @@ const UpdateResponse = `
         "admin_state_up": false,
         "tenant_id": "4fd44f30292945e481c7b8a0c8908869",
         "shared": true,
-        "id": "4e8e5957-649f-477b-9e5b-f1f75b21c03c"
+        "id": "4e8e5957-649f-477b-9e5b-f1f75b21c03c",
+        "provider:segmentation_id": 1234567890,
+        "provider:physical_network": null,
+        "provider:network_type": "local"
     }
 }`
 
