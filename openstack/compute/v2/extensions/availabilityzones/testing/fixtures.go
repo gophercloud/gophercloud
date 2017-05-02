2634be58f4f6c8b7da3bba4c5fc6a35e1a720c65
Di Xu <stephenhsu90@gmail.com>
2017-05-02 16:35:05 +0800

    compute: Lists and gets detailed availability zone information

---
+++ openstack/compute/v2/extensions/availabilityzones/testing/fixtures.go
@@ -0,0 +1,171 @@
+package testing
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	az "github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/availabilityzones"
+	th "github.com/gophercloud/gophercloud/testhelper"
+	"github.com/gophercloud/gophercloud/testhelper/client"
+)
+
+const GetOutput = `
+{
+    "availabilityZoneInfo": [
+        {
+            "hosts": null,
+            "zoneName": "nova",
+            "zoneState": {
+                "available": true
+            }
+        }
+    ]
+}
+`
+
+const GetDetailOutput = `
+{
+    "availabilityZoneInfo": [
+        {
+            "hosts": {
+                "conductor": {
+                    "nova-conductor": {
+                        "active": true,
+                        "available": true,
+                        "updated_at": null
+                    }
+                },
+                "consoleauth": {
+                    "nova-consoleauth": {
+                        "active": true,
+                        "available": true,
+                        "updated_at": null
+                    }
+                },
+                "network": {
+                    "nova-network": {
+                        "active": true,
+                        "available": true,
+                        "updated_at": "2015-09-18T09:50:55.000000"
+                    }
+                },
+                "scheduler": {
+                    "nova-scheduler": {
+                        "active": true,
+                        "available": true,
+                        "updated_at": null
+                    }
+                }
+            },
+            "zoneName": "internal",
+            "zoneState": {
+                "available": true
+            }
+        },
+        {
+            "hosts": {
+                "compute": {
+                    "nova-compute": {
+                        "active": true,
+                        "available": true,
+                        "updated_at": null
+                    }
+                }
+            },
+            "zoneName": "nova",
+            "zoneState": {
+                "available": true
+            }
+        }
+    ]
+}
+`
+
+var OSAZResult = az.OSAvailabilityZone{
+	AvailabilityZoneInfo: []az.AvailabilityZone{
+		{
+			Hosts:     az.Hosts{},
+			ZoneName:  "nova",
+			ZoneState: az.ZoneState{Available: true},
+		},
+	},
+}
+
+var nilTime time.Time
+var OSAZDetailResult = az.OSAvailabilityZone{
+	AvailabilityZoneInfo: []az.AvailabilityZone{
+		{
+			Hosts: az.Hosts{
+				Conductor: az.Conductor{
+					NovaConductor: az.StateofService{
+						Active:    true,
+						Available: true,
+						UpdatedAt: nilTime,
+					},
+				},
+				Consoleauth: az.Consoleauth{
+					NovaConsoleauth: az.StateofService{
+						Active:    true,
+						Available: true,
+						UpdatedAt: nilTime,
+					},
+				},
+				Network: az.Network{
+					NovaNetwork: az.StateofService{
+						Active:    true,
+						Available: true,
+						UpdatedAt: time.Date(2015, 9, 18, 9, 50, 55, 0, time.UTC),
+					},
+				},
+				Scheduler: az.Scheduler{
+					NovaScheduler: az.StateofService{
+						Active:    true,
+						Available: true,
+						UpdatedAt: nilTime,
+					},
+				},
+			},
+			ZoneName:  "internal",
+			ZoneState: az.ZoneState{Available: true},
+		},
+		{
+			Hosts: az.Hosts{
+				Compute: az.Compute{
+					NovaCompute: az.StateofService{
+						Active:    true,
+						Available: true,
+						UpdatedAt: nilTime,
+					},
+				},
+			},
+			ZoneName:  "nova",
+			ZoneState: az.ZoneState{Available: true},
+		},
+	},
+}
+
+// HandleGetSuccessfully configures the test server to respond to a Get request
+// for availability zone information.
+func HandleGetSuccessfully(t *testing.T) {
+	th.Mux.HandleFunc("/os-availability-zone", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprintf(w, GetOutput)
+	})
+}
+
+// HandleGetDetailSuccessfully configures the test server to respond to a Get request
+// for detailed availability zone information.
+func HandleGetDetailSuccessfully(t *testing.T) {
+	th.Mux.HandleFunc("/os-availability-zone/detail", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprintf(w, GetDetailOutput)
+	})
+}
