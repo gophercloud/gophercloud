60f08ddcdc9aaa4bc19e84d90c9b958bef2fd4ac
Joe Topjian <joe@topjian.net>
2017-05-28 19:57:54 -0600

    Refactor RemainingKeys to not alter original map

--- internal/util.go
+++ internal/util.go
@@ -5,21 +5,22 @@ import (
 )
 
 // RemainingKeys will inspect a struct and compare it to a map. Any key that
-// is defined in a JSON tag of the struct will be removed from the map. The
-// remaining map and keys are then returned.
+// is not defined in a JSON tag of the struct will be added to the extras map
+// and returned.
 //
 // This is useful for determining the extra fields returned in response bodies
 // for resources that can contain an arbitrary or dynamic number of fields.
-func RemainingKeys(s interface{}, m map[string]interface{}) map[string]interface{} {
+func RemainingKeys(s interface{}, m map[string]interface{}) (extras map[string]interface{}) {
+	extras = make(map[string]interface{})
 	valueOf := reflect.ValueOf(s)
 	typeOf := reflect.TypeOf(s)
 	for i := 0; i < valueOf.NumField(); i++ {
 		field := typeOf.Field(i)
 		tagValue := field.Tag.Get("json")
-		if _, ok := m[tagValue]; ok {
-			delete(m, tagValue)
+		if _, ok := m[tagValue]; !ok {
+			extras[tagValue] = m[tagValue]
 		}
 	}
 
-	return m
+	return
 }
