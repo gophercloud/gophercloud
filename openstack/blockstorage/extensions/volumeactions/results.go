cda048531232579003454433376c3b961b46d6a7
Vitaly Khabarov <vitkhab@gmail.com>
2017-07-16 23:34:05 +0300

    Added Extract method to UploadImageResult object

--- openstack/blockstorage/extensions/volumeactions/results.go
+++ openstack/blockstorage/extensions/volumeactions/results.go
@@ -50,6 +50,15 @@ func (r commonResult) Extract() (map[string]interface{}, error) {
 	return s.ConnectionInfo, err
 }
 
+// Extract will get an object with info about image being uploaded out of the UploadImageResult object.
+func (r UploadImageResult) Extract() (map[string]interface{}, error) {
+	var s struct {
+		VolumeUploadImage   map[string]interface{} `json:"os-volume_upload_image"`
+	}
+	err := r.ExtractInto(&s)
+	return s.VolumeUploadImage, err
+}
+
 // InitializeConnectionResult contains the response body and error from a Get request.
 type InitializeConnectionResult struct {
 	commonResult
