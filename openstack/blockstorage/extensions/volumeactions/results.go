6c5efa7ce22b68d1f2c3715f204cbc9e2e63c75f
Vitaly Khabarov <vitkhab@gmail.com>
2017-07-17 14:15:25 +0300

    Added structure VolumeImage

--- openstack/blockstorage/extensions/volumeactions/results.go
+++ openstack/blockstorage/extensions/volumeactions/results.go
@@ -1,6 +1,10 @@
 package volumeactions
 
-import "github.com/gophercloud/gophercloud"
+import (
+	"time"
+
+	"github.com/gophercloud/gophercloud"
+)
 
 // AttachResult contains the response body and error from a Get request.
 type AttachResult struct {
@@ -50,10 +54,32 @@ func (r commonResult) Extract() (map[string]interface{}, error) {
 	return s.ConnectionInfo, err
 }
 
+// VolumeImage contains information about volume upload to an image service.
+type VolumeImage struct {
+	// The ID of a volume an image is created from.
+	VolumeId string `json:"id"`
+	// Container format, may be bare, ofv, ova, etc.
+	ContainerFormat string `json:"container_format"`
+	// Disk format, may be raw, qcow2, vhd, vdi, vmdk, etc.
+	DiskFormat string `json:"disk_format"`
+	// Human-readable description for the volume.
+	Description string `json:"display_description"`
+	// The ID of an image being created.
+	ImageId string `json:"image_id"`
+	// Human-readable display name for the image.
+	ImageName string `json:"image_name"`
+	// Size of the volume in GB.
+	Size int `json:"size"`
+	// Current status of the volume.
+	Status string `json:"status"`
+	// The date when this volume was last updated.
+	UpdatedAt time.Time `json:"-"`
+}
+
 // Extract will get an object with info about image being uploaded out of the UploadImageResult object.
-func (r UploadImageResult) Extract() (map[string]interface{}, error) {
+func (r UploadImageResult) Extract() (VolumeImage, error) {
 	var s struct {
-		VolumeUploadImage map[string]interface{} `json:"os-volume_upload_image"`
+		VolumeUploadImage VolumeImage `json:"os-volume_upload_image"`
 	}
 	err := r.ExtractInto(&s)
 	return s.VolumeUploadImage, err
