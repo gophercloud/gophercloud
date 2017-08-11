d8b8a5ef1fe885102138383466a1656b9773c056
Joe Topjian <joe@topjian.net>
2017-08-10 22:53:17 -0600

    BlockStorage: apiversions

---
+++ openstack/blockstorage/apiversions/testing/fixtures.go
@@ -0,0 +1,144 @@
+package testing
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	th "github.com/gophercloud/gophercloud/testhelper"
+	"github.com/gophercloud/gophercloud/testhelper/client"
+)
+
+const APIListResponse = `
+{
+    "versions": [
+        {
+            "id": "v1.0",
+            "links": [
+                {
+                    "href": "http://docs.openstack.org/",
+                    "rel": "describedby",
+                    "type": "text/html"
+                },
+                {
+                    "href": "http://localhost:8776/v1/",
+                    "rel": "self"
+                }
+            ],
+            "media-types": [
+                {
+                    "base": "application/json",
+                    "type": "application/vnd.openstack.volume+json;version=1"
+                }
+            ],
+            "min_version": "",
+            "status": "DEPRECATED",
+            "updated": "2016-05-02T20:25:19Z",
+            "version": ""
+        },
+        {
+            "id": "v2.0",
+            "links": [
+                {
+                    "href": "http://docs.openstack.org/",
+                    "rel": "describedby",
+                    "type": "text/html"
+                },
+                {
+                    "href": "http://localhost:8776/v2/",
+                    "rel": "self"
+                }
+            ],
+            "media-types": [
+                {
+                    "base": "application/json",
+                    "type": "application/vnd.openstack.volume+json;version=1"
+                }
+            ],
+            "min_version": "",
+            "status": "SUPPORTED",
+            "updated": "2014-06-28T12:20:21Z",
+            "version": ""
+        },
+        {
+            "id": "v3.0",
+            "links": [
+                {
+                    "href": "http://docs.openstack.org/",
+                    "rel": "describedby",
+                    "type": "text/html"
+                },
+                {
+                    "href": "http://localhost:8776/v3/",
+                    "rel": "self"
+                }
+            ],
+            "media-types": [
+                {
+                    "base": "application/json",
+                    "type": "application/vnd.openstack.volume+json;version=1"
+                }
+            ],
+            "min_version": "3.0",
+            "status": "CURRENT",
+            "updated": "2016-02-08T12:20:21Z",
+            "version": "3.27"
+        }
+    ]
+}
+`
+
+const APIGetResponse = `
+{
+    "versions": [
+        {
+            "id": "v3.0",
+            "links": [
+                {
+                    "href": "http://docs.openstack.org/",
+                    "rel": "describedby",
+                    "type": "text/html"
+                },
+                {
+                    "href": "http://localhost:8776/v3/",
+                    "rel": "self"
+                }
+            ],
+            "media-types": [
+                {
+                    "base": "application/json",
+                    "type": "application/vnd.openstack.volume+json;version=1"
+                }
+            ],
+            "min_version": "3.0",
+            "status": "CURRENT",
+            "updated": "2016-02-08T12:20:21Z",
+            "version": "3.27"
+        }
+    ]
+}
+`
+
+func MockListResponse(t *testing.T) {
+	th.Mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, APIListResponse)
+	})
+}
+
+func MockGetResponse(t *testing.T) {
+	th.Mux.HandleFunc("/v3/", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, APIGetResponse)
+	})
+}
