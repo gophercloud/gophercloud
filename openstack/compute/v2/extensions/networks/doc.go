0051332ae65c3290f6a7f02935d1a8ced4892f90
Joe Topjian <joe@topjian.net>
2017-08-13 22:51:57 -0600

    Compute v2: networks docs

--- openstack/compute/v2/extensions/networks/doc.go
+++ openstack/compute/v2/extensions/networks/doc.go
@@ -1,2 +1,24 @@
-// Package network provides the ability to manage nova-networks
+/*
+Package networks provides the ability to create and manage networks in cloud
+environments using nova-network.
+
+This package can also be used to retrieve network details of Neutron-based
+networks.
+
+Example to List Networks
+
+	allPages, err := networks.List(computeClient).AllPages()
+	if err != nil {
+		panic("Unable to retrieve networks: %s", err)
+	}
+
+	allNetworks, err := networks.ExtractNetworks(allPages)
+	if err != nil {
+		panic("Unable to extract networks: %s", err)
+	}
+
+	for _, network := range allNetworks {
+		fmt.Println("%+v\n", network)
+	}
+*/
 package networks
