d92aae12eafeb66b18c8070a976f46431f1ce2ef
Joe Topjian <joe@topjian.net>
2017-08-13 20:07:09 -0600

    Compute v2: hypervisors docs

--- openstack/compute/v2/extensions/hypervisors/doc.go
+++ openstack/compute/v2/extensions/hypervisors/doc.go
@@ -1,3 +1,21 @@
-// Package hypervisors gives information and control of the os-hypervisors
-// portion of the compute API
+/*
+Package hypervisors returns details about the hypervisors in the OpenStack
+cloud.
+
+Example of Retrieving Details of All Hypervisors
+
+	allPages, err := hypervisors.List(computeClient).AllPages()
+	if err != nil {
+		panic("Unable to retrieve hypervisors: %s", err)
+	}
+
+	allHypervisors, err := hypervisors.ExtractHypervisors(allPages)
+	if err != nil {
+		panic("Unable to extract hypervisors: %s", err)
+	}
+
+	for _, hypervisor := range allHypervisors {
+		fmt.Println("%+v\n", hypervisor)
+	}
+*/
 package hypervisors
