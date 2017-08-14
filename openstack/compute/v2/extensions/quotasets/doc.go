88e9712a999a4b5ca6f591b5d58f3755e7b6cbc7
Joe Topjian <joe@topjian.net>
2017-08-13 23:17:37 -0600

    Compute v2: quotasets docs

--- openstack/compute/v2/extensions/quotasets/doc.go
+++ openstack/compute/v2/extensions/quotasets/doc.go
@@ -1,3 +1,27 @@
-// Package quotasets provides information and interaction with QuotaSet
-// extension for the OpenStack Compute service.
+/*
+Package quotasets enables retrieving and managing Compute quotas.
+
+Example to Get a Quota Set
+
+	quotaset, err := quotasets.Get(computeClient, "tenant-id").Extract()
+	if err != nil {
+		panic("Unable to retrieve quotaset: %s", err)
+	}
+
+	fmt.Println("%+v\n", quotaset)
+
+Example to Update a Quota Set
+
+	updateOpts := quotasets.UpdateOpts{
+		FixedIPs: gophercloud.IntToPointer(100),
+		Cores:    gophercloud.IntToPointer(64),
+	}
+
+	quotaset, err := quotasets.Update(computeClient, "tenant-id", updateOpts).Extract()
+	if err != nil {
+		panic("Unable to update quotaset: %s", err)
+	}
+
+	fmt.Println("%+v\n", quotaset)
+*/
 package quotasets
