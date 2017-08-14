cba23774aa317494de40d44658ed36d76bfced5f
Joe Topjian <joe@topjian.net>
2017-08-13 22:45:03 -0600

    Compute v2: limits docs

---
+++ openstack/compute/v2/extensions/limits/doc.go
@@ -0,0 +1,17 @@
+/*
+Package limits shows rate and limit information for a tenant/project.
+
+Example to Retrieve Limits for a Tenant
+
+	getOpts := limits.GetOpts{
+		TenantID: "tenant-id",
+	}
+
+	limits, err := limits.Get(computeClient, getOpts).Extract()
+	if err != nil {
+		panic("Unable to retrieve limits: %s", err)
+	}
+
+	fmt.Println("%+v\n", limits)
+*/
+package limits
