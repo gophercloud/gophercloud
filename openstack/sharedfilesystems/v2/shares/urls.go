b38d5d9e82cb4d528ef96c0a18df908a66578731
Pavel Pospisil <pospispa@users.noreply.github.com>
2017-08-15 03:44:37 +0200

    Shared Filesystems: Grant Access (#346)
    * Shared File Systems: Grant Access
    
    By default shares are inaccessible because they do not have set any access permissions.
    
    That's why the Grant Access request is added.
    
    * Added unit tests for POST Grant Access request.
    
    * Fixes for the first round of code reviews.
    
    * Fixes for the second round of code reviews.
    
    * Fixes for the third round of code reviews.
    
    * Fixes for the fourth round of code reviews.
    
    * Fixes for the fifth round of code reviews.

--- openstack/sharedfilesystems/v2/shares/urls.go
+++ openstack/sharedfilesystems/v2/shares/urls.go
@@ -17,3 +17,7 @@ func getURL(c *gophercloud.ServiceClient, id string) string {
 func getExportLocationsURL(c *gophercloud.ServiceClient, id string) string {
 	return c.ServiceURL("shares", id, "export_locations")
 }
+
+func grantAccessURL(c *gophercloud.ServiceClient, id string) string {
+	return c.ServiceURL("shares", id, "action")
+}
