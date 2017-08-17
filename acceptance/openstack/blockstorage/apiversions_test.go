a1296cc47345fe166b0cd4eb453489c505bd749c
Joe Topjian <joe@topjian.net>
2017-08-16 20:10:48 -0600

    BlockStorage: Add acceptance test for apiversions

---
+++ acceptance/openstack/blockstorage/apiversions_test.go
@@ -0,0 +1,46 @@
+// +build acceptance blockstorage
+
+package blockstorage
+
+import (
+	"testing"
+
+	"github.com/gophercloud/gophercloud/acceptance/clients"
+	"github.com/gophercloud/gophercloud/acceptance/tools"
+	"github.com/gophercloud/gophercloud/openstack/blockstorage/apiversions"
+)
+
+func TestAPIVersionsList(t *testing.T) {
+	client, err := clients.NewBlockStorageV2Client()
+	if err != nil {
+		t.Fatalf("Unable to create a blockstorage client: %v", err)
+	}
+
+	allPages, err := apiversions.List(client).AllPages()
+	if err != nil {
+		t.Fatalf("Unable to retrieve API versions: %v", err)
+	}
+
+	allVersions, err := apiversions.ExtractAPIVersions(allPages)
+	if err != nil {
+		t.Fatalf("Unable to extract API versions: %v", err)
+	}
+
+	for _, v := range allVersions {
+		tools.PrintResource(t, v)
+	}
+}
+
+func TestAPIVersionsGet(t *testing.T) {
+	client, err := clients.NewBlockStorageV2Client()
+	if err != nil {
+		t.Fatalf("Unable to create a blockstorage client: %v", err)
+	}
+
+	v, err := apiversions.Get(client, "v2").Extract()
+	if err != nil {
+		t.Fatalf("Unable to retrieve API version: %v", err)
+	}
+
+	tools.PrintResource(t, v)
+}
