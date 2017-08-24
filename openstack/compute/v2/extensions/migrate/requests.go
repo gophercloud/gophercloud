8d5685e9dbce0f17f370f022031bd3f8c3766ac4
Joe Topjian <joe@topjian.net>
2017-08-23 18:44:10 -0600

    Compute v2: Use MigrateResult for the Migrate response

--- openstack/compute/v2/extensions/migrate/requests.go
+++ openstack/compute/v2/extensions/migrate/requests.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Migrate will initiate a migration of the instance to another host.
-func Migrate(client *gophercloud.ServiceClient, id string) (r gophercloud.ErrResult) {
+func Migrate(client *gophercloud.ServiceClient, id string) (r MigrateResult) {
 	_, r.Err = client.Post(actionURL(client, id), map[string]interface{}{"migrate": nil}, nil, nil)
 	return
 }
