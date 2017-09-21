405c461d7e4af48ae1a884d9d755f6e1f86cc07c
phionah bugosi <bugosip@gmail.com>
2017-09-21 18:51:28 +0300

    Compute v2: Add Lock/Unlock Action relevant error return type

--- openstack/compute/v2/extensions/lockunlock/requests.go
+++ openstack/compute/v2/extensions/lockunlock/requests.go
@@ -7,13 +7,13 @@ func actionURL(client *gophercloud.ServiceClient, id string) string {
 }
 
 // Lock is the operation responsible for locking a Compute server.
-func Lock(client *gophercloud.ServiceClient, id string) (r gophercloud.ErrResult) {
+func Lock(client *gophercloud.ServiceClient, id string) (r LockResult) {
 	_, r.Err = client.Post(actionURL(client, id), map[string]interface{}{"lock": nil}, nil, nil)
 	return
 }
 
 // Unlock is the operation responsible for unlocking a Compute server.
-func Unlock(client *gophercloud.ServiceClient, id string) (r gophercloud.ErrResult) {
+func Unlock(client *gophercloud.ServiceClient, id string) (r UnlockResult) {
 	_, r.Err = client.Post(actionURL(client, id), map[string]interface{}{"unlock": nil}, nil, nil)
 	return
 }
