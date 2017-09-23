e05eb28a67270889404c8b1c2a1557e88467d6a6
Joe Topjian <joe@topjian.net>
2017-09-23 17:10:54 +0000

    Compute v2: lockunlock doc fix

--- openstack/compute/v2/extensions/lockunlock/results.go
+++ openstack/compute/v2/extensions/lockunlock/results.go
@@ -4,8 +4,9 @@ import (
 	"github.com/gophercloud/gophercloud"
 )
 
-// LockResult and UnlockResult are the responses from a Lock and Unlock operations respectively. Call its ExtractErr
-// method to determine if the suceeded or failed.
+// LockResult and UnlockResult are the responses from a Lock and Unlock
+// operations respectively. Call their ExtractErr methods to determine if the
+// requests suceeded or failed.
 type LockResult struct {
 	gophercloud.ErrResult
 }
