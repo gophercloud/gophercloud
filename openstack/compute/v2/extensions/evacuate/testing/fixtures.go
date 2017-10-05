245245121df5bf35894d343ad2ab49909aaab725
Phionah Bugosi <bugosip@gmail.com>
2017-10-05 10:25:56 +0300

    Compute V2: Add evacuate Action

--- openstack/compute/v2/extensions/evacuate/testing/fixtures.go
+++ openstack/compute/v2/extensions/evacuate/testing/fixtures.go
@@ -8,11 +8,18 @@ import (
 	"github.com/gophercloud/gophercloud/testhelper/client"
 )
 
-func mockMigrateResponse(t *testing.T, id string) {
+func mockEvacuateResponse(t *testing.T, id string) {
 	th.Mux.HandleFunc("/servers/"+id+"/action", func(w http.ResponseWriter, r *http.Request) {
 		th.TestMethod(t, r, "POST")
 		th.TestHeader(t, r, "X-Auth-Token", client.TokenID)
-		th.TestJSONRequest(t, r, `{"evacuate": null}`)
+		th.TestJSONRequest(t, r, `
+		{
+		    "evacuate": {
+		    	"host": "derp",
+		        "adminPass": "false"
+		    }
+		}
+		      `)
 		w.WriteHeader(http.StatusAccepted)
 	})
 }
--- openstack/compute/v2/extensions/evacuate/testing/requests_test.go
+++ openstack/compute/v2/extensions/evacuate/testing/requests_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/evacuate"
 	th "github.com/gophercloud/gophercloud/testhelper"
 	"github.com/gophercloud/gophercloud/testhelper/client"
+
 )
 
 const serverID = "b16ba811-199d-4ffd-8839-ba96c1185a67"
@@ -14,8 +15,13 @@ func TestEvacuate(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
 
-	mockMigrateResponse(t, serverID)
+	mockEvacuateResponse(t, serverID)
 
-	err := evacuate.Evacuate(client.ServiceClient(), serverID).ExtractErr()
-	th.AssertNoErr(t, err)
+	err := evacuate.Evacuate(client.ServiceClient(), serverID, evacuate.EvacuateOpts{
+		Host:					"derp",
+		AdminPass: 				"false",
+	}).ExtractErr()
+	if err != nil && err.Error() != "EOF" {
+		t.Fatalf("Unable to evacuate to server: %s", err)
+	}
 }
