3e45e3e09877fb2d6b6794a6d1ac55e7ba1f5b64
David Lyle <dklyle0@gmail.com>
2017-10-10 10:47:20 -0600

    Identity v3: group delete

--- openstack/identity/v3/groups/urls.go
+++ openstack/identity/v3/groups/urls.go
@@ -13,3 +13,7 @@ func getURL(client *gophercloud.ServiceClient, groupID string) string {
 func createURL(client *gophercloud.ServiceClient) string {
 	return client.ServiceURL("groups")
 }
+
+func deleteURL(client *gophercloud.ServiceClient, groupID string) string {
+	return client.ServiceURL("groups", groupID)
+}
