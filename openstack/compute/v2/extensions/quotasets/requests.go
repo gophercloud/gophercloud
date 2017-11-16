4d3eab63fa133682899ec72690bed886ad6fc033
Olivier Gagnon <oli.gagnon4418@gmail.com>
2017-11-16 10:51:36 -0500

    fixed details url

--- openstack/compute/v2/extensions/quotasets/requests.go
+++ openstack/compute/v2/extensions/quotasets/requests.go
@@ -9,14 +9,14 @@ import (
 // Get returns public data about a previously created QuotaSet.
 func Get(client *gophercloud.ServiceClient, tenantID string) GetResult {
 	var res GetResult
-	_, res.Err = client.Get(getDetailURL(client, tenantID), &res.Body, nil)
+	_, res.Err = client.Get(getURL(client, tenantID), &res.Body, nil)
 	return res
 }
 
 // GetDetail returns detailed public data about a previously created QuotaSet.
 func GetDetail(client *gophercloud.ServiceClient, tenantID string) GetDetailResult {
 	var res GetDetailResult
-	_, res.Err = client.Get(getDetailURL(client, fmt.Sprintf("%s/detail", tenantID)), &res.Body, nil)
+	_, res.Err = client.Get(getDetailURL(client, tenantID), &res.Body, nil)
 	return res
 }
 
--- openstack/compute/v2/extensions/quotasets/urls.go
+++ openstack/compute/v2/extensions/quotasets/urls.go
@@ -8,14 +8,18 @@ func resourceURL(c *gophercloud.ServiceClient) string {
 	return c.ServiceURL(resourcePath)
 }
 
-func getDetailURL(c *gophercloud.ServiceClient, tenantID string) string {
+func getURL(c *gophercloud.ServiceClient, tenantID string) string {
 	return c.ServiceURL(resourcePath, tenantID)
 }
 
+func getDetailURL(c *gophercloud.ServiceClient, tenantID string) string {
+	return c.ServiceURL(resourcePath, tenantID, "details")
+}
+
 func updateURL(c *gophercloud.ServiceClient, tenantID string) string {
-	return getDetailURL(c, tenantID)
+	return getURL(c, tenantID)
 }
 
 func deleteURL(c *gophercloud.ServiceClient, tenantID string) string {
-	return getDetailURL(c, tenantID)
+	return getURL(c, tenantID)
 }
