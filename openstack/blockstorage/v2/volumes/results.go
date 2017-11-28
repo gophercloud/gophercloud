ca9f9646489cb83d0c5e0b218b538eb352f13c1e
TommyLike <tommylikehu@gmail.com>
2017-11-28 16:01:59 +0800

    Supprt extend volume in v2/v3 APIs
    Add extending volume support in v2/v3 APIs

--- openstack/blockstorage/v2/volumes/results.go
+++ openstack/blockstorage/v2/volumes/results.go
@@ -152,3 +152,8 @@ type UpdateResult struct {
 type DeleteResult struct {
 	gophercloud.ErrResult
 }
+
+// ActionResult contains the response body and error from a Action request.
+type ActionResult struct {
+	gophercloud.ErrResult
+}
