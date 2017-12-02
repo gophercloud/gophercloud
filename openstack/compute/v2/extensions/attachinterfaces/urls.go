ccb42439e679bf8196995ea00feaa196487455be
Andrei Ozerov <ozerov@selectel.ru>
2017-12-02 16:05:08 +0300

    Add Nova interface-detach support
    Add support to detach interface from a server through a DELETE on: /v2.1/{tenant_id}/servers/{server_id}/os-interface/{port_id}
    
    The same operation with the OpenStack CLI is done with:
      nova interface-detach <server_id> <port_id>

--- openstack/compute/v2/extensions/attachinterfaces/urls.go
+++ openstack/compute/v2/extensions/attachinterfaces/urls.go
@@ -13,3 +13,6 @@ func getInterfaceURL(client *gophercloud.ServiceClient, serverID, portID string)
 func createInterfaceURL(client *gophercloud.ServiceClient, serverID string) string {
 	return client.ServiceURL("servers", serverID, "os-interface")
 }
+func deleteInterfaceURL(client *gophercloud.ServiceClient, serverID, portID string) string {
+	return client.ServiceURL("servers", serverID, "os-interface", portID)
+}
