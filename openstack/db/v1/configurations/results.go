731dbfe6ecc4c704b58f5cc1e41ba0b9ae35cdaf
Eugene Taranov <eugene@taranov.me>
2017-12-05 00:38:08 +0300

    fix

--- openstack/db/v1/configurations/results.go
+++ openstack/db/v1/configurations/results.go
@@ -1,21 +1,43 @@
 package configurations
 
 import (
+	"encoding/json"
+	"time"
+
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/pagination"
 )
 
 // Config represents a configuration group API resource.
 type Config struct {
-	Created              string `json:"created"`
-	Updated              string `json:"updated"`
-	DatastoreName        string `json:"datastore_name"`
-	DatastoreVersionID   string `json:"datastore_version_id"`
-	DatastoreVersionName string `json:"datastore_version_name"`
+	Created              time.Time `json:"created"`
+	Updated              time.Time `json:"updated"`
+	DatastoreName        string    `json:"datastore_name"`
+	DatastoreVersionID   string    `json:"datastore_version_id"`
+	DatastoreVersionName string    `json:"datastore_version_name"`
 	Description          string
 	ID                   string
 	Name                 string
 	Values               map[string]interface{}
+}
+
+func (r *Config) UnmarshalJSON(b []byte) error {
+	type tmp Config
+	var s struct {
+		tmp
+		Created gophercloud.JSONRFC3339NoZ `json:"created"`
+		Updated gophercloud.JSONRFC3339NoZ `json:"updated"`
+	}
+	err := json.Unmarshal(b, &s)
+	if err != nil {
+		return err
+	}
+	*r = Config(s.tmp)
+
+	r.Created = time.Time(s.Created)
+	r.Updated = time.Time(s.Updated)
+
+	return nil
 }
 
 // ConfigPage contains a page of Config resources in a paginated collection.
