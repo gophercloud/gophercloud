6d9bf8c02219fd13d1c5b4079b74390cd718eef6
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-01-01 00:25:02 +0300

    Add Neutron subnetpool list support. (#694)
    * Add Neutron subnetpool list support.
    Add support to list Neutron subnetpools through a GET request on /v2.0/subnetpools
    
    The same command with OpenStack CLI is done with:
    openstack subnet pool list
    
    or
    
    neutron subnetpool-list
    
    * Add Neutron subnetpools list acceptance test
    
    * Update subnetpools acceptance test build options
    
    Add extension name to the subnetpools acceptance test.
    
    * Add custom UnmarshalJSON to subnetpools
    
    Neutron API can return string type in following subnetpool's fields:
    
    - default_prefixlen
    - min_prefixlen
    - max_prefixlen
    
    But it should be integer based on the API reference.
    We need to be sure that Gophercloud can work with
    both types in those fields.
    
    * Update subnetpool test fixtures
    
    Some fields in the SubnetPoolsListResult should be a string:
    
    - default_prefixlen
    - max_prefixlen
    - min_prefixlen
    
    * Refactor subnetpools test fixture
    
    Use spaces for indentation inside the SubnetPoolsListResult instead of hard tabs.

---
+++ acceptance/openstack/networking/v2/extensions/subnetpools/subnetpools_test.go
@@ -0,0 +1,32 @@
+// +build acceptance networking subnetpools
+
+package v2
+
+import (
+	"testing"
+
+	"github.com/gophercloud/gophercloud/acceptance/clients"
+	"github.com/gophercloud/gophercloud/acceptance/tools"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/subnetpools"
+)
+
+func TestSubnetPoolsList(t *testing.T) {
+	client, err := clients.NewNetworkV2Client()
+	if err != nil {
+		t.Fatalf("Unable to create a network client: %v", err)
+	}
+
+	allPages, err := subnetpools.List(client, nil).AllPages()
+	if err != nil {
+		t.Fatalf("Unable to list subnetpools: %v", err)
+	}
+
+	allSubnetPools, err := subnetpools.ExtractSubnetPools(allPages)
+	if err != nil {
+		t.Fatalf("Unable to extract subnetpools: %v", err)
+	}
+
+	for _, subnetpool := range allSubnetPools {
+		tools.PrintResource(t, subnetpool)
+	}
+}
