59c00d486aff1390fa975ea8f2e8b7b55f5621f3
Olivier Gagnon <oli.gagnon4418@gmail.com>
2018-01-10 11:10:34 -0500

    updated usage/doc.go to reflect package name

--- openstack/compute/v2/extensions/usage/doc.go
+++ openstack/compute/v2/extensions/usage/doc.go
@@ -1,22 +1,22 @@
 /*
-Package simpletenantusage provides information and interaction with the
+Package usage provides information and interaction with the
 SimpleTenantUsage extension for the OpenStack Compute service.
 
 Example to Retrieve Usage for a Single Tenant:
 	start := time.Date(2017, 01, 21, 10, 4, 20, 0, time.UTC)
 	end := time.Date(2017, 01, 21, 10, 4, 20, 0, time.UTC)
 
-    singleTenantOpts := simpletenantusage.SingleTenantOpts{
+    singleTenantOpts := usage.SingleTenantOpts{
         Start: &start,
         End: &end,
     }
 
-    page, err := simpletenantusage.SingleTenant(computeClient, tenantID, singleTenantOpts).AllPages()
+    page, err := usage.SingleTenant(computeClient, tenantID, singleTenantOpts).AllPages()
     if err != nil {
         panic(err)
     }
 
-    usage, err := simpletenantusage.ExtractSingleTenant(page)
+    usage, err := usage.ExtractSingleTenant(page)
     if err != nil {
         panic(err)
     }
