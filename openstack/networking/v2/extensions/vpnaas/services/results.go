c14e67810e2c5a9eaf5dbdf3a530647c0f06e41c
sreinkemeier <sreinkemeier@inovex.de>
2018-02-08 19:34:59 +0100

    Added results file for create method

---
+++ openstack/networking/v2/extensions/vpnaas/services/results.go
@@ -0,0 +1,33 @@
+package services
+
+import (
+	"github.com/gophercloud/gophercloud"
+)
+
+// Service is a VPN Service
+type Service struct {
+	TenantID        string  `json:"tenant_id"`
+	SubnetID        string  `json:"subnet_id,omitempty"`
+	RouterID        string  `json:"router_id"`
+	Description     string  `json:"description,omitempty"`
+	AdminStateUp    *bool   `json:"admin_state_up"`
+	ProjectID       string  `json:"project_id"`
+	Name            string  `json:"name,omitempty"`
+	Status          string  `json:"status"`
+	ID              string  `json:"id"`
+	ExternalV6IP	string 	`json:"external_v6_ip"`
+	ExternalV4IP	string 	`json:"external_v4_ip"`
+	FlavorID		string 	`json:"flavor_id"`
+}
+
+type commonResult struct {
+	gophercloud.Result
+}
+
+// CreateResult represents the result of a create operation. Call its Extract
+// method to interpret it as a Service.
+type CreateResult struct {
+	commonResult
+}
+
+
