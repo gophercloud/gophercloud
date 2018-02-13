5289e7f86736b5565c0ac6b206ab879845d5bb5c
Kevin Zhao <kevinzs2048@gmail.com>
2018-02-13 09:47:36 -0600

    Add support for Zun experimental API "Capsule"Get (#725)
    * Add support for Zun experimental API "Capsule" get
    
    Also add the test case and acceptance test.
    
    Signed-off-by: Kevin Zhao <kevinzs2048@gmail.com>
    
    * Add SubnetID to Addresses
    
    Signed-off-by: Kevin Zhao <kevin.zhaoshuai@gmail.com>

--- acceptance/clients/clients.go
+++ acceptance/clients/clients.go
@@ -424,3 +424,22 @@ func NewSharedFileSystemV2Client() (*gophercloud.ServiceClient, error) {
 		Region: os.Getenv("OS_REGION_NAME"),
 	})
 }
+
+// NewContainerExperimentalClient returns a *ServiceClient for making calls
+// to the OpenStack Container Experimental API. An error will be returned
+// if authentication or client creation was not possible.
+func NewContainerExperimentalClient() (*gophercloud.ServiceClient, error) {
+	ao, err := openstack.AuthOptionsFromEnv()
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := openstack.AuthenticatedClient(ao)
+	if err != nil {
+		return nil, err
+	}
+
+	return openstack.NewContainerExperimental(client, gophercloud.EndpointOpts{
+		Region: os.Getenv("OS_REGION_NAME"),
+	})
+}
---
+++ acceptance/openstack/container/experimental/capsules_test.go
@@ -0,0 +1,28 @@
+package experimental
+
+import (
+	"testing"
+
+	"github.com/gophercloud/gophercloud/acceptance/clients"
+	"github.com/gophercloud/gophercloud/openstack/container/experimental/capsules"
+	th "github.com/gophercloud/gophercloud/testhelper"
+)
+
+func TestCapsuleGet(t *testing.T) {
+	client, err := clients.NewContainerExperimentalClient()
+	if err != nil {
+		t.Fatalf("Unable to create an container experimental client: %v", err)
+	}
+	th.AssertNoErr(t, err)
+	capsuleUUID := "e6c913bb-b4e4-409d-8b71-3e029f196458"
+	if capsuleUUID == "" {
+		t.Fatalf("In order to retrieve a capsule, the CapsuleUUID must be set")
+	}
+	capsule, err := capsules.Get(client, capsuleUUID).Extract()
+	// Get a capsule
+
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, capsule.Status, "Running")
+	th.AssertEquals(t, capsule.MetaName, "template")
+	th.AssertEquals(t, capsule.CPU, float64(2.0))
+}
