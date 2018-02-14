29371273097f31bf5d3282f75195b9bcd227b6fd
sreinkemeier <sreinkemeier@inovex.de>
2018-02-14 13:59:34 +0100

    formatting of imports for tests

--- openstack/networking/v2/extensions/vpnaas/services/testing/requests_test.go
+++ openstack/networking/v2/extensions/vpnaas/services/testing/requests_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"testing"
-	
+
 	"github.com/gophercloud/gophercloud"
 	fake "github.com/gophercloud/gophercloud/openstack/networking/v2/common"
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/vpnaas/services"
