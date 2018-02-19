d0e111f814fa188e136c9ed10caf2d28451c3d2e
sreinkemeier <sreinkemeier@inovex.de>
2018-02-19 14:01:45 +0100

    Added list request

--- openstack/networking/v2/extensions/vpnaas/ipsecpolicies/requests.go
+++ openstack/networking/v2/extensions/vpnaas/ipsecpolicies/requests.go
@@ -1,6 +1,9 @@
 package ipsecpolicies
 
-import "github.com/gophercloud/gophercloud"
+import (
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/pagination"
+)
 
 type TransformProtocol string
 type AuthAlgorithm string
@@ -120,4 +123,50 @@ func Delete(c *gophercloud.ServiceClient, id string) (r DeleteResult) {
 func Get(c *gophercloud.ServiceClient, id string) (r GetResult) {
 	_, r.Err = c.Get(resourceURL(c, id), &r.Body, nil)
 	return
+}
+
+// ListOptsBuilder allows extensions to add additional parameters to the
+// List request.
+type ListOptsBuilder interface {
+	ToPolicyListQuery() (string, error)
+}
+
+// ListOpts allows the filtering of paginated collections through
+// the API. Filtering is achieved by passing in struct field values that map to
+// the IPSec policy attributes you want to see returned.
+type ListOpts struct {
+	ID                  string   `q:"id"`
+	TenantID            string   `q:"tenant_id"`
+	Name                string   `q:"name"`
+	Description         string   `q:"description"`
+	ProjectID           string   `q:"project_id"`
+	AuthAlgorithm       string   `q:"auth_algorithm"`
+	EncapsulationMode   string   `q:"encapsulation_mode"`
+	EncryptionAlgorithm string   `q:"encryption_algorithm"`
+	PFS                 string   `q:"pfs"`
+	Lifetime            Lifetime `q:"lifetime"`
+	TransformProtocol   string   `q:"transform_protocol"`
+}
+
+// ToPolicyListQuery formats a ListOpts into a query string.
+func (opts ListOpts) ToPolicyListQuery() (string, error) {
+	q, err := gophercloud.BuildQueryString(opts)
+	return q.String(), err
+}
+
+// List returns a Pager which allows you to iterate over a collection of
+// IPSec policies. It accepts a ListOpts struct, which allows you to filter
+// the returned collection for greater efficiency.
+func List(c *gophercloud.ServiceClient, opts ListOptsBuilder) pagination.Pager {
+	url := rootURL(c)
+	if opts != nil {
+		query, err := opts.ToPolicyListQuery()
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		url += query
+	}
+	return pagination.NewPager(c, url, func(r pagination.PageResult) pagination.Page {
+		return PolicyPage{pagination.LinkedPageBase{PageResult: r}}
+	})
 }
