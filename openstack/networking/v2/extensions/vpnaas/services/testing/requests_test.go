a3a5eb426a650a1184b3964e2c5c6c78a03021e7
Simon Reinkemeier <simonre@users.noreply.github.com>
2018-02-28 17:36:07 +0100

    Vpnaas: Service Get/List (#771)
    * unit test for List and Get
    
    * request functions for list and get added to requests.go
    
    * edited comment for accuracy
    
    * Added relevant parts for service listing to results.go
    
    * Made test compare structs instead of values
    
    * Added acceptance test
    
    * Added documentation in doc.go file
    
    * Added missing parameters
    
    * Added project_id as a field to result struct
    
    * removed id field from ListOpts

--- openstack/networking/v2/extensions/vpnaas/services/testing/requests_test.go
+++ openstack/networking/v2/extensions/vpnaas/services/testing/requests_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gophercloud/gophercloud"
 	fake "github.com/gophercloud/gophercloud/openstack/networking/v2/common"
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/vpnaas/services"
+	"github.com/gophercloud/gophercloud/pagination"
 	th "github.com/gophercloud/gophercloud/testhelper"
 )
 
@@ -46,7 +47,8 @@ func TestCreate(t *testing.T) {
         "tenant_id": "10039663455a446d8ba2cbb058b0f578",
         "external_v4_ip": "172.32.1.11",
         "id": "5c561d9d-eaea-45f6-ae3e-08d1a7080828",
-        "description": "OpenStack VPN service"
+        "description": "OpenStack VPN service",
+		"project_id": "10039663455a446d8ba2cbb058b0f578"
     }
 }
     `)
@@ -69,9 +71,116 @@ func TestCreate(t *testing.T) {
 		AdminStateUp: true,
 		SubnetID:     "",
 		TenantID:     "10039663455a446d8ba2cbb058b0f578",
+		ProjectID:    "10039663455a446d8ba2cbb058b0f578",
 		ExternalV4IP: "172.32.1.11",
 		ID:           "5c561d9d-eaea-45f6-ae3e-08d1a7080828",
 		Description:  "OpenStack VPN service",
+	}
+	th.AssertDeepEquals(t, expected, *actual)
+}
+
+func TestList(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/vpn/vpnservices", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, `
+{
+   "vpnservices":[
+        {
+            "router_id": "66e3b16c-8ce5-40fb-bb49-ab6d8dc3f2aa",
+            "status": "PENDING_CREATE",
+            "name": "vpnservice1",
+            "admin_state_up": true,
+            "subnet_id": null,
+            "project_id": "10039663455a446d8ba2cbb058b0f578",
+            "tenant_id": "10039663455a446d8ba2cbb058b0f578",
+            "description": "Test VPN service"
+        }
+   ]
+}
+      `)
+	})
+
+	count := 0
+
+	services.List(fake.ServiceClient(), services.ListOpts{}).EachPage(func(page pagination.Page) (bool, error) {
+		count++
+		actual, err := services.ExtractServices(page)
+		if err != nil {
+			t.Errorf("Failed to extract members: %v", err)
+			return false, err
+		}
+
+		expected := []services.Service{
+			{
+				Status:       "PENDING_CREATE",
+				Name:         "vpnservice1",
+				AdminStateUp: true,
+				TenantID:     "10039663455a446d8ba2cbb058b0f578",
+				ProjectID:    "10039663455a446d8ba2cbb058b0f578",
+				Description:  "Test VPN service",
+				SubnetID:     "",
+				RouterID:     "66e3b16c-8ce5-40fb-bb49-ab6d8dc3f2aa",
+			},
+		}
+
+		th.CheckDeepEquals(t, expected, actual)
+
+		return true, nil
+	})
+
+	if count != 1 {
+		t.Errorf("Expected 1 page, got %d", count)
+	}
+}
+
+func TestGet(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/vpn/vpnservices/5c561d9d-eaea-45f6-ae3e-08d1a7080828", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, `
+{
+    "vpnservice": {
+        "router_id": "66e3b16c-8ce5-40fb-bb49-ab6d8dc3f2aa",
+        "status": "PENDING_CREATE",
+        "name": "vpnservice1",
+        "admin_state_up": true,
+        "subnet_id": null,
+        "project_id": "10039663455a446d8ba2cbb058b0f578",
+        "tenant_id": "10039663455a446d8ba2cbb058b0f578",
+        "id": "5c561d9d-eaea-45f6-ae3e-08d1a7080828",
+        "description": "VPN test service"
+    }
+}
+        `)
+	})
+
+	actual, err := services.Get(fake.ServiceClient(), "5c561d9d-eaea-45f6-ae3e-08d1a7080828").Extract()
+	th.AssertNoErr(t, err)
+	expected := services.Service{
+		Status:       "PENDING_CREATE",
+		Name:         "vpnservice1",
+		Description:  "VPN test service",
+		AdminStateUp: true,
+		ID:           "5c561d9d-eaea-45f6-ae3e-08d1a7080828",
+		TenantID:     "10039663455a446d8ba2cbb058b0f578",
+		ProjectID:    "10039663455a446d8ba2cbb058b0f578",
+		RouterID:     "66e3b16c-8ce5-40fb-bb49-ab6d8dc3f2aa",
+		SubnetID:     "",
 	}
 	th.AssertDeepEquals(t, expected, *actual)
 }
