352d9ee47ea2ca95e009b67695971e3fc2c73f68
sreinkemeier <sreinkemeier@inovex.de>
2018-03-06 13:51:53 +0100

    Added documentation

--- openstack/networking/v2/extensions/vpnaas/siteconnections/requests.go
+++ openstack/networking/v2/extensions/vpnaas/siteconnections/requests.go
@@ -23,33 +23,101 @@ const (
 	InitiatorResponseOnly  Initiator = "response-only"
 )
 
+// DPDCreateOpts contains all the values needed to create a valid configuration for Dead Peer detection protocols
 type DPDCreateOpts struct {
-	Action   Action `json:"action,omitempty"`
-	Timeout  int    `json:"timeout,omitempty"`
-	Interval int    `json:"interval,omitempty"`
+	// The dead peer detection (DPD) action.
+	// A valid value is clear, hold, restart, disabled, or restart-by-peer.
+	// Default value is hold.
+	Action Action `json:"action,omitempty"`
+
+	// The dead peer detection (DPD) timeout in seconds.
+	// A valid value is a positive integer that is greater than the DPD interval value.
+	// Default is 120.
+	Timeout int `json:"timeout,omitempty"`
+
+	// The dead peer detection (DPD) interval, in seconds.
+	// A valid value is a positive integer.
+	// Default is 30.
+	Interval int `json:"interval,omitempty"`
 }
 
 // CreateOpts contains all the values needed to create a new IPSec site connection
 type CreateOpts struct {
-	IKEPolicyID        string         `json:"ikepolicy_id,omitempty"`
-	VPNServiceID       string         `json:"vpnservice_id,omitempty"`
-	LocalEPGroupID     string         `json:"local_ep_group_id,omitempty"`
-	IPSecPolicyID      string         `json:"ipsecpolicy_id,omitempty"`
-	PeerID             string         `json:"peer_id"`
-	TenantID           string         `json:"tenant_id,omitempty"`
-	PeerEPGroupID      string         `json:"peer_ep_group_id,omitempty"`
-	LocalID            string         `json:"local_id,omitempty"`
-	Name               string         `json:"name,omitempty"`
-	Description        string         `json:"description,omitempty"`
-	PeerAddress        string         `json:"peer_address"`
-	RouteMode          string         `json:"route_mode,omitempty"`
-	PSK                string         `json:"psk"`
-	Initiator          Initiator      `json:"initiator,omitempty"`
-	PeerCIDRs          []string       `json:"peer_cidrs,omitempty"`
-	AdminStateUp       *bool          `json:"admin_state_up,omitempty"`
-	DPD                *DPDCreateOpts `json:"dpd,omitempty"`
-	AuthenticationMode string         `json:"auth_mode,omitempty"`
-	MTU                int            `json:"mtu,omitempty"`
+	// The ID of the IKE policy
+	IKEPolicyID string `json:"ikepolicy_id"`
+
+	// The ID of the VPN Service
+	VPNServiceID string `json:"vpnservice_id"`
+
+	// The ID for the endpoint group that contains private subnets for the local side of the connection.
+	// You must specify this parameter with the peer_ep_group_id parameter unless
+	// in backward- compatible mode where peer_cidrs is provided with a subnet_id for the VPN service.
+	LocalEPGroupID string `json:"local_ep_group_id,omitempty"`
+
+	// The ID of the IPsec policy.
+	IPSecPolicyID string `json:"ipsecpolicy_id"`
+
+	// The peer router identity for authentication.
+	// A valid value is an IPv4 address, IPv6 address, e-mail address, key ID, or FQDN.
+	// Typically, this value matches the peer_address value.
+	PeerID string `json:"peer_id"`
+
+	// The ID of the project
+	TenantID string `json:"tenant_id,omitempty"`
+
+	// The ID for the endpoint group that contains private CIDRs in the form < net_address > / < prefix >
+	// for the peer side of the connection.
+	// You must specify this parameter with the local_ep_group_id parameter unless in backward-compatible mode
+	// where peer_cidrs is provided with a subnet_id for the VPN service.
+	PeerEPGroupID string `json:"peer_ep_group_id,omitempty"`
+
+	// An ID to be used instead of the external IP address for a virtual router used in traffic between instances on different networks in east-west traffic.
+	// Most often, local ID would be domain name, email address, etc.
+	// If this is not configured then the external IP address will be used as the ID.
+	LocalID string `json:"local_id,omitempty"`
+
+	// The human readable name of the connection.
+	// Does not have to be unique.
+	// Default is an empty string
+	Name string `json:"name,omitempty"`
+
+	// The human readable description of the connection.
+	// Does not have to be unique.
+	// Default is an empty string
+	Description string `json:"description,omitempty"`
+
+	// The peer gateway public IPv4 or IPv6 address or FQDN.
+	PeerAddress string `json:"peer_address"`
+
+	// The route mode.
+	// A valid value is static, which is the default.
+	RouteMode string `json:"route_mode,omitempty"`
+
+	// The pre-shared key.
+	// A valid value is any string.
+	PSK string `json:"psk"`
+
+	// Indicates whether this VPN can only respond to connections or both respond to and initiate connections.
+	// A valid value is response-only or bi-directional. Default is bi-directional.
+	Initiator Initiator `json:"initiator,omitempty"`
+
+	// (Deprecated) Unique list of valid peer private CIDRs in the form < net_address > / < prefix > .
+	PeerCIDRs []string `json:"peer_cidrs,omitempty"`
+
+	// The administrative state of the resource, which is up (true) or down (false).
+	// Default is false
+	AdminStateUp *bool `json:"admin_state_up,omitempty"`
+
+	// A dictionary with dead peer detection (DPD) protocol controls.
+	DPD *DPDCreateOpts `json:"dpd,omitempty"`
+
+	// The authentication mode.
+	// A valid value is psk, which is the default.
+	AuthenticationMode string `json:"auth_mode,omitempty"`
+
+	// The maximum transmission unit (MTU) value to address fragmentation.
+	// Minimum value is 68 for IPv4, and 1280 for IPv6.
+	MTU int `json:"mtu,omitempty"`
 }
 
 // ToServiceCreateMap casts a CreateOpts struct to a map.
--- openstack/networking/v2/extensions/vpnaas/siteconnections/results.go
+++ openstack/networking/v2/extensions/vpnaas/siteconnections/results.go
@@ -5,35 +5,86 @@ import (
 )
 
 type DPD struct {
-	Action   string `json:"action"`
-	Timeout  int    `json:"timeout"`
-	Interval int    `json:"interval"`
+	// Action is the dead peer detection (DPD) action.
+	Action string `json:"action"`
+
+	// Timeout is the dead peer detection (DPD) timeout in seconds.
+	Timeout int `json:"timeout"`
+
+	// Interval is the dead peer detection (DPD) interval in seconds.
+	Interval int `json:"interval"`
 }
 
 // Connection is an IPSec site connection
 type Connection struct {
-	IKEPolicyID        string   `json:"ikepolicy_id"`
-	VPNServiceID       string   `json:"vpnservice_id"`
-	LocalEPGroupID     string   `json:"local_ep_group_id"`
-	IPSecPolicyID      string   `json:"ipsecpolicy_id"`
-	PeerID             string   `json:"peer_id"`
-	TenantID           string   `json:"tenant_id"`
-	PeerEPGroupID      string   `json:"peer_ep_group_id"`
-	LocalID            string   `json:"local_id"`
-	Name               string   `json:"name"`
-	Description        string   `json:"description"`
-	PeerAddress        string   `json:"peer_address"`
-	RouteMode          string   `json:"route_mode"`
-	PSK                string   `json:"psk"`
-	Initiator          string   `json:"initiator"`
-	PeerCIDRs          []string `json:"peer_cidrs"`
-	AdminStateUp       bool     `json:"admin_state_up"`
-	DPD                DPD      `json:"dpd"`
-	AuthenticationMode string   `json:"auth_mode"`
-	MTU                int      `json:"mtu"`
-	Status             string   `json:"status"`
-	ProjectID          string   `json:"project_id"`
-	ID                 string   `json:"id"`
+	// IKEPolicyID is the ID of the IKE policy.
+	IKEPolicyID string `json:"ikepolicy_id"`
+
+	// VPNServiceID is the ID of the VPN service.
+	VPNServiceID string `json:"vpnservice_id"`
+
+	// LocalEPGroupID is the ID for the endpoint group that contains private subnets for the local side of the connection.
+	LocalEPGroupID string `json:"local_ep_group_id"`
+
+	// IPSecPolicyID is the ID of the IPSec policy
+	IPSecPolicyID string `json:"ipsecpolicy_id"`
+
+	// PeerID is the peer router identity for authentication.
+	PeerID string `json:"peer_id"`
+
+	// TenantID is the ID of the project.
+	TenantID string `json:"tenant_id"`
+
+	// ProjectID is the ID of the project.
+	ProjectID string `json:"project_id"`
+
+	// PeerEPGroupID is the ID for the endpoint group that contains private CIDRs in the form < net_address > / < prefix >
+	// for the peer side of the connection.
+	PeerEPGroupID string `json:"peer_ep_group_id"`
+
+	// LocalID is an ID to be used instead of the external IP address for a virtual router used in traffic
+	// between instances on different networks in east-west traffic.
+	LocalID string `json:"local_id"`
+
+	// Name is the human readable name of the connection.
+	Name string `json:"name"`
+
+	// Description is the human readable description of the connection.
+	Description string `json:"description"`
+
+	// PeerAddress is the peer gateway public IPv4 or IPv6 address or FQDN.
+	PeerAddress string `json:"peer_address"`
+
+	// RouteMode is the route mode.
+	RouteMode string `json:"route_mode"`
+
+	// PSK is the pre-shared key.
+	PSK string `json:"psk"`
+
+	// Initiator indicates whether this VPN can only respond to connections or both respond to and initiate connections.
+	Initiator string `json:"initiator"`
+
+	// PeerCIDRs is a unique list of valid peer private CIDRs in the form < net_address > / < prefix > .
+	PeerCIDRs []string `json:"peer_cidrs"`
+
+	// AdminStateUp is the administrative state of the connection.
+	AdminStateUp bool `json:"admin_state_up"`
+
+	// DPD is the dead peer detection (DPD) protocol controls.
+	DPD DPD `json:"dpd"`
+
+	// AuthenticationMode is the authentication mode.
+	AuthenticationMode string `json:"auth_mode"`
+
+	// MTU is the maximum transmission unit (MTU) value to address fragmentation.
+	MTU int `json:"mtu"`
+
+	// Status indicates whether the IPsec connection is currently operational.
+	// Values are ACTIVE, DOWN, BUILD, ERROR, PENDING_CREATE, PENDING_UPDATE, or PENDING_DELETE.
+	Status string `json:"status"`
+
+	// ID is the id of the connection
+	ID string `json:"id"`
 }
 
 type commonResult struct {
