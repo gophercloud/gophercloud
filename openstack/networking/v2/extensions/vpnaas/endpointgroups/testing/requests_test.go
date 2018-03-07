70627a329dce8b5da1bd927dd77216de9632dc4f
sreinkemeier <sreinkemeier@inovex.de>
2018-03-07 17:28:05 +0100

    Added List function for Endpoint groups

--- openstack/networking/v2/extensions/vpnaas/endpointgroups/testing/requests_test.go
+++ openstack/networking/v2/extensions/vpnaas/endpointgroups/testing/requests_test.go
@@ -7,6 +7,7 @@ import (
 
 	fake "github.com/gophercloud/gophercloud/openstack/networking/v2/common"
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/vpnaas/endpointgroups"
+	"github.com/gophercloud/gophercloud/pagination"
 	th "github.com/gophercloud/gophercloud/testhelper"
 )
 
@@ -121,6 +122,69 @@ func TestGet(t *testing.T) {
 		Type: "cidr",
 	}
 	th.AssertDeepEquals(t, expected, *actual)
+}
+
+func TestList(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/vpn/endpoint-groups", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, `
+		{
+	"endpoint_groups": [
+		{
+        "description": "",
+        "tenant_id": "4ad57e7ce0b24fca8f12b9834d91079d",
+        "project_id": "4ad57e7ce0b24fca8f12b9834d91079d",
+        "endpoints": [
+            "10.2.0.0/24",
+            "10.3.0.0/24"
+        ],
+        "type": "cidr",
+        "id": "6ecd9cf3-ca64-46c7-863f-f2eb1b9e838a",
+        "name": "peers"
+		}
+	]
+}
+	  `)
+	})
+
+	count := 0
+
+	endpointgroups.List(fake.ServiceClient(), endpointgroups.ListOpts{}).EachPage(func(page pagination.Page) (bool, error) {
+		count++
+		actual, err := endpointgroups.ExtractEndpointGroups(page)
+		if err != nil {
+			t.Errorf("Failed to extract members: %v", err)
+			return false, err
+		}
+		expected := []endpointgroups.EndpointGroup{
+			{
+				Name:        "peers",
+				TenantID:    "4ad57e7ce0b24fca8f12b9834d91079d",
+				ProjectID:   "4ad57e7ce0b24fca8f12b9834d91079d",
+				ID:          "6ecd9cf3-ca64-46c7-863f-f2eb1b9e838a",
+				Description: "",
+				Endpoints: []string{
+					"10.2.0.0/24",
+					"10.3.0.0/24",
+				},
+				Type: "cidr",
+			},
+		}
+		th.CheckDeepEquals(t, expected, actual)
+
+		return true, nil
+	})
+
+	if count != 1 {
+		t.Errorf("Expected 1 page, got %d", count)
+	}
 }
 
 func TestDelete(t *testing.T) {
