419b1b15b366f8121a4958f01ea2c2c789092d18
sreinkemeier <sreinkemeier@inovex.de>
2018-03-13 19:42:02 +0100

    Added request and result for Get operation

--- openstack/networking/v2/extensions/vpnaas/siteconnections/requests.go
+++ openstack/networking/v2/extensions/vpnaas/siteconnections/requests.go
@@ -134,3 +134,9 @@ func Delete(c *gophercloud.ServiceClient, id string) (r DeleteResult) {
 	_, r.Err = c.Delete(resourceURL(c, id), nil)
 	return
 }
+
+// Get retrieves a particular IPSec site connection based on its unique ID.
+func Get(c *gophercloud.ServiceClient, id string) (r GetResult) {
+	_, r.Err = c.Get(resourceURL(c, id), &r.Body, nil)
+	return
+}
