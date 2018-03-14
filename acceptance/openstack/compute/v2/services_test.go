7cbf3e965bbde6b14f4440602ff81af47a6aa5a1
Joe Topjian <joe@topjian.net>
2018-03-14 01:49:41 +0000

    Acc Compute v2: Updating services tests

--- acceptance/openstack/compute/v2/services_test.go
+++ acceptance/openstack/compute/v2/services_test.go
@@ -8,9 +8,12 @@ import (
 	"github.com/gophercloud/gophercloud/acceptance/clients"
 	"github.com/gophercloud/gophercloud/acceptance/tools"
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/services"
+	th "github.com/gophercloud/gophercloud/testhelper"
 )
 
 func TestServicesList(t *testing.T) {
+	clients.RequireAdmin(t)
+
 	client, err := clients.NewComputeV2Client()
 	if err != nil {
 		t.Fatalf("Unable to create a compute client: %v", err)
@@ -26,7 +29,14 @@ func TestServicesList(t *testing.T) {
 		t.Fatalf("Unable to extract services")
 	}
 
+	var found bool
 	for _, service := range allServices {
 		tools.PrintResource(t, service)
+
+		if service.Binary == "nova-scheduler" {
+			found = true
+		}
 	}
+
+	th.AssertEquals(t, found, true)
 }
