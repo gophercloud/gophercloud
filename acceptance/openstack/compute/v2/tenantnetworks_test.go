3fcb865f46b8dfd521565255af931aaf92063a76
Joe Topjian <joe@topjian.net>
2018-03-14 01:52:23 +0000

    Acc Compute v2: Updating tenantnetworks tests

--- acceptance/openstack/compute/v2/tenantnetworks_test.go
+++ acceptance/openstack/compute/v2/tenantnetworks_test.go
@@ -8,9 +8,15 @@ import (
 	"github.com/gophercloud/gophercloud/acceptance/clients"
 	"github.com/gophercloud/gophercloud/acceptance/tools"
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/tenantnetworks"
+	th "github.com/gophercloud/gophercloud/testhelper"
 )
 
 func TestTenantNetworksList(t *testing.T) {
+	choices, err := clients.AcceptanceTestChoicesFromEnv()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	client, err := clients.NewComputeV2Client()
 	if err != nil {
 		t.Fatalf("Unable to create a compute client: %v", err)
@@ -26,9 +32,16 @@ func TestTenantNetworksList(t *testing.T) {
 		t.Fatalf("Unable to list networks: %v", err)
 	}
 
+	var found bool
 	for _, network := range allTenantNetworks {
 		tools.PrintResource(t, network)
+
+		if network.Name == choices.NetworkName {
+			found = true
+		}
 	}
+
+	th.AssertEquals(t, found, true)
 }
 
 func TestTenantNetworksGet(t *testing.T) {
