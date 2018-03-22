912aaaaadcd4db92e0e62e13b7edbe0462497132
sreinkemeier <sreinkemeier@inovex.de>
2018-03-22 15:53:16 +0100

    Added list function for site connections

--- openstack/networking/v2/extensions/vpnaas/siteconnections/requests.go
+++ openstack/networking/v2/extensions/vpnaas/siteconnections/requests.go
@@ -2,6 +2,7 @@ package siteconnections
 
 import (
 	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/pagination"
 )
 
 // CreateOptsBuilder allows extensions to add additional parameters to the
@@ -111,7 +112,7 @@ type CreateOpts struct {
 	MTU int `json:"mtu,omitempty"`
 }
 
-// ToServiceCreateMap casts a CreateOpts struct to a map.
+// ToConnectionCreateMap casts a CreateOpts struct to a map.
 func (opts CreateOpts) ToConnectionCreateMap() (map[string]interface{}, error) {
 	return gophercloud.BuildRequestBody(opts, "ipsec_site_connection")
 }
@@ -139,4 +140,55 @@ func Delete(c *gophercloud.ServiceClient, id string) (r DeleteResult) {
 func Get(c *gophercloud.ServiceClient, id string) (r GetResult) {
 	_, r.Err = c.Get(resourceURL(c, id), &r.Body, nil)
 	return
+}
+
+// ListOptsBuilder allows extensions to add additional parameters to the
+// List request.
+type ListOptsBuilder interface {
+	ToConnectionListQuery() (string, error)
+}
+
+// ListOpts allows the filtering and sorting of paginated collections through
+// the API. Filtering is achieved by passing in struct field values that map to
+// the IPSec site connection attributes you want to see returned.
+type ListOpts struct {
+	IKEPolicyID    string    `q:"ikepolicy_id"`
+	VPNServiceID   string    `q:"vpnservice_id"`
+	LocalEPGroupID string    `q:"local_ep_group_id"`
+	IPSecPolicyID  string    `q:"ipsecpolicy_id"`
+	PeerID         string    `q:"peer_id"`
+	TenantID       string    `q:"tenant_id"`
+	ProjectID      string    `q:"project_id"`
+	PeerEPGroupID  string    `q:"peer_ep_group_id"`
+	LocalID        string    `q:"local_id"`
+	Name           string    `q:"name"`
+	Description    string    `q:"description"`
+	PeerAddress    string    `q:"peer_address"`
+	PSK            string    `q:"psk"`
+	Initiator      Initiator `q:"initiator"`
+	AdminStateUp   *bool     `q:"admin_state_up"`
+	MTU            int       `q:"mtu"`
+}
+
+// ToConnectionListQuery formats a ListOpts into a query string.
+func (opts ListOpts) ToConnectionListQuery() (string, error) {
+	q, err := gophercloud.BuildQueryString(opts)
+	return q.String(), err
+}
+
+// List returns a Pager which allows you to iterate over a collection of
+// IPSec site connections. It accepts a ListOpts struct, which allows you to filter
+// and sort the returned collection for greater efficiency.
+func List(c *gophercloud.ServiceClient, opts ListOptsBuilder) pagination.Pager {
+	url := rootURL(c)
+	if opts != nil {
+		query, err := opts.ToConnectionListQuery()
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		url += query
+	}
+	return pagination.NewPager(c, url, func(r pagination.PageResult) pagination.Page {
+		return ConnectionPage{pagination.LinkedPageBase{PageResult: r}}
+	})
 }
--- openstack/networking/v2/extensions/vpnaas/siteconnections/results.go
+++ openstack/networking/v2/extensions/vpnaas/siteconnections/results.go
@@ -2,6 +2,7 @@ package siteconnections
 
 import (
 	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/pagination"
 )
 
 type DPD struct {
@@ -89,6 +90,43 @@ type Connection struct {
 
 type commonResult struct {
 	gophercloud.Result
+}
+
+// ConnectionPage is the page returned by a pager when traversing over a
+// collection of IPSec site connections.
+type ConnectionPage struct {
+	pagination.LinkedPageBase
+}
+
+// NextPageURL is invoked when a paginated collection of IPSec site connections has
+// reached the end of a page and the pager seeks to traverse over a new one.
+// In order to do this, it needs to construct the next page's URL.
+func (r ConnectionPage) NextPageURL() (string, error) {
+	var s struct {
+		Links []gophercloud.Link `json:"ipsec_site_connections_links"`
+	}
+	err := r.ExtractInto(&s)
+	if err != nil {
+		return "", err
+	}
+	return gophercloud.ExtractNextURL(s.Links)
+}
+
+// IsEmpty checks whether a ConnectionPage struct is empty.
+func (r ConnectionPage) IsEmpty() (bool, error) {
+	is, err := ExtractConnections(r)
+	return len(is) == 0, err
+}
+
+// ExtractConnections accepts a Page struct, specifically a Connection struct,
+// and extracts the elements into a slice of Connection structs. In other words,
+// a generic collection is mapped into a relevant slice.
+func ExtractConnections(r pagination.Page) ([]Connection, error) {
+	var s struct {
+		Connections []Connection `json:"ipsec_site_connections"`
+	}
+	err := (r.(ConnectionPage)).ExtractInto(&s)
+	return s.Connections, err
 }
 
 // Extract is a function that accepts a result and extracts an IPSec site connection.
