508ee2f967c63b3a916ae1776c31735b0b22014c
Jude Cross <jucross@blizzard.com>
2018-03-26 22:52:38 -0700

    Add create function for queues

--- openstack/messaging/v2/queues/doc.go
+++ openstack/messaging/v2/queues/doc.go
@@ -1,6 +1,10 @@
 /*
 Package queues provides information and interaction with the queues through
+<<<<<<< HEAD
 the OpenStack Messaging (Zaqar) service.
+=======
+the OpenStack Messaging(Zaqar) service.
+>>>>>>> Add create function for queues
 
 Lists all queues and creates, shows information for updates, deletes, and actions on a queue.
 
@@ -27,6 +31,7 @@ Example to List Queues
 Example to Create a Queue
 
 	createOpts := queues.CreateOpts{
+<<<<<<< HEAD
 		QueueName:                  "My_Queue",
 		MaxMessagesPostSize:        262143,
 		DefaultMessageTTL:          3700,
@@ -37,6 +42,17 @@ Example to Create a Queue
 	}
 
 	err := queues.Create(client, createOpts).ExtractErr()
+=======
+		MaxMessagesPostSize: 262143,
+		DefaultMessageTTL: 3700,
+		DefaultMessageDelay: 25,
+		DeadLetterQueueMessageTTL: 3500,
+		MaxClaimCount: 10,
+		Description: "Test queue.",
+	}
+
+	err := queues.Create(client, "My Queue", clientID, createOpts).ExtractErr()
+>>>>>>> Add create function for queues
 	if err != nil {
 		panic(err)
 	}
