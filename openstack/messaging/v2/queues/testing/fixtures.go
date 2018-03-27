97b2ae7f4093d9a6c306708c5ce581959745b44e
Jude Cross <jucross@blizzard.com>
2018-03-27 11:01:09 -0700

    Add unit tests for update queue

--- openstack/messaging/v2/queues/testing/fixtures.go
+++ openstack/messaging/v2/queues/testing/fixtures.go
@@ -43,6 +43,23 @@ const ListQueuesResponse = `
    ]
 }`
 
+// UpdateQueueRequest is a sample request to update a queue.
+const UpdateQueueRequest = `
+[
+    {
+        "op": "replace",
+        "path": "/metadata/_max_claim_count",
+        "value": 10
+    }
+]`
+
+// UpdateQueueResponse is a sample response to a update queue.
+const UpdateQueueResponse = `
+{
+    "_max_claim_count": 10
+}`
+
+
 // FirstQueue is the first result in a List.
 var FirstQueue = queues.Queue{
 	Href: "/v2/queues/beijing",
@@ -79,5 +96,18 @@ func HandleCreateSuccessfully(t *testing.T) {
 			th.TestJSONRequest(t, r, CreateQueueRequest)
 
 			w.WriteHeader(http.StatusNoContent)
+		})
+}
+
+// HandleUpdateSuccessfully configures the test server to respond to an Update request.
+func HandleUpdateSuccessfully(t *testing.T) {
+	th.Mux.HandleFunc(fmt.Sprintf("/v2/queues/%s", QueueName),
+		func(w http.ResponseWriter, r *http.Request) {
+			th.TestMethod(t, r, "PATCH")
+			th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+			th.TestJSONRequest(t, r, UpdateQueueRequest)
+
+			w.Header().Add("Content-Type", "application/json")
+			fmt.Fprintf(w, UpdateQueueResponse)
 		})
 }
