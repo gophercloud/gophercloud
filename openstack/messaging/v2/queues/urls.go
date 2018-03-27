1d74ff24504ff12eb41dfa2ab607cf1c753638b1
Jude Cross <jucross@blizzard.com>
2018-03-26 23:32:30 -0700

    Add list function for queues

--- openstack/messaging/v2/queues/urls.go
+++ openstack/messaging/v2/queues/urls.go
@@ -9,8 +9,16 @@ func commonURL(client *gophercloud.ServiceClient) string {
 	return client.ServiceURL(ApiVersion, ApiName)
 }
 
+func commonURL(client *gophercloud.ServiceClient) string {
+	return client.ServiceURL(apiVersion, apiName)
+}
+
 func createURL(client *gophercloud.ServiceClient, queueName string) string {
 	return client.ServiceURL(ApiVersion, ApiName, queueName)
+}
+
+func listURL(client *gophercloud.ServiceClient) string {
+	return commonURL(client)
 }
 
 func listURL(client *gophercloud.ServiceClient) string {
