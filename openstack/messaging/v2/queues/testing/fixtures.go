54eb4199a9376cdd8e8387cfe3d06cafa96ea87f
Jude Cross <jucross@blizzard.com>
2018-03-26 23:33:35 -0700

    Add unit tests for list queues

--- openstack/messaging/v2/queues/testing/fixtures.go
+++ openstack/messaging/v2/queues/testing/fixtures.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/gophercloud/gophercloud/openstack/messaging/v2/queues"
 	th "github.com/gophercloud/gophercloud/testhelper"
 	fake "github.com/gophercloud/gophercloud/testhelper/client"
 )
@@ -26,6 +27,48 @@ const CreateQueueRequest = `
     "_max_claim_count": 10,
     "description": "Queue for unit testing."
 }`
+
+// ListQueuesResponse is a sample response to a List queues.
+const ListQueuesResponse = `
+{
+   "queues":[
+      {
+         "href": "/v2/queues/beijing",
+         "name": "beijing"
+      },
+      {
+         "href": "/v2/queues/london",
+         "name": "london"
+      }
+   ]
+}`
+
+// FirstQueue is the first result in a List.
+var FirstQueue = queues.Queue{
+	Href: "/v2/queues/beijing",
+	Name: "beijing",
+}
+
+// SecondQueue is the second result in a List.
+var SecondQueue = queues.Queue{
+	Href: "/v2/queues/london",
+	Name: "london",
+}
+
+// ExpectedQueueSlice is the expected result in a List.
+var ExpectedQueueSlice = []queues.Queue{FirstQueue, SecondQueue}
+
+// HandleListSuccessfully configures the test server to respond to a List request.
+func HandleListSuccessfully(t *testing.T) {
+	th.Mux.HandleFunc("/v2/queues",
+		func(w http.ResponseWriter, r *http.Request) {
+			th.TestMethod(t, r, "GET")
+			th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+			w.Header().Add("Content-Type", "application/json")
+			fmt.Fprintf(w, ListQueuesResponse)
+		})
+}
 
 // HandleCreateSuccessfully configures the test server to respond to a Create request.
 func HandleCreateSuccessfully(t *testing.T) {
--- openstack/messaging/v2/queues/testing/requests_test.go
+++ openstack/messaging/v2/queues/testing/requests_test.go
@@ -4,9 +4,30 @@ import (
 	"testing"
 
 	"github.com/gophercloud/gophercloud/openstack/messaging/v2/queues"
+	"github.com/gophercloud/gophercloud/pagination"
 	th "github.com/gophercloud/gophercloud/testhelper"
 	fake "github.com/gophercloud/gophercloud/testhelper/client"
 )
+
+func TestList(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	HandleListSuccessfully(t)
+
+	listOpts := queues.ListOpts{}
+
+	count := 0
+	err := queues.List(fake.ServiceClient(), ClientID, listOpts).EachPage(func(page pagination.Page) (bool, error) {
+		count++
+		actual, err := queues.ExtractQueues(page)
+		th.AssertNoErr(t, err)
+		th.CheckDeepEquals(t, ExpectedQueueSlice, actual)
+
+		return true, nil
+	})
+	th.AssertNoErr(t, err)
+	th.CheckEquals(t, 1, count)
+}
 
 func TestCreate(t *testing.T) {
 	th.SetupHTTP()
