45fa946caa38bfbedc2280220b98d33646739a09
Jude Cross <jucross@blizzard.com>
2018-03-28 15:02:57 -0700

    Fix documentation

--- openstack/messaging/v2/queues/doc.go
+++ openstack/messaging/v2/queues/doc.go
@@ -1,21 +1,22 @@
 /*
 Package queues provides information and interaction with the queues through
-the OpenStack Messaging(Zaqar) service.
+the OpenStack Messaging (Zaqar) service.
 
 Lists all queues and creates, shows information for updates, deletes, and actions on a queue.
 
 Example to Create a Queue
 
 	createOpts := queues.CreateOpts{
-		MaxMessagesPostSize: 262143,
-		DefaultMessageTTL: 3700,
-		DefaultMessageDelay: 25,
-		DeadLetterQueueMessageTTL: 3500,
-		MaxClaimCount: 10,
-		Description: "Test queue.",
+		QueueName:					"My_Queue",
+		MaxMessagesPostSize: 		262143,
+		DefaultMessageTTL: 			3700,
+		DefaultMessageDelay: 		25,
+		DeadLetterQueueMessageTTL: 	3500,
+		MaxClaimCount: 				10,
+		Extra: 						map[string]interface{}{"description": "Test queue."},
 	}
 
-	err := queues.Create(client, "My Queue", clientID, createOpts).ExtractErr()
+	err := queues.Create(client, clientID, createOpts).ExtractErr()
 	if err != nil {
 		panic(err)
 	}
