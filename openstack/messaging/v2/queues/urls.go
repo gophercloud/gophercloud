7475db987ceb50c3a08dae3045d445505601548b
Jude Cross <jucross@blizzard.com>
2018-03-28 15:03:24 -0700

    Update urls to use contants

--- openstack/messaging/v2/queues/urls.go
+++ openstack/messaging/v2/queues/urls.go
@@ -2,9 +2,9 @@ package queues
 
 import "github.com/gophercloud/gophercloud"
 
-var apiVersion = "v2"
-var apiName = "queues"
+const ApiVersion = "v2"
+const ApiName = "queues"
 
 func createURL(client *gophercloud.ServiceClient, queueName string) string {
-	return client.ServiceURL(apiVersion, apiName, queueName)
+	return client.ServiceURL(ApiVersion, ApiName, queueName)
 }
