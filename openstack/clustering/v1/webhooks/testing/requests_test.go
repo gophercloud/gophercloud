ffeb2ba8aee016b9f6afb6a231beda1a2a5f3037
Jack Kuei <jkuei@blizzard.com>
2018-03-29 10:20:16 -0700

    Fixed trigger opts to properly construct the query strings for "V" and "params" Added test cases for nil, empty, and params to webhook trigger Added test case for testing query string construction

--- openstack/clustering/v1/webhooks/testing/requests_test.go
+++ openstack/clustering/v1/webhooks/testing/requests_test.go
@@ -12,7 +12,7 @@ import (
 	fake "github.com/gophercloud/gophercloud/testhelper/client"
 )
 
-func TestWebhooks(t *testing.T) {
+func TestWebhookTrigger(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
 
@@ -29,12 +29,30 @@ func TestWebhooks(t *testing.T) {
 			}`)
 	})
 
-	result, err := webhooks.Trigger(fake.ServiceClient(), "f93f83f6-762b-41b6-b757-80507834d394", webhooks.TriggerOpts{}).Extract()
+	result, err := webhooks.Trigger(fake.ServiceClient(),
+		"f93f83f6-762b-41b6-b757-80507834d394",
+		webhooks.TriggerOpts{V: "1", Params: map[string]string{"foo": "bar", "bar": "baz"}}).Extract()
 	th.AssertNoErr(t, err)
 	th.AssertEquals(t, result, "290c44fa-c60f-4d75-a0eb-87433ba982a3")
 }
 
-// Returns invalid type (integer) for action id
+// Test webhook with params that generates query strings
+func TestWebhookParams(t *testing.T) {
+	to := webhooks.TriggerOpts{
+		V: "1",
+		Params: map[string]string{
+			"foo": "bar",
+			"bar": "baz",
+		},
+	}
+
+	expected := "?V=1&bar=baz&foo=bar"
+	actual, err := to.ToWebhookTriggerQuery()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, actual, expected)
+}
+
+// Nagative test case for returning invalid type (integer) for action id
 func TestWebhooksInvalidAction(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
@@ -52,7 +70,60 @@ func TestWebhooksInvalidAction(t *testing.T) {
 			}`)
 	})
 
-	_, err := webhooks.Trigger(fake.ServiceClient(), "f93f83f6-762b-41b6-b757-80507834d394", webhooks.TriggerOpts{}).Extract()
+	_, err := webhooks.Trigger(fake.ServiceClient(),
+		"f93f83f6-762b-41b6-b757-80507834d394",
+		webhooks.TriggerOpts{V: "1"}).Extract()
 	isValid := err.(*json.UnmarshalTypeError) == nil
 	th.AssertEquals(t, false, isValid)
+}
+
+// Negative test case for passing empty TriggerOpt
+func TestWebhookTriggerInvalidEmptyOpt(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "POST")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, `
+			{
+				"action": "290c44fa-c60f-4d75-a0eb-87433ba982a3"
+			}`)
+	})
+
+	_, err := webhooks.Trigger(fake.ServiceClient(),
+		"f93f83f6-762b-41b6-b757-80507834d394",
+		webhooks.TriggerOpts{}).Extract()
+	if err == nil {
+		t.Errorf("Expected error without V param")
+	}
+}
+
+// Negative test case for passing in nil for TriggerOpt
+func TestWebhookTriggerInvalidNilOpt(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "POST")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, `
+			{
+				"action": "290c44fa-c60f-4d75-a0eb-87433ba982a3"
+			}`)
+	})
+
+	_, err := webhooks.Trigger(fake.ServiceClient(), "f93f83f6-762b-41b6-b757-80507834d394", nil).Extract()
+
+	if err == nil {
+		t.Errorf("Expected error with nil param")
+	}
 }
