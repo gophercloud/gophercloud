d6196a245e226ab7356f08f9bb5a8ea11183275b
Duc Truong <dtruong@blizzard.com>
2018-03-29 16:14:29 -0700

    fix policytype list example

--- openstack/clustering/v1/policytypes/doc.go
+++ openstack/clustering/v1/policytypes/doc.go
@@ -4,18 +4,18 @@ Clustering Service.
 
 Example to list policy types
 
-  allPages, err := policytypes.List(computeClient).AllPages()
-if err != nil {
-		panic(err)
-	}
+    allPages, err := policytypes.List(clusteringClient).AllPages()
+    if err != nil {
+        panic(err)
+    }
 
-	allPolicyTypes, err := actions.ExtractPolicyTypes(allPages)
-	if err != nil {
-		panic(err)
-	}
+    allPolicyTypes, err := actions.ExtractPolicyTypes(allPages)
+    if err != nil {
+        panic(err)
+    }
 
-	for _, policyType := range allPolicyTypes {
-		fmt.Printf("%+v\n", policyType)
-	}
+    for _, policyType := range allPolicyTypes {
+        fmt.Printf("%+v\n", policyType)
+    }
 */
 package policytypes
