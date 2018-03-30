03f9a3cdc75a858d54685447ae6643f95d37a0de
Wayne Warren <wayne.warren.s@gmail.com>
2018-03-29 19:30:16 -0500

    Update QuotaSet and UpdateQuotaSet types for block storage.

--- openstack/blockstorage/v3/extensions/quotasets/requests.go
+++ openstack/blockstorage/v3/extensions/quotasets/requests.go
@@ -40,47 +40,29 @@ func Delete(client *gophercloud.ServiceClient, tenantID string) (res DeleteResul
 // All int-values are pointers so they can be nil if they are not needed.
 // You can use gopercloud.IntToPointer() for convenience
 type UpdateOpts struct {
-	// FixedIPs is number of fixed ips alloted this quota_set.
-	FixedIPs *int `json:"fixed_ips,omitempty"`
+	// Volumes is the number of volumes that are allowed for each project.
+	Volumes *int `json:"volumes,omitempty"`
 
-	// FloatingIPs is number of floating ips alloted this quota_set.
-	FloatingIPs *int `json:"floating_ips,omitempty"`
+	// Snapshots is the number of snapshots that are allowed for each project.
+	Snapshots *int `json:"snapshots,omitempty"`
 
-	// InjectedFileContentBytes is content bytes allowed for each injected file.
-	InjectedFileContentBytes *int `json:"injected_file_content_bytes,omitempty"`
+	// Gigabytes is the size (GB) of volumes and snapshots that are allowed for
+	// each project.
+	Gigabytes *int `json:"gigabytes,omitempty"`
 
-	// InjectedFilePathBytes is allowed bytes for each injected file path.
-	InjectedFilePathBytes *int `json:"injected_file_path_bytes,omitempty"`
+	// PerVolumeGigabytes is the size (GB) of volumes and snapshots that are
+	// allowed for each project and the specifed volume type.
+	PerVolumeGigabytes *int `json:"per_volume_gigabytes,omitempty"`
 
-	// InjectedFiles is injected files allowed for each project.
-	InjectedFiles *int `json:"injected_files,omitempty"`
+	// Backups is the number of backups that are allowed for each project.
+	Backups *int `json:"backups,omitempty"`
 
-	// KeyPairs is number of ssh keypairs.
-	KeyPairs *int `json:"key_pairs,omitempty"`
+	// BackupGigabytes is the size (GB) of backups that are allowed for each
+	// project.
+	BackupGigabytes *int `json:"backup_gigabytes,omitempty"`
 
-	// MetadataItems is number of metadata items allowed for each instance.
-	MetadataItems *int `json:"metadata_items,omitempty"`
-
-	// RAM is megabytes allowed for each instance.
-	RAM *int `json:"ram,omitempty"`
-
-	// SecurityGroupRules is rules allowed for each security group.
-	SecurityGroupRules *int `json:"security_group_rules,omitempty"`
-
-	// SecurityGroups security groups allowed for each project.
-	SecurityGroups *int `json:"security_groups,omitempty"`
-
-	// Cores is number of instance cores allowed for each project.
-	Cores *int `json:"cores,omitempty"`
-
-	// Instances is number of instances allowed for each project.
-	Instances *int `json:"instances,omitempty"`
-
-	// Number of ServerGroups allowed for the project.
-	ServerGroups *int `json:"server_groups,omitempty"`
-
-	// Max number of Members for each ServerGroup.
-	ServerGroupMembers *int `json:"server_group_members,omitempty"`
+	// Groups is the number of groups that are allowed for each project.
+	Groups *int `json:"groups,omitempty"`
 
 	// Force will update the quotaset even if the quota has already been used
 	// and the reserved quota exceeds the new quota.
