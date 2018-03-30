18e5c4c842a0d274199a83982fa0ede6eb3b8aff
Jack Kuei <jkuei@blizzard.com>
2018-03-30 11:50:46 -0700

    senlin-profiles-create

---
+++ acceptance/openstack/clustering/v1/autoscaling_test.go
@@ -0,0 +1,62 @@
+package v1
+
+import (
+	"testing"
+
+	"github.com/gophercloud/gophercloud/acceptance/clients"
+	"github.com/gophercloud/gophercloud/openstack/clustering/v1/profiles"
+
+	"github.com/gophercloud/gophercloud/acceptance/tools"
+	th "github.com/gophercloud/gophercloud/testhelper"
+)
+
+var testName string
+
+func TestAutoScaling(t *testing.T) {
+	testName = tools.RandomString("TESTACC-", 8)
+	profileCreate(t)
+}
+
+func profileCreate(t *testing.T) {
+	client, err := clients.NewClusteringV1Client()
+	if err != nil {
+		t.Fatalf("Unable to create clustering client: %v", err)
+	}
+
+	networks := []map[string]interface{}{
+		{"network": "sandbox-internal-net"},
+	}
+
+	props := &map[string]interface{}{
+		"name":            "centos-server",
+		"flavor":          "t2.micro",
+		"image":           "centos7.3-latest",
+		"networks":        networks,
+		"security_groups": "",
+	}
+
+	profileName := testName
+	optsProfile := &profiles.CreateOpts{
+		Metadata: map[string]interface{}{
+			"foo":  "bar",
+			"test": "123",
+		},
+		Name: profileName,
+		Spec: map[string]interface{}{
+			"type":       "os.nova.server",
+			"version":    "1.0",
+			"properties": props,
+		},
+	}
+
+	profile, err := profiles.Create(client, optsProfile).Extract()
+	if err != nil {
+		t.Fatalf("Unable to create profile %s: %v", profileName, err)
+	} else {
+		t.Logf("Profile created %v", profile)
+	}
+
+	th.AssertEquals(t, profileName, profile.Name)
+	th.AssertEquals(t, "os.nova.server", (map[string]interface{})(profile.Spec)["type"])
+	th.AssertEquals(t, "1.0", (map[string]interface{})(profile.Spec)["version"])
+}
