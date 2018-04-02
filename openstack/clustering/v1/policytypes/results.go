d2426f82c33d910620b0a6a34ecc5fa57a693f6c
Duc Truong <dkt26111@gmail.com>
2018-04-02 11:49:58 -0700

    Clustering PolicyType Get implementation (#861)
    * Add Clustering PolicyType Get implementation
    
    * Changed PolicyTypeDetail Schema to map[string]interface{}
    
    * Updated test fixtures to use real response from API

--- openstack/clustering/v1/policytypes/results.go
+++ openstack/clustering/v1/policytypes/results.go
@@ -1,6 +1,7 @@
 package policytypes
 
 import (
+	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/pagination"
 )
 
@@ -35,4 +36,28 @@ type PolicyTypePage struct {
 func (page PolicyTypePage) IsEmpty() (bool, error) {
 	policyTypes, err := ExtractPolicyTypes(page)
 	return len(policyTypes) == 0, err
+}
+
+// PolicyTypeDetail represents the detailed policy type information for a clustering policy type
+type PolicyTypeDetail struct {
+	Name          string                         `json:"name"`
+	Schema        map[string]interface{}         `json:"schema"`
+	SupportStatus map[string][]SupportStatusType `json:"support_status,omitempty"`
+}
+
+// Extract provides access to the individual policy type returned by Get and extracts PolicyTypeDetail
+func (r policyTypeResult) Extract() (*PolicyTypeDetail, error) {
+	var s struct {
+		PolicyType *PolicyTypeDetail `json:"policy_type"`
+	}
+	err := r.ExtractInto(&s)
+	return s.PolicyType, err
+}
+
+type policyTypeResult struct {
+	gophercloud.Result
+}
+
+type GetResult struct {
+	policyTypeResult
 }
