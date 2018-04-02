bb0efb6109760cf377a062d6286244e673d3534d
Duc Truong <dtruong@blizzard.com>
2018-04-02 12:39:59 -0700

    Add Clustering Policy List Implementation

---
+++ acceptance/openstack/clustering/v1/policies_test.go
@@ -0,0 +1,35 @@
+// +build acceptance clustering policies
+
+package v1
+
+import (
+	"testing"
+
+	"github.com/gophercloud/gophercloud/acceptance/clients"
+	"github.com/gophercloud/gophercloud/acceptance/tools"
+	"github.com/gophercloud/gophercloud/openstack/clustering/v1/policies"
+	th "github.com/gophercloud/gophercloud/testhelper"
+)
+
+func TestPolicyList(t *testing.T) {
+	client, err := clients.NewClusteringV1Client()
+	th.AssertNoErr(t, err)
+
+	allPages, err := policies.List(client, nil).AllPages()
+	th.AssertNoErr(t, err)
+
+	allPolicies, err := policies.ExtractPolicies(allPages)
+	th.AssertNoErr(t, err)
+
+	for _, v := range allPolicies {
+		tools.PrintResource(t, v)
+
+		if v.CreatedAt != nil {
+			t.Log("Created at: " + (*v.CreatedAt).String())
+		}
+
+		if v.UpdatedAt != nil {
+			t.Log("Updated at: " + (*v.UpdatedAt).String())
+		}
+	}
+}
