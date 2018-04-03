92ea29d4f7b295050500a81a5ebfc1a706b87a25
Wayne Warren <wayne.warren.s@gmail.com>
2018-04-03 12:24:49 -0500

    Remove blockstorage quotaset defaults code.

--- openstack/blockstorage/v3/extensions/quotasets/urls.go
+++ openstack/blockstorage/v3/extensions/quotasets/urls.go
@@ -7,7 +7,3 @@ const resourcePath = "os-quota-sets"
 func getURL(c *gophercloud.ServiceClient, projectID string) string {
 	return c.ServiceURL(resourcePath, projectID)
 }
-
-func getDefaultsURL(c *gophercloud.ServiceClient, projectID string) string {
-	return c.ServiceURL(resourcePath, projectID, "defaults")
-}
