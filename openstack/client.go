31f5f65f0ca028f5ccc87328c3ea9631a7812255
Jude Cross <jucross@blizzard.com>
2018-04-07 00:17:33 -0700

    Add clientID parameter to NewMessagingV2

--- openstack/client.go
+++ openstack/client.go
@@ -403,6 +403,8 @@ func NewClusteringV1(client *gophercloud.ProviderClient, eo gophercloud.Endpoint
 
 // NewMessagingV2 creates a ServiceClient that may be used with the v2 messaging
 // service.
-func NewMessagingV2(client *gophercloud.ProviderClient, eo gophercloud.EndpointOpts) (*gophercloud.ServiceClient, error) {
-	return initClientOpts(client, eo, "messaging")
+func NewMessagingV2(client *gophercloud.ProviderClient, clientID string, eo gophercloud.EndpointOpts) (*gophercloud.ServiceClient, error) {
+	sc, err := initClientOpts(client, eo, "messaging")
+	sc.MoreHeaders = map[string]string{"Client-ID": clientID}
+	return sc, err
 }
