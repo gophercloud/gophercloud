bdd02f5d09fab52a1cc004aa541004c75772cd3a
Wayne Warren <wayne.warren.s@gmail.com>
2018-04-13 11:26:09 -0500

    Functions in requests.go should return named result.

--- openstack/blockstorage/extensions/quotasets/requests.go
+++ openstack/blockstorage/extensions/quotasets/requests.go
@@ -11,8 +11,7 @@ func Get(client *gophercloud.ServiceClient, projectID string) (r GetResult) {
 }
 
 // Get returns public data about the project's default block storage quotas.
-func GetDefaults(client *gophercloud.ServiceClient, projectID string) GetResult {
-	var res GetResult
-	_, res.Err = client.Get(getDefaultsURL(client, projectID), &res.Body, nil)
-	return res
+func GetDefaults(client *gophercloud.ServiceClient, projectID string) (r GetResult) {
+	_, r.Err = client.Get(getDefaultsURL(client, projectID), &r.Body, nil)
+	return
 }
