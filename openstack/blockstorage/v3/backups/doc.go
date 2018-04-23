930059bf157fae6ebf2a6db2ece10058e4071d3e
Eric M Gonzalez <eric@awnix.com>
2018-04-23 17:44:21 -0500

    inital copy and replace of blockstorage/v3/backups
    same as v2, "cp -a" and "sed -i -e "s/snapshot/backup/g"

---
+++ openstack/blockstorage/v3/backups/doc.go
@@ -0,0 +1,5 @@
+// Package backups provides information and interaction with backups in the
+// OpenStack Block Storage service. A backup is a point in time copy of the
+// data contained in an external storage volume, and can be controlled
+// programmatically.
+package backups
---
+++ openstack/blockstorage/v3/backups/testing/doc.go
@@ -0,0 +1,2 @@
+// backups_v3
+package testing
