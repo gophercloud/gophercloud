1ed923f8c92f701abf12d3eade7efff8552445b8
Kevin Zhao <kevin.zhao@arm.com>
2018-04-26 11:35:24 +0800

    Remove the unused error type
    Change-Id: Ib6f016107d3c5c367ceeb968c699c981c3238643
    Signed-off-by: Kevin Zhao <kevin.zhao@arm.com>

--- openstack/container/v1/capsules/errors.go
+++ openstack/container/v1/capsules/errors.go
@@ -6,28 +6,10 @@ import (
 	"github.com/gophercloud/gophercloud"
 )
 
-type ErrInvalidEnvironment struct {
-	gophercloud.BaseError
-	Section string
-}
-
-func (e ErrInvalidEnvironment) Error() string {
-	return fmt.Sprintf("Environment has wrong section: %s", e.Section)
-}
-
 type ErrInvalidDataFormat struct {
 	gophercloud.BaseError
 }
 
 func (e ErrInvalidDataFormat) Error() string {
 	return fmt.Sprintf("Data in neither json nor yaml format.")
-}
-
-type ErrInvalidTemplateFormatVersion struct {
-	gophercloud.BaseError
-	Version string
-}
-
-func (e ErrInvalidTemplateFormatVersion) Error() string {
-	return fmt.Sprintf("Template format version not found.")
 }
