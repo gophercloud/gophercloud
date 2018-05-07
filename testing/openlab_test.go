4115b59b651231d812849d771054ef594cfe8d05
zhang.zujian <zhang.zujian@datatom.com>
2018-05-07 16:49:05 +0800

    add some tests to check openlab testing

---
+++ testing/openlab_test.go
@@ -0,0 +1,11 @@
+package testing
+
+import "testing"
+
+func TestPassed(t *testing.T) {
+	t.Log("This is a test which will pass")
+}
+
+func TestNotPassed(t *testing.T) {
+	t.Fatal("This is a test which won't pass")
+}
