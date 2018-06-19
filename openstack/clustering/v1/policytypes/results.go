9b4b018254e9897ec68dcb48ebd0248933b08d7c
Joe Topjian <joe@topjian.net>
2018-06-19 03:29:11 +0000

    Clustering v1: policytypes updates

--- openstack/clustering/v1/policytypes/results.go
+++ openstack/clustering/v1/policytypes/results.go
@@ -19,13 +19,32 @@ type SupportStatusType struct {
 	Since  string `json:"since"`
 }
 
-// ExtractPolicyTypes interprets a page of results as a slice of PolicyTypes.
-func ExtractPolicyTypes(r pagination.Page) ([]PolicyType, error) {
+// PolicyTypeDetail represents the detailed policy type information for a
+// clustering policy type.
+type PolicyTypeDetail struct {
+	Name          string                         `json:"name"`
+	Schema        map[string]interface{}         `json:"schema"`
+	SupportStatus map[string][]SupportStatusType `json:"support_status,omitempty"`
+}
+
+// policyTypeResult is the base result of a Policy Type operation.
+type policyTypeResult struct {
+	gophercloud.Result
+}
+
+// Extract interprets any policyTypeResult result as a PolicyTypeDetail.
+func (r policyTypeResult) Extract() (*PolicyTypeDetail, error) {
 	var s struct {
-		PolicyTypes []PolicyType `json:"policy_types"`
+		PolicyType *PolicyTypeDetail `json:"policy_type"`
 	}
-	err := (r.(PolicyTypePage)).ExtractInto(&s)
-	return s.PolicyTypes, err
+	err := r.ExtractInto(&s)
+	return s.PolicyType, err
+}
+
+// GetResult is the result of a Get operation. Call its Extract method to
+// interpret it as a PolicyTypeDetail.
+type GetResult struct {
+	policyTypeResult
 }
 
 // PolicyTypePage contains a single page of all policy types from a List call.
@@ -39,28 +58,11 @@ func (page PolicyTypePage) IsEmpty() (bool, error) {
 	return len(policyTypes) == 0, err
 }
 
-// PolicyTypeDetail represents the detailed policy type information for a
-// clustering policy type.
-type PolicyTypeDetail struct {
-	Name          string                         `json:"name"`
-	Schema        map[string]interface{}         `json:"schema"`
-	SupportStatus map[string][]SupportStatusType `json:"support_status,omitempty"`
-}
-
-// Extract provides access to the individual policy type returned by Get
-// and extracts PolicyTypeDetail.
-func (r policyTypeResult) Extract() (*PolicyTypeDetail, error) {
+// ExtractPolicyTypes returns a slice of PolicyTypes from a List operation.
+func ExtractPolicyTypes(r pagination.Page) ([]PolicyType, error) {
 	var s struct {
-		PolicyType *PolicyTypeDetail `json:"policy_type"`
+		PolicyTypes []PolicyType `json:"policy_types"`
 	}
-	err := r.ExtractInto(&s)
-	return s.PolicyType, err
-}
-
-type policyTypeResult struct {
-	gophercloud.Result
-}
-
-type GetResult struct {
-	policyTypeResult
+	err := (r.(PolicyTypePage)).ExtractInto(&s)
+	return s.PolicyTypes, err
 }
