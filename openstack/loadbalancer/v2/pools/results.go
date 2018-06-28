225db9789eb70cdf61482fb9581e444bd032d919
Aya Igarashi <ladiclexxx@gmail.com>
2018-06-28 10:27:14 +0900

    Add ProvisioningStatus fields to /v2/loadbalancer

--- openstack/loadbalancer/v2/pools/results.go
+++ openstack/loadbalancer/v2/pools/results.go
@@ -92,6 +92,10 @@ type Pool struct {
 
 	// The Monitor associated with this Pool.
 	Monitor monitors.Monitor `json:"healthmonitor"`
+
+	// The provisioning status of the pool. Showing the pool detail API does not use this value.
+	// This value is ACTIVE, PENDING_* or ERROR.
+	ProvisioningStatus string `json:"provisioning_status"`
 }
 
 // PoolPage is the page returned by a pager when traversing over a
@@ -196,6 +200,10 @@ type Member struct {
 
 	// The unique ID for the Member.
 	ID string `json:"id"`
+
+	// The provisioning status of the pool. Showing the pool detail API does not use this value.
+	// This value is ACTIVE, PENDING_* or ERROR.
+	ProvisioningStatus string `json:"provisioning_status"`
 }
 
 // MemberPage is the page returned by a pager when traversing over a
