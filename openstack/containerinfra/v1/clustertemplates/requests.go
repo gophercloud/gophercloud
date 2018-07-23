75306672c5bee84b38f85003dee9e8773400b809
Jack Kuei <jkuei@blizzard.com>
2018-07-23 09:57:37 -0700

    Sort field names lexigraphically

--- openstack/containerinfra/v1/clustertemplates/requests.go
+++ openstack/containerinfra/v1/clustertemplates/requests.go
@@ -13,32 +13,32 @@ type CreateOptsBuilder interface {
 
 // CreateOpts params
 type CreateOpts struct {
-	Name                string            `json:"name,omitempty"`
+	APIServerPort       int               `json:"apiserver_port,omitempty"`
 	COE                 string            `json:"coe" required:"true"`
-	ImageID             string            `json:"image_id" required:"true"`
-	FlavorID            string            `json:"flavor_id,omitempty"`
-	MasterFlavorID      string            `json:"master_flavor_id,omitempty"`
 	DNSNameServer       string            `json:"dns_nameserver,omitempty"`
-	KeyPairID           string            `json:"keypair_id,omitempty"`
+	DockerStorageDriver string            `json:"docker_storage_driver,omitempty"`
+	DockerVolumeSize    int               `json:"docker_volume_size,omitempty"`
 	ExternalNetworkID   string            `json:"external_network_id,omitempty"`
 	FixedNetwork        string            `json:"fixed_network,omitempty"`
 	FixedSubnet         string            `json:"fixed_subnet,omitempty"`
-	NetworkDriver       string            `json:"network_driver,omitempty"`
-	APIServerPort       int               `json:"apiserver_port,omitempty"`
-	DockerVolumeSize    int               `json:"docker_volume_size,omitempty"`
+	FlavorID            string            `json:"flavor_id,omitempty"`
+	FloatingIPEnabled   *bool             `json:"floating_ip_enabled,omitempty"`
 	HTTPProxy           string            `json:"http_proxy,omitempty"`
 	HTTPSProxy          string            `json:"https_proxy,omitempty"`
-	NoProxy             string            `json:"no_proxy,omitempty"`
-	VolumeDriver        string            `json:"volume_driver,omitempty"`
-	RegistryEnabled     *bool             `json:"registry_enabled,omitempty"`
-	Labels              map[string]string `json:"labels,omitempty"`
-	TLSDisabled         *bool             `json:"tls_disabled,omitempty"`
-	Public              *bool             `json:"public,omitempty"`
-	ServerType          string            `json:"server_type,omitempty"`
+	ImageID             string            `json:"image_id" required:"true"`
 	InsecureRegistry    string            `json:"insecure_registry,omitempty"`
-	DockerStorageDriver string            `json:"docker_storage_driver,omitempty"`
+	KeyPairID           string            `json:"keypair_id,omitempty"`
+	Labels              map[string]string `json:"labels,omitempty"`
+	MasterFlavorID      string            `json:"master_flavor_id,omitempty"`
 	MasterLBEnabled     *bool             `json:"master_lb_enabled,omitempty"`
-	FloatingIPEnabled   *bool             `json:"floating_ip_enabled,omitempty"`
+	Name                string            `json:"name,omitempty"`
+	NetworkDriver       string            `json:"network_driver,omitempty"`
+	NoProxy             string            `json:"no_proxy,omitempty"`
+	Public              *bool             `json:"public,omitempty"`
+	RegistryEnabled     *bool             `json:"registry_enabled,omitempty"`
+	ServerType          string            `json:"server_type,omitempty"`
+	TLSDisabled         *bool             `json:"tls_disabled,omitempty"`
+	VolumeDriver        string            `json:"volume_driver,omitempty"`
 }
 
 // ToClusterCreateMap constructs a request body from CreateOpts.
--- openstack/containerinfra/v1/clustertemplates/results.go
+++ openstack/containerinfra/v1/clustertemplates/results.go
@@ -24,37 +24,37 @@ func (r commonResult) Extract() (*ClusterTemplate, error) {
 
 // Represents a template for a Cluster Template
 type ClusterTemplate struct {
-	InsecureRegistry    string             `json:"insecure_registry"`
-	Links               []gophercloud.Link `json:"links"`
-	HTTPProxy           string             `json:"http_proxy"`
-	UpdatedAt           time.Time          `json:"updated_at"`
-	FloatingIPEnabled   bool               `json:"floating_ip_enabled"`
+	APIServerPort       string             `json:"apiserver_port"`
+	COE                 string             `json:"coe"`
+	ClusterDistro       string             `json:"cluster_distro"`
+	CreatedAt           time.Time          `json:"created_at"`
+	DNSNameServer       string             `json:"dns_nameserver"`
+	DockerStorageDriver string             `json:"docker_storage_driver"`
+	DockerVolumeSize    int                `json:"docker_volume_size"`
+	ExternalNetworkID   string             `json:"external_network_id"`
+	FixedNetwork        string             `json:"fixed_network"`
 	FixedSubnet         string             `json:"fixed_subnet"`
-	MasterFlavorID      string             `json:"master_flavor_id"`
-	UserID              string             `json:"user_id"`
-	UUID                string             `json:"uuid"`
-	NoProxy             string             `json:"no_proxy"`
+	FlavorID            string             `json:"flavor_id"`
+	FloatingIPEnabled   bool               `json:"floating_ip_enabled"`
+	HTTPProxy           string             `json:"http_proxy"`
 	HTTPSProxy          string             `json:"https_proxy"`
-	TLSDisabled         bool               `json:"tls_disabled"`
+	ImageID             string             `json:"image_id"`
+	InsecureRegistry    string             `json:"insecure_registry"`
 	KeyPairID           string             `json:"keypair_id"`
+	Labels              map[string]string  `json:"labels"`
+	Links               []gophercloud.Link `json:"links"`
+	MasterFlavorID      string             `json:"master_flavor_id"`
+	MasterLBEnabled     bool               `json:"master_lb_enabled"`
+	Name                string             `json:"name"`
+	NetworkDriver       string             `json:"network_driver"`
+	NoProxy             string             `json:"no_proxy"`
 	ProjectID           string             `json:"project_id"`
 	Public              bool               `json:"public"`
-	Labels              map[string]string  `json:"labels"`
-	DockerVolumeSize    int                `json:"docker_volume_size"`
-	ServerType          string             `json:"server_type"`
-	ExternalNetworkID   string             `json:"external_network_id"`
-	ClusterDistro       string             `json:"cluster_distro"`
-	ImageID             string             `json:"image_id"`
-	VolumeDriver        string             `json:"volume_driver"`
 	RegistryEnabled     bool               `json:"registry_enabled"`
-	DockerStorageDriver string             `json:"docker_storage_driver"`
-	APIServerPort       string             `json:"apiserver_port"`
-	Name                string             `json:"name"`
-	CreatedAt           time.Time          `json:"created_at"`
-	NetworkDriver       string             `json:"network_driver"`
-	FixedNetwork        string             `json:"fixed_network"`
-	COE                 string             `json:"coe"`
-	FlavorID            string             `json:"flavor_id"`
-	MasterLBEnabled     bool               `json:"master_lb_enabled"`
-	DNSNameServer       string             `json:"dns_nameserver"`
+	ServerType          string             `json:"server_type"`
+	TLSDisabled         bool               `json:"tls_disabled"`
+	UUID                string             `json:"uuid"`
+	UpdatedAt           time.Time          `json:"updated_at"`
+	UserID              string             `json:"user_id"`
+	VolumeDriver        string             `json:"volume_driver"`
 }
