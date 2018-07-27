4037dc8cf6222b41ad198afb46dc91bff9316bb0
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-07-27 13:57:04 +0300

    Compute V2: extend documentation for the consoles
    Add a note about Nova microversion for the "remoteconsole" package
    usage.

--- openstack/compute/v2/extensions/remoteconsoles/doc.go
+++ openstack/compute/v2/extensions/remoteconsoles/doc.go
@@ -1,14 +1,20 @@
 /*
 Package remoteconsoles provides the ability to create server remote consoles
 through the Compute API.
+You need to specify at least "2.6" microversion for the ComputeClient to use
+that API.
 
 Example of Creating a new RemoteConsole
+
+  computeClient, err := openstack.NewComputeV2(providerClient, endpointOptions)
+  computeClient.Microversion = "2.6"
 
   createOpts := remoteconsoles.CreateOpts{
     Protocol: string(remoteconsoles.RemoteConsoleVNCProtocol),
     Type:     string(remoteconsoles.RemoteConsoleNoVNCType),
 	}
-	serverID := "b16ba811-199d-4ffd-8839-ba96c1185a67"
+  serverID := "b16ba811-199d-4ffd-8839-ba96c1185a67"
+
   remtoteConsole, err := remoteconsoles.Create(computeClient, serverID, createOpts).Extract()
   if err != nil {
     panic(err)
