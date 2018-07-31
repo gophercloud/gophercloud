d633ca3ee4ddfd6b9bd0bbae6605ae7549f49ef8
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-07-31 11:41:37 +0300

    Compute V2: add rescueunrescue package docs
    Add documentation for the "rescueunrescue" package.

--- openstack/compute/v2/extensions/rescueunrescue/doc.go
+++ openstack/compute/v2/extensions/rescueunrescue/doc.go
@@ -1,1 +1,20 @@
+/*
+Package rescueunrescue provides the ability to send server to a rescue mode and
+to return it back.
+
+Example to Rescue a server
+
+  rescueOpts := rescueunrescue.RescueOpts{
+    AdminPass:      "aUPtawPzE9NU",
+    RescueImageRef: "115e5c5b-72f0-4a0a-9067-60706545248c",
+  }
+  serverID := "3f54d05f-3430-4d80-aa07-63e6af9e2488"
+
+  adminPass, err := rescueunrescue.Rescue(computeClient, serverID, rescueOpts).Extract()
+  if err != nil {
+    panic(err)
+  }
+
+  fmt.Printf("adminPass of the rescued server %s: %s\n", serverID, adminPass)
+*/
 package rescueunrescue
