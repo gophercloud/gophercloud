168b5f57f067809a9f48131fcf0aaecf82b74cdd
Nathan Castelein <nathan.castelein@corp.ovh.com>
2018-08-27 10:44:24 +0200

    Add Workflowv2 clients

--- acceptance/clients/clients.go
+++ acceptance/clients/clients.go
@@ -607,3 +607,24 @@ func NewContainerInfraV1Client() (*gophercloud.ServiceClient, error) {
 		Region: os.Getenv("OS_REGION_NAME"),
 	})
 }
+
+// NewWorkflowV2Client returns a *ServiceClient for making calls
+// to the OpenStack Workflow v2 API (Mistral). An error will be returned if
+// authentication or client creation failed.
+func NewWorkflowV2Client() (*gophercloud.ServiceClient, error) {
+	ao, err := openstack.AuthOptionsFromEnv()
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := openstack.AuthenticatedClient(ao)
+	if err != nil {
+		return nil, err
+	}
+
+	client = configureDebug(client)
+
+	return openstack.NewWorkflowV2(client, gophercloud.EndpointOpts{
+		Region: os.Getenv("OS_REGION_NAME"),
+	})
+}
