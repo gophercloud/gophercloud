35e680c54f026902bddcfb9f0c4acf21c27afee6
Jack Kuei <jkuei@blizzard.com>
2018-08-29 12:12:39 -0700

    Removed unused code

--- openstack/containerinfra/v1/clusters/urls.go
+++ openstack/containerinfra/v1/clusters/urls.go
@@ -13,9 +13,6 @@ func commonURL(client *gophercloud.ServiceClient) string {
 func createURL(client *gophercloud.ServiceClient) string {
 	return commonURL(client)
 }
-func idURL(client *gophercloud.ServiceClient, id string) string {
-	return client.ServiceURL(apiName, id)
-}
 
 func getURL(c *gophercloud.ServiceClient, id string) string {
 	return c.ServiceURL("clusters", id)
