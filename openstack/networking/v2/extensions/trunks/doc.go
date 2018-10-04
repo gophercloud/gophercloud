7566894ff05ed40344d36add5460aa00f397f1b5
Antoni Segura Puimedon <celebdor@gmail.com>
2018-10-04 14:53:43 +0200

    Networking v2 Trunk support - Create
    This patch adds the Networking extension trunk support for Create
    operations.
    
    Signed-off-by: Antoni Segura Puimedon <celebdor@gmail.com>

---
+++ openstack/networking/v2/extensions/trunks/doc.go
@@ -0,0 +1,51 @@
+/*
+Package trunks provides the ability to retrieve and manage trunks through the Neutron API.
+Trunks allow you to multiplex multiple ports traffic on a single port. For example, you could
+have a compute instance port be the parent port of a trunk and inside the VM run workloads
+using other ports, without the need of plugging those ports.
+
+Example of a new empty Trunk creation
+
+	iTrue := true
+	createOpts := trunks.CreateOpts{
+		Name:         "gophertrunk",
+		Description:  "Trunk created by gophercloud",
+		AdminStateUp: &iTrue,
+		PortID:       "a6f0560c-b7a8-401f-bf6e-d0a5c851ae10",
+	}
+
+	trunk, err := trunks.Create(networkClient, trunkOpts).Extract()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", trunk)
+
+Example of a new Trunk creation with 2 subports
+
+	iTrue := true
+	createOpts := trunks.CreateOpts{
+		Name:         "gophertrunk",
+		Description:  "Trunk created by gophercloud",
+		AdminStateUp: &iTrue,
+		PortID:       "a6f0560c-b7a8-401f-bf6e-d0a5c851ae10",
+		Subports: []trunks.Subport{
+			{
+				SegmentationID:   1,
+				SegmentationType: "vlan",
+				PortID:           "bf4efcc0-b1c7-4674-81f0-31f58a33420a",
+			},
+			{
+				SegmentationID:   10,
+				SegmentationType: "vlan",
+				PortID:           "2cf671b9-02b3-4121-9e85-e0af3548d112",
+			},
+		},
+	}
+
+	trunk, err := trunks.Create(client, trunkOpts).Extract()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", trunk)
+*/
+package trunks
