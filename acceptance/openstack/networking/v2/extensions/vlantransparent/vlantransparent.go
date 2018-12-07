67c148cded3578b9fd0324eddc6a5c6ca39be623
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-12-07 16:15:31 +0300

    NetworkingV2: add VLAN transparent list helper
    Add ListVLANTransparentNetworks acceptance tests helper.

--- acceptance/openstack/networking/v2/extensions/vlantransparent/vlantransparent.go
+++ acceptance/openstack/networking/v2/extensions/vlantransparent/vlantransparent.go
@@ -1,6 +1,9 @@
 package v2
 
 import (
+	"testing"
+
+	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/vlantransparent"
 	"github.com/gophercloud/gophercloud/openstack/networking/v2/networks"
 )
@@ -10,4 +13,32 @@ import (
 type VLANTransparentNetwork struct {
 	networks.Network
 	vlantransparent.TransparentExt
+}
+
+// ListVLANTransparentNetworks will list networks with the "vlan-transparent"
+// extension. An error will be returned networks could not be listed.
+func ListVLANTransparentNetworks(t *testing.T, client *gophercloud.ServiceClient) ([]*VLANTransparentNetwork, error) {
+	iTrue := true
+	networkListOpts := networks.ListOpts{}
+	listOpts := vlantransparent.ListOptsExt{
+		ListOptsBuilder: networkListOpts,
+		VLANTransparent: &iTrue,
+	}
+
+	var allNetworks []*VLANTransparentNetwork
+
+	t.Log("Attempting to list VLAN-transparent networks")
+
+	allPages, err := networks.List(client, listOpts).AllPages()
+	if err != nil {
+		return nil, err
+	}
+	err = networks.ExtractNetworksInto(allPages, &allNetworks)
+	if err != nil {
+		return nil, err
+	}
+
+	t.Log("Successfully retrieved networks.")
+
+	return allNetworks, nil
 }
