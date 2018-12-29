fcad7e05c79f6aaaf643555fd78ae14f6dcb6dbb
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-12-29 11:47:40 +0300

    Address scopes: add unit tests for List request
    Add basic unit test for the List request.

---
+++ openstack/networking/v2/extensions/layer3/addressscopes/testing/fixtures.go
@@ -0,0 +1,49 @@
+package testing
+
+import "github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/layer3/addressscopes"
+
+// AddressScopesListResult represents raw response for the List request.
+const AddressScopesListResult = `
+{
+    "address_scopes": [
+        {
+            "name": "scopev4",
+            "tenant_id": "4a9807b773404e979b19633f38370643",
+            "ip_version": 4,
+            "shared": false,
+            "project_id": "4a9807b773404e979b19633f38370643",
+            "id": "9cc35860-522a-4d35-974d-51d4b011801e"
+        },
+        {
+            "name": "scopev6",
+            "tenant_id": "4a9807b773404e979b19633f38370643",
+            "ip_version": 6,
+            "shared": true,
+            "project_id": "4a9807b773404e979b19633f38370643",
+            "id": "be992b82-bf42-4ab7-bf7b-6baa8759d388"
+        }
+    ]
+}
+`
+
+// AddressScope1 represents first unmarshalled address scope from the
+// AddressScopesListResult.
+var AddressScope1 = addressscopes.AddressScope{
+	ID:        "9cc35860-522a-4d35-974d-51d4b011801e",
+	Name:      "scopev4",
+	TenantID:  "4a9807b773404e979b19633f38370643",
+	ProjectID: "4a9807b773404e979b19633f38370643",
+	IPversion: 4,
+	Shared:    false,
+}
+
+// AddressScope2 represents second unmarshalled address scope from the
+// AddressScopesListResult.
+var AddressScope2 = addressscopes.AddressScope{
+	ID:        "be992b82-bf42-4ab7-bf7b-6baa8759d388",
+	Name:      "scopev6",
+	TenantID:  "4a9807b773404e979b19633f38370643",
+	ProjectID: "4a9807b773404e979b19633f38370643",
+	IPversion: 6,
+	Shared:    true,
+}
---
+++ openstack/networking/v2/extensions/layer3/addressscopes/testing/requests_test.go
@@ -0,0 +1,51 @@
+package testing
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	fake "github.com/gophercloud/gophercloud/openstack/networking/v2/common"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/layer3/addressscopes"
+	"github.com/gophercloud/gophercloud/pagination"
+	th "github.com/gophercloud/gophercloud/testhelper"
+)
+
+func TestList(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/addressscopes", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, AddressScopesListResult)
+	})
+
+	count := 0
+
+	addressscopes.List(fake.ServiceClient(), addressscopes.ListOpts{}).EachPage(func(page pagination.Page) (bool, error) {
+		count++
+		actual, err := addressscopes.ExtractAddressScopes(page)
+		if err != nil {
+			t.Errorf("Failed to extract addressscopes: %v", err)
+			return false, nil
+		}
+
+		expected := []addressscopes.AddressScope{
+			AddressScope1,
+			AddressScope2,
+		}
+
+		th.CheckDeepEquals(t, expected, actual)
+
+		return true, nil
+	})
+
+	if count != 1 {
+		t.Errorf("Expected 1 page, got %d", count)
+	}
+}
