f27ceddc323ff01fdd909ac8377fb06b12db7f4f
Andrei Ozerov <andrei.ozerov92@gmail.com>
2019-01-06 03:17:28 +0300

    NetworkingV2: add Address scopes Create method (#1381)
    * NetworkingV2: implement address-scopes List call
    
    Add "address-scopes" package with List method.
    
    * Address scopes: add unit tests for List request
    
    Add basic unit test for the List request.
    
    * Address scopres: fix resource URL
    
    Use "address-scopes" instead of "addressscopes".
    
    * NetworkingV2: implement address-scopes Get call
    
    Implement address-scopes Get method with tests and documentation.
    
    * Address scopes: fix URL for Get request test
    
    Fix test handler URL.
    
    * NetworkingV2: add Address scopes Create method
    
    Implement Create method for the "address-scopes" extension with tests
    and documentation example.
    
    * Address scopes: fix documentation
    
    Fix documentation format and package name in the
    "openstack/networking/v2/extensions/layer3/addressscopes/doc.go".
    
    * Address scopes: rename IPversion to IPVersion
    
    Use "IPVersion" as the field name of the Address scope.

---
+++ openstack/networking/v2/extensions/layer3/addressscopes/doc.go
@@ -0,0 +1,43 @@
+/*
+Package addressscopes provides the ability to retrieve and manage Address scopes through the Neutron API.
+
+Example of Listing Address scopes
+
+    listOpts := addressscopes.ListOpts{
+        IPVersion: 6,
+    }
+
+    allPages, err := addressscopes.List(networkClient, listOpts).AllPages()
+    if err != nil {
+        panic(err)
+    }
+
+    allAddressScopes, err := addressscopes.ExtractAddressScopes(allPages)
+    if err != nil {
+        panic(err)
+    }
+
+    for _, addressScope := range allAddressScopes {
+        fmt.Printf("%+v\n", addressScope)
+    }
+
+Example to Get an Address scope
+
+    addressScopeID = "9cc35860-522a-4d35-974d-51d4b011801e"
+    addressScope, err := addressscopes.Get(networkClient, addressScopeID).Extract()
+    if err != nil {
+        panic(err)
+    }
+
+Example to Create a new Address scope
+
+    addressScopeOpts := addressscopes.CreateOpts{
+        Name: "my_address_scope",
+        IPVersion: 6,
+    }
+    addressScope, err := addressscopes.Create(networkClient, addressScopeOpts).Extract()
+    if err != nil {
+        panic(err)
+    }
+*/
+package addressscopes
