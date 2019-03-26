0d99fea8e36d0ccd8911ac024624c511430bf3be
Maysa <maysa.macedo95@gmail.com>
2019-03-26 14:43:47 -0400

    Update comment to have method name

--- openstack/loadbalancer/v2/apiversions/requests.go
+++ openstack/loadbalancer/v2/apiversions/requests.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gophercloud/gophercloud/pagination"
 )
 
-// lists all the load balancer API versions available to end-users.
+// List lists all the load balancer API versions available to end-users.
 func List(c *gophercloud.ServiceClient) pagination.Pager {
 	return pagination.NewPager(c, apiVersionsURL(c), func(r pagination.PageResult) pagination.Page {
 		return APIVersionPage{pagination.SinglePageBase(r)}
