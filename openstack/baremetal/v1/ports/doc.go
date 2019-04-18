bee12df7a499db758d24e5a3376a75f94de13ff5
Joe Topjian <joe@topjian.net>
2019-04-18 02:18:26 +0000

    Docs: Updating baremetal/inspector docs

--- openstack/baremetal/v1/ports/doc.go
+++ openstack/baremetal/v1/ports/doc.go
@@ -1,5 +1,3 @@
-package ports
-
 /*
  Package ports contains the functionality to Listing, Searching, Creating, Updating,
  and Deleting of bare metal Port resources
@@ -7,8 +5,9 @@ package ports
  API reference: https://developer.openstack.org/api-ref/baremetal/#ports-ports
 
 
- 	// Example to List Ports with Detail
- 	ports.ListDetail(client, ports.ListOpts{}).EachPage(func(page pagination.Page) (bool, error) {
+Example to List Ports with Detail
+
+ 	ports.ListDetail(client, nil).EachPage(func(page pagination.Page) (bool, error) {
  		portList, err := ports.ExtractPorts(page)
  		if err != nil {
  			return false, err
@@ -21,10 +20,13 @@ package ports
  		return true, nil
  	})
 
- 	// Example to List Ports
- 	ports.List(client, ports.ListOpts{
+Example to List Ports
+
+	listOpts := ports.ListOpts{
  		Limit: 10,
- 	}).EachPage(func(page pagination.Page) (bool, error) {
+	}
+
+ 	ports.List(client, listOpts).EachPage(func(page pagination.Page) (bool, error) {
  		portList, err := ports.ExtractPorts(page)
  		if err != nil {
  			return false, err
@@ -37,37 +39,47 @@ package ports
  		return true, nil
  	})
 
- 	// Example to Create a Port
- 	createPort, err := ports.Create(client, ports.CreateOpts{
+Example to Create a Port
+
+	createOpts := ports.CreateOpts{
  		NodeUUID: "e8920409-e07e-41bb-8cc1-72acb103e2dd",
 		Address: "00:1B:63:84:45:E6",
     PhysicalNetwork: "my-network",
- 	}).Extract()
+	}
+
+ 	createPort, err := ports.Create(client, createOpts).Extract()
  	if err != nil {
  		panic(err)
  	}
 
- 	// Example to Get a Port
+Example to Get a Port
+
  	showPort, err := ports.Get(client, "c9afd385-5d89-4ecb-9e1c-68194da6b474").Extract()
  	if err != nil {
  		panic(err)
  	}
 
- 	// Example to Update a Port
- 	updatePort, err := ports.Update(client, "c9afd385-5d89-4ecb-9e1c-68194da6b474", ports.UpdateOpts{
+Example to Update a Port
+
+	updateOpts := ports.UpdateOpts{
  		ports.UpdateOperation{
  			Op:    ReplaceOp,
  			Path:  "/address",
  			Value: "22:22:22:22:22:22",
  		},
- 	}).Extract()
+	}
+
+ 	updatePort, err := ports.Update(client, "c9afd385-5d89-4ecb-9e1c-68194da6b474", updateOpts).Extract()
  	if err != nil {
  		panic(err)
  	}
 
- 	// Example to Delete a Port
+Example to Delete a Port
+
  	err = ports.Delete(client, "c9afd385-5d89-4ecb-9e1c-68194da6b474").ExtractErr()
  	if err != nil {
  		panic(err)
+	}
 
 */
+package ports
--- openstack/baremetalintrospection/noauth/doc.go
+++ openstack/baremetalintrospection/noauth/doc.go
@@ -1,9 +1,7 @@
-package noauth
-
 /*
 Package noauth provides support for noauth bare metal introspection endpoints.
 
-	Example of obtaining and using a client:
+Example of obtaining and using a client:
 
 	client, err := noauth.NewBareMetalIntrospectionNoAuth(noauth.EndpointOpts{
 		IronicInspectorEndpoint: "http://localhost:5050/v1/",
@@ -14,3 +12,4 @@ Package noauth provides support for noauth bare metal introspection endpoints.
 
 	introspection.GetIntrospectionStatus(client, "a62b8495-52e2-407b-b3cb-62775d04c2b8")
 */
+package noauth
