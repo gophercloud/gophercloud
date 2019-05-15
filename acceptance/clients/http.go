1992d5238d782a2ae0063b16eb9705548e6b351b
kayrus <kayrus@users.noreply.github.com>
2019-05-15 03:18:19 +0200

    Don't print token on token auth method (#1567)

--- acceptance/clients/http.go
+++ acceptance/clients/http.go
@@ -129,6 +129,9 @@ func (lrt *LogRoundTripper) formatJSON(raw []byte) string {
 					v["password"] = "***"
 				}
 			}
+			if v, ok := v["token"].(map[string]interface{}); ok {
+				v["id"] = "***"
+			}
 		}
 	}
 
