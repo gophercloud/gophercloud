7cdf87ec09734d2c1aa9dd3a2dbed17fe16e393c
Andrei Ozerov <andrei.ozerov92@gmail.com>
2019-05-25 09:16:35 +0300

    Networking V2: add QoS policy extension for Port (#1580)
    * Networking V2: add QoS policy extension for Port
    
    Allow to Create a Port with the QoS policy.
    
    Allow to add or delete a QoS policy from an existing Port.
    
    Allow to retrieve a Port with QoS policy from the API.
    
    * Networking V2: cleanup QoS for Ports
    
    Cleanup format, docs and tests.

---
+++ openstack/networking/v2/extensions/qos/policies/doc.go
@@ -0,0 +1,93 @@
+/*
+Package policies provides information and interaction with the QoS policy extension
+for the OpenStack Networking service.
+
+Example to Get a Port with a QoS policy
+
+	var portWithQoS struct {
+        ports.Port
+        policies.QoSPolicyExt
+    }
+
+    portID := "46d4bfb9-b26e-41f3-bd2e-e6dcc1ccedb2"
+
+    err = ports.Get(client, portID).ExtractInto(&portWithQoS)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("Port: %+v\n", portWithQoS)
+
+Example to Create a Port with a QoS policy
+
+	var portWithQoS struct {
+		ports.Port
+		policies.QoSPolicyExt
+	}
+
+	policyID := "d6ae28ce-fcb5-4180-aa62-d260a27e09ae"
+	networkID := "7069db8d-e817-4b39-a654-d2dd76e73d36"
+
+	portCreateOpts := ports.CreateOpts{
+		NetworkID: networkID,
+	}
+
+	createOpts := policies.PortCreateOptsExt{
+		CreateOptsBuilder: portCreateOpts,
+		QoSPolicyID:       policyID,
+	}
+
+	err = ports.Create(client, createOpts).ExtractInto(&portWithQoS)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Port: %+v\n", portWithQoS)
+
+Example to add a QoS policy to an existing Port
+
+    var portWithQoS struct {
+        ports.Port
+        policies.QoSPolicyExt
+    }
+
+    portUpdateOpts := ports.UpdateOpts{}
+
+    policyID := "d6ae28ce-fcb5-4180-aa62-d260a27e09ae"
+
+    updateOpts := policies.PortUpdateOptsExt{
+        UpdateOptsBuilder: portUpdateOpts,
+        QoSPolicyID: &policyID,
+    }
+
+    err := ports.Update(client, "65c0ee9f-d634-4522-8954-51021b570b0d", updateOpts).ExtractInto(&portWithQoS)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Printf("Port: %+v\n", portWithQoS)
+
+Example to delete a QoS policy from the existing Port
+
+    var portWithQoS struct {
+        ports.Port
+        policies.QoSPolicyExt
+    }
+
+    portUpdateOpts := ports.UpdateOpts{}
+
+    policyID := ""
+
+    updateOpts := policies.PortUpdateOptsExt{
+        UpdateOptsBuilder: portUpdateOpts,
+        QoSPolicyID: &policyID,
+    }
+
+    err := ports.Update(client, "65c0ee9f-d634-4522-8954-51021b570b0d", updateOpts).ExtractInto(&portWithQoS)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Printf("Port: %+v\n", portWithQoS)
+*/
+package policies
---
+++ openstack/networking/v2/extensions/qos/policies/testing/requests_test.go
@@ -0,0 +1,152 @@
+package testing
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	fake "github.com/gophercloud/gophercloud/openstack/networking/v2/common"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/qos/policies"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/ports"
+	th "github.com/gophercloud/gophercloud/testhelper"
+)
+
+func TestGetPort(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/ports/65c0ee9f-d634-4522-8954-51021b570b0d", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		_, err := fmt.Fprintf(w, GetPortResponse)
+		th.AssertNoErr(t, err)
+	})
+
+	var p struct {
+		ports.Port
+		policies.QoSPolicyExt
+	}
+	err := ports.Get(fake.ServiceClient(), "65c0ee9f-d634-4522-8954-51021b570b0d").ExtractInto(&p)
+	th.AssertNoErr(t, err)
+
+	th.AssertEquals(t, p.ID, "65c0ee9f-d634-4522-8954-51021b570b0d")
+	th.AssertEquals(t, p.QoSPolicyID, "591e0597-39a6-4665-8149-2111d8de9a08")
+}
+
+func TestCreatePort(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/ports", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "POST")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		th.TestHeader(t, r, "Content-Type", "application/json")
+		th.TestHeader(t, r, "Accept", "application/json")
+		th.TestJSONRequest(t, r, CreatePortRequest)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+
+		_, err := fmt.Fprintf(w, CreatePortResponse)
+		th.AssertNoErr(t, err)
+	})
+
+	var p struct {
+		ports.Port
+		policies.QoSPolicyExt
+	}
+	portCreateOpts := ports.CreateOpts{
+		NetworkID: "a87cc70a-3e15-4acf-8205-9b711a3531b7",
+	}
+	createOpts := policies.PortCreateOptsExt{
+		CreateOptsBuilder: portCreateOpts,
+		QoSPolicyID:       "591e0597-39a6-4665-8149-2111d8de9a08",
+	}
+	err := ports.Create(fake.ServiceClient(), createOpts).ExtractInto(&p)
+	th.AssertNoErr(t, err)
+
+	th.AssertEquals(t, p.NetworkID, "a87cc70a-3e15-4acf-8205-9b711a3531b7")
+	th.AssertEquals(t, p.TenantID, "d6700c0c9ffa4f1cb322cd4a1f3906fa")
+	th.AssertEquals(t, p.ID, "65c0ee9f-d634-4522-8954-51021b570b0d")
+	th.AssertEquals(t, p.QoSPolicyID, "591e0597-39a6-4665-8149-2111d8de9a08")
+}
+
+func TestUpdatePortWithPolicy(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/ports/65c0ee9f-d634-4522-8954-51021b570b0d", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "PUT")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		th.TestHeader(t, r, "Content-Type", "application/json")
+		th.TestHeader(t, r, "Accept", "application/json")
+		th.TestJSONRequest(t, r, UpdatePortWithPolicyRequest)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		_, err := fmt.Fprintf(w, UpdatePortWithPolicyResponse)
+		th.AssertNoErr(t, err)
+	})
+
+	policyID := "591e0597-39a6-4665-8149-2111d8de9a08"
+
+	var p struct {
+		ports.Port
+		policies.QoSPolicyExt
+	}
+	portUpdateOpts := ports.UpdateOpts{}
+	updateOpts := policies.PortUpdateOptsExt{
+		UpdateOptsBuilder: portUpdateOpts,
+		QoSPolicyID:       &policyID,
+	}
+	err := ports.Update(fake.ServiceClient(), "65c0ee9f-d634-4522-8954-51021b570b0d", updateOpts).ExtractInto(&p)
+	th.AssertNoErr(t, err)
+
+	th.AssertEquals(t, p.NetworkID, "a87cc70a-3e15-4acf-8205-9b711a3531b7")
+	th.AssertEquals(t, p.TenantID, "d6700c0c9ffa4f1cb322cd4a1f3906fa")
+	th.AssertEquals(t, p.ID, "65c0ee9f-d634-4522-8954-51021b570b0d")
+	th.AssertEquals(t, p.QoSPolicyID, "591e0597-39a6-4665-8149-2111d8de9a08")
+}
+
+func TestUpdatePortWithoutPolicy(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/ports/65c0ee9f-d634-4522-8954-51021b570b0d", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "PUT")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		th.TestHeader(t, r, "Content-Type", "application/json")
+		th.TestHeader(t, r, "Accept", "application/json")
+		th.TestJSONRequest(t, r, UpdatePortWithoutPolicyRequest)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		_, err := fmt.Fprintf(w, UpdatePortWithoutPolicyResponse)
+		th.AssertNoErr(t, err)
+	})
+
+	policyID := ""
+
+	var p struct {
+		ports.Port
+		policies.QoSPolicyExt
+	}
+	portUpdateOpts := ports.UpdateOpts{}
+	updateOpts := policies.PortUpdateOptsExt{
+		UpdateOptsBuilder: portUpdateOpts,
+		QoSPolicyID:       &policyID,
+	}
+	err := ports.Update(fake.ServiceClient(), "65c0ee9f-d634-4522-8954-51021b570b0d", updateOpts).ExtractInto(&p)
+	th.AssertNoErr(t, err)
+
+	th.AssertEquals(t, p.NetworkID, "a87cc70a-3e15-4acf-8205-9b711a3531b7")
+	th.AssertEquals(t, p.TenantID, "d6700c0c9ffa4f1cb322cd4a1f3906fa")
+	th.AssertEquals(t, p.ID, "65c0ee9f-d634-4522-8954-51021b570b0d")
+	th.AssertEquals(t, p.QoSPolicyID, "")
+}
