0914ad1fd037be3acda3c7441935001b2d0d228d
Andrei Ozerov <andrei.ozerov92@gmail.com>
2019-06-11 13:19:30 +0300

    Networking V2: add QoS DSCP marking acc test
    Implement TestDSCPMarkingRulesCRUD and add CreateDSCPMarkingRule test
    helper.

--- acceptance/openstack/networking/v2/extensions/qos/rules/rules_test.go
+++ acceptance/openstack/networking/v2/extensions/qos/rules/rules_test.go
@@ -53,3 +53,46 @@ func TestBandwidthLimitRulesCRUD(t *testing.T) {
 
 	th.AssertEquals(t, found, true)
 }
+
+func TestDSCPMarkingRulesCRUD(t *testing.T) {
+	client, err := clients.NewNetworkV2Client()
+	th.AssertNoErr(t, err)
+
+	// Create a QoS policy
+	policy, err := accpolicies.CreateQoSPolicy(t, client)
+	th.AssertNoErr(t, err)
+	defer policies.Delete(client, policy.ID)
+
+	tools.PrintResource(t, policy)
+
+	// Create a QoS policy rule.
+	rule, err := CreateDSCPMarkingRule(t, client, policy.ID)
+	th.AssertNoErr(t, err)
+	defer rules.DeleteDSCPMarkingRule(client, policy.ID, rule.ID)
+
+	// Update the QoS policy rule.
+	dscpMark := 20
+	updateOpts := rules.UpdateDSCPMarkingRuleOpts{
+		DSCPMark: &dscpMark,
+	}
+	newRule, err := rules.UpdateDSCPMarkingRule(client, policy.ID, rule.ID, updateOpts).ExtractDSCPMarkingRule()
+	th.AssertNoErr(t, err)
+
+	tools.PrintResource(t, newRule)
+	th.AssertEquals(t, newRule.DSCPMark, 20)
+
+	allPages, err := rules.ListDSCPMarkingRules(client, policy.ID, rules.DSCPMarkingRulesListOpts{}).AllPages()
+	th.AssertNoErr(t, err)
+
+	allRules, err := rules.ExtractDSCPMarkingRules(allPages)
+	th.AssertNoErr(t, err)
+
+	var found bool
+	for _, rule := range allRules {
+		if rule.ID == newRule.ID {
+			found = true
+		}
+	}
+
+	th.AssertEquals(t, found, true)
+}
