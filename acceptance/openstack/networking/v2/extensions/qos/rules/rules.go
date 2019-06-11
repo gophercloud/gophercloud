8b7d9c2fd33b81ea0adee423d55a118fcc1229da
Andrei Ozerov <andrei.ozerov92@gmail.com>
2019-06-11 23:10:52 +0300

    Networking V2: add QoS DSCP marking acc test (#1612)
    Implement TestDSCPMarkingRulesCRUD and add CreateDSCPMarkingRule test
    helper.

--- acceptance/openstack/networking/v2/extensions/qos/rules/rules.go
+++ acceptance/openstack/networking/v2/extensions/qos/rules/rules.go
@@ -33,3 +33,26 @@ func CreateBandwidthLimitRule(t *testing.T, client *gophercloud.ServiceClient, p
 
 	return rule, nil
 }
+
+// CreateDSCPMarkingRule will create a QoS DSCPMarkingRule associated with the provided QoS policy.
+// An error will be returned if the QoS rule could not be created.
+func CreateDSCPMarkingRule(t *testing.T, client *gophercloud.ServiceClient, policyID string) (*rules.DSCPMarkingRule, error) {
+	dscpMark := 26
+
+	createOpts := rules.CreateDSCPMarkingRuleOpts{
+		DSCPMark: dscpMark,
+	}
+
+	t.Logf("Attempting to create a QoS DSCP marking rule with dscp_mark: %d", dscpMark)
+
+	rule, err := rules.CreateDSCPMarkingRule(client, policyID, createOpts).ExtractDSCPMarkingRule()
+	if err != nil {
+		return nil, err
+	}
+
+	t.Logf("Succesfully created a QoS DSCP marking rule")
+
+	th.AssertEquals(t, dscpMark, rule.DSCPMark)
+
+	return rule, nil
+}
