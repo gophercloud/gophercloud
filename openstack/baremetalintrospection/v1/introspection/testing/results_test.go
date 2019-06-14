a626d7b2cbfb94824321291f89eb1b7e718a84e2
Brad P. Crochet <brad@redhat.com>
2019-06-14 11:11:27 -0400

    Baremetal Introspection: Move hostname to InventoryType
    The IPA patch added the hostname to the Inventory field, not as a top-level
    field. This moves the hostname into the InventoryType where it belongs.

--- openstack/baremetalintrospection/v1/introspection/testing/results_test.go
+++ openstack/baremetalintrospection/v1/introspection/testing/results_test.go
@@ -87,3 +87,80 @@ func TestExtraHardware(t *testing.T) {
 		t.Errorf("Failed to unmarshal ExtraHardware data: %s", err)
 	}
 }
+
+func TestHostnameInInventory(t *testing.T) {
+	inventoryJson := `{
+		"bmc_address":"192.167.2.134",
+		"interfaces":[
+		   {
+			  "lldp":[],
+			  "product":"0x0001",
+			  "vendor":"0x1af4",
+			  "name":"eth1",
+			  "has_carrier":true,
+			  "ipv4_address":"172.24.42.101",
+			  "client_id":null,
+			  "mac_address":"52:54:00:47:20:4d"
+		   },
+		   {
+			  "lldp": [
+				[1, "04112233aabbcc"],
+				[5, "737730312d646973742d31622d623132"]
+			  ],
+			  "product":"0x0001",
+			  "vendor":"0x1af4",
+			  "name":"eth0",
+			  "has_carrier":true,
+			  "ipv4_address":"172.24.42.100",
+			  "client_id":null,
+			  "mac_address":"52:54:00:4e:3d:30"
+		   }
+		],
+		"disks":[
+		   {
+			  "rotational":true,
+			  "vendor":"0x1af4",
+			  "name":"/dev/vda",
+			  "hctl":null,
+			  "wwn_vendor_extension":null,
+			  "wwn_with_extension":null,
+			  "model":"",
+			  "wwn":null,
+			  "serial":null,
+			  "size":13958643712
+		   }
+		],
+		"boot":{
+		   "current_boot_mode":"bios",
+		   "pxe_interface":"52:54:00:4e:3d:30"
+		},
+		"system_vendor":{
+		   "serial_number":"Not Specified",
+		   "product_name":"Bochs",
+		   "manufacturer":"Bochs"
+		},
+		"memory":{
+		   "physical_mb":2048,
+		   "total":2105864192
+		},
+		"cpu":{
+		   "count":2,
+		   "frequency":"2100.084",
+			"flags": [
+				"fpu",
+				"mmx",
+				"fxsr",
+			  	"sse",
+			  	"sse2"
+			],
+			"architecture":"x86_64"
+		},
+		"hostname": "master-0"
+	}`
+
+	var output introspection.InventoryType
+	err := json.Unmarshal([]byte(inventoryJson), &output)
+	if err != nil {
+		t.Errorf("Failed to unmarshal Inventory data: %s", err)
+	}
+}
