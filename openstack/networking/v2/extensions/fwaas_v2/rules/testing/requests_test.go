734b1b242786af34a06d7309c2777fc69653b6f1
Colin Fowler <elethiomel@gmail.com>
2019-08-28 10:27:33 +0000

    tests need to use fwaas_v2, not fwaas

--- openstack/networking/v2/extensions/fwaas_v2/rules/testing/requests_test.go
+++ openstack/networking/v2/extensions/fwaas_v2/rules/testing/requests_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 
 	fake "github.com/gophercloud/gophercloud/openstack/networking/v2/common"
-	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/fwaas/rules"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/fwaas_v2/rules"
 	"github.com/gophercloud/gophercloud/pagination"
 	th "github.com/gophercloud/gophercloud/testhelper"
 )
