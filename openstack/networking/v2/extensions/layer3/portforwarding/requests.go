5e6a252846431f507727e434418684571cfddc51
sreinkemeier <sreinkemeier@inovex.de>
2019-09-06 14:51:53 +0200

    Add list request

--- openstack/networking/v2/extensions/layer3/portforwarding/requests.go
+++ openstack/networking/v2/extensions/layer3/portforwarding/requests.go
@@ -1,6 +1,53 @@
 package portforwarding
 
-import "github.com/gophercloud/gophercloud"
+import (
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/pagination"
+)
+
+type ListOptsBuilder interface {
+	ToPortForwardingListQuery() (string, error)
+}
+
+// ListOpts allows the filtering and sorting of paginated collections through
+// the API. Filtering is achieved by passing in struct field values that map to
+// the port forwarding attributes you want to see returned. SortKey allows you to
+// sort by a particular network attribute. SortDir sets the direction, and is
+// either `asc' or `desc'. Marker and Limit are used for pagination.
+type ListOpts struct {
+	ID                string `q:"id"`
+	InternalPortID    string `q:"internal_port_id"`
+	ExternalPort      string `q:"external_port"`
+	InternalIPAddress string `q:"internal_ip_address"`
+	Protocol          string `q:"protocol"`
+	InternalPort      string `q:"internal_port"`
+	SortKey           string `q:"sort_key"`
+	SortDir           string `q:"sort_dir"`
+	Fields            string `q:"fields"`
+}
+
+// ToNetworkListQuery formats a ListOpts into a query string.
+func (opts ListOpts) ToPortForwardingListQuery() (string, error) {
+	q, err := gophercloud.BuildQueryString(opts)
+	return q.String(), err
+}
+
+// List returns a Pager which allows you to iterate over a collection of
+// floating IP resources. It accepts a ListOpts struct, which allows you to
+// filter and sort the returned collection for greater efficiency.
+func ListPortForwardings(c *gophercloud.ServiceClient, opts ListOptsBuilder, id string) pagination.Pager {
+	url := portForwardingUrl(c, id)
+	if opts != nil {
+		query, err := opts.ToPortForwardingListQuery()
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		url += query
+	}
+	return pagination.NewPager(c, url, func(r pagination.PageResult) pagination.Page {
+		return PortForwardingPage{pagination.LinkedPageBase{PageResult: r}}
+	})
+}
 
 // CreateOpts contains all the values needed to create a new port forwarding
 // resource. All attributes are required.
