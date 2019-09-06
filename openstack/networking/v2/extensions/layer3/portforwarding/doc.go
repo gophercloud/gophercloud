1e6b7b77946eae6aa1d207baf9e9a7288cbda202
sreinkemeier <sreinkemeier@inovex.de>
2019-09-06 15:21:19 +0200

    Add documentation

--- openstack/networking/v2/extensions/layer3/portforwarding/doc.go
+++ openstack/networking/v2/extensions/layer3/portforwarding/doc.go
@@ -2,6 +2,23 @@
 package portforwarding enables management and retrieval of port forwarding resources for Floating IPs from the
 OpenStack Networking service.
 
+Example to list all Port Forwardings for a floating IP
+
+	fipID := "2f245a7b-796b-4f26-9cf9-9e82d248fda7"
+	allPages, err := portforwarding.List(client, portforwarding.ListOpts{}, fipID).AllPages()
+	if err != nil {
+		panic(err)
+	}
+
+	allPFs, err := portforwarding.ExtractPortForwardings(allPages)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, pf := range allPFs {
+		fmt.Printf("%+v\n", pf)
+	}
+
 Example to Create a Port Forwarding for a floating IP
 
 	createOpts := &portforwarding.CreateOpts{
