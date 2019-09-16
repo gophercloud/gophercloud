ba9854084bf3a33298ee67217d2fc4e38707e46a
sreinkemeier <sreinkemeier@inovex.de>
2019-09-16 15:25:39 +0200

    Add doc

--- openstack/networking/v2/extensions/layer3/portforwarding/doc.go
+++ openstack/networking/v2/extensions/layer3/portforwarding/doc.go
@@ -45,11 +45,26 @@ Example to Create a Port Forwarding for a floating IP
 		panic(err)
 	}
 
+Example to Update a Port Forwarding
+
+	updateOpts := portforwarding.UpdateOpts{
+		Protocol:     "udp",
+		InternalPort: 30,
+		ExternalPort: 678,
+	}
+	fipID := "2f245a7b-796b-4f26-9cf9-9e82d248fda7"
+	pfID := "725ade3c-9760-4880-8080-8fc2dbab9acc"
+
+	pf, err := portforwarding.Update(client, fipID, pfID, updateOpts).Extract()
+	if err != nil {
+		panic(err)
+	}
+
 Example to Delete a Port forwarding
 
 	fipID := "2f245a7b-796b-4f26-9cf9-9e82d248fda7"
 	pfID := "725ade3c-9760-4880-8080-8fc2dbab9acc"
-	err := floatingips.Delete(networkClient, fipID, pfID).ExtractErr()
+	err := portforwarding.Delete(networkClient, fipID, pfID).ExtractErr()
 	if err != nil {
 		panic(err)
 	}
