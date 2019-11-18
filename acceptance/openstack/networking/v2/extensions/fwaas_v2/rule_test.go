cb69c88cd8c58d9c6c433a06904e879e7206bbf2
Colin Fowler <elethiomel@gmail.com>
2019-11-18 17:26:56 +0000

    Rules Get

--- acceptance/openstack/networking/v2/extensions/fwaas_v2/rule_test.go
+++ acceptance/openstack/networking/v2/extensions/fwaas_v2/rule_test.go
@@ -7,10 +7,11 @@ import (
 
 	"github.com/gophercloud/gophercloud/acceptance/clients"
 	"github.com/gophercloud/gophercloud/acceptance/tools"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/fwaas_v2/rules"
 	th "github.com/gophercloud/gophercloud/testhelper"
 )
 
-func TestRuleCD(t *testing.T) {
+func TestRuleCRD(t *testing.T) {
 	client, err := clients.NewNetworkV2Client()
 	th.AssertNoErr(t, err)
 
@@ -19,4 +20,9 @@ func TestRuleCD(t *testing.T) {
 	defer DeleteRule(t, client, rule.ID)
 
 	tools.PrintResource(t, rule)
+
+	newRule, err := rules.Get(client, rule.ID).Extract()
+	th.AssertNoErr(t, err)
+
+	tools.PrintResource(t, newRule)
 }
