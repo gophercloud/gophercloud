c89f9a57596a457bbbb82409e13b1e350715a1ca
Thomas Hartland <thomas.george.hartland@cern.ch>
2019-11-21 09:59:02 +0100

    Add containerinfra nodegroups Create acceptance test

--- acceptance/openstack/containerinfra/v1/nodegroups_test.go
+++ acceptance/openstack/containerinfra/v1/nodegroups_test.go
@@ -3,10 +3,13 @@
 package v1
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/acceptance/clients"
+	"github.com/gophercloud/gophercloud/acceptance/tools"
 	"github.com/gophercloud/gophercloud/openstack/containerinfra/v1/nodegroups"
 	th "github.com/gophercloud/gophercloud/testhelper"
 )
@@ -34,8 +37,23 @@ func TestNodeGroupsCRUD(t *testing.T) {
 	th.AssertNoErr(t, err)
 	defer DeleteCluster(t, client, clusterID)
 
+	var nodeGroupID string
+
 	t.Run("list", func(t *testing.T) { testNodeGroupsList(t, client, clusterID) })
 	t.Run("listone-get", func(t *testing.T) { testNodeGroupGet(t, client, clusterID) })
+	t.Run("create", func(t *testing.T) { nodeGroupID = testNodeGroupCreate(t, client, clusterID) })
+
+	t.Logf("Created nodegroup: %s", nodeGroupID)
+
+	// Wait for the node group to finish creating
+	err = tools.WaitForTimeout(func() (bool, error) {
+		ng, err := nodegroups.Get(client, clusterID, nodeGroupID).Extract()
+		if err != nil {
+			return false, fmt.Errorf("error waiting for node group to create: %v", err)
+		}
+		return (ng.Status == "CREATE_COMPLETE"), nil
+	}, 900*time.Second)
+	th.AssertNoErr(t, err)
 }
 
 func testNodeGroupsList(t *testing.T, client *gophercloud.ServiceClient, clusterID string) {
@@ -70,4 +88,17 @@ func testNodeGroupGet(t *testing.T, client *gophercloud.ServiceClient, clusterID
 	// Should have got the same node group as from the list
 	th.AssertEquals(t, ngID, ng.UUID)
 	th.AssertEquals(t, "worker", ng.Role)
+}
+
+func testNodeGroupCreate(t *testing.T, client *gophercloud.ServiceClient, clusterID string) string {
+	name := tools.RandomString("test-ng-", 8)
+	createOpts := nodegroups.CreateOpts{
+		Name: name,
+	}
+
+	ng, err := nodegroups.Create(client, clusterID, createOpts).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, name, ng.Name)
+
+	return ng.UUID
 }
