e9f7949ea2f8a464e8d9c842d50b81bd2b6b747c
Colin Fowler <elethiomel@gmail.com>
2019-11-26 12:39:16 +0000

    expand acceptance test for rule updates

--- acceptance/openstack/networking/v2/extensions/fwaas_v2/rule_test.go
+++ acceptance/openstack/networking/v2/extensions/fwaas_v2/rule_test.go
@@ -23,12 +23,16 @@ func TestRuleCRUD(t *testing.T) {
 	tools.PrintResource(t, rule)
 
 	ruleDescription := "Some rule description"
+	ruleProtocol := rules.ProtocolICMP
 	updateOpts := rules.UpdateOpts{
 		Description: &ruleDescription,
+		Protocol:    &ruleProtocol,
 	}
 
-	_, err = rules.Update(client, rule.ID, updateOpts).Extract()
+	ruleUpdated, err := rules.Update(client, rule.ID, updateOpts).Extract()
 	th.AssertNoErr(t, err)
+	th.AssertEquals(t, ruleUpdated.Description, ruleDescription)
+	th.AssertEquals(t, ruleUpdated.Protocol, string(ruleProtocol))
 
 	newRule, err := rules.Get(client, rule.ID).Extract()
 	th.AssertNoErr(t, err)
