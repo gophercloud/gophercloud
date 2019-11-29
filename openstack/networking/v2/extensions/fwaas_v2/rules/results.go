4654070080585e5358af3b00c8f07029821c0179
Colin Fowler <elethiomel@gmail.com>
2019-11-29 21:22:42 +0000

    [wip] FWaaS v2.0 Rule List (#1783)
    * FWaaSv2 Rule Create
    
    * minor formatting changes in imports to pass ./script/format
    
    * Add specific type for Action as per https://github.com/gophercloud/gophercloud/pull/1768/files/ad42c6277e24d42c4dacb5cb0ccf10181d2c8306#r346643142
    
    * rename PolicyID to FirewallPolicyID as per https://github.com/gophercloud/gophercloud/pull/1768/files/ad42c6277e24d42c4dacb5cb0ccf10181d2c8306#r346644820
    
    * removed Position as per https://github.com/gophercloud/gophercloud/pull/1768/files/ad42c6277e24d42c4dacb5cb0ccf10181d2c8306#r346646305
    
    * Add ProjectID to Rule
    
    * Initial acceptance test for creating rule
    
    * FWaaSv2 Rule Delete
    
    * Added Deletion of Rule to Rules acceptance test
    
    * convert Protocol to string in acceptance tests to prevent AssertEquals failures
    
    * minor refactor of TestRuleCD acceptance test
    
    * fix formatting with goimports
    
    * Rules Get
    
    * Rule Update
    
    * Use Protocol and Action in UpdateOpts struct
    
    * Use Protocol and Action types in TestUpdate unit test
    
    * expand acceptance test for rule updates
    
    * Rule List
    
    * Change Enabled and Shared in ListOpts to * bool as per https://github.com/gophercloud/gophercloud/pull/1783#pullrequestreview-324039113

--- openstack/networking/v2/extensions/fwaas_v2/rules/results.go
+++ openstack/networking/v2/extensions/fwaas_v2/rules/results.go
@@ -2,6 +2,7 @@ package rules
 
 import (
 	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/pagination"
 )
 
 // Rule represents a firewall rule
@@ -21,6 +22,43 @@ type Rule struct {
 	FirewallPolicyID     string `json:"firewall_policy_id"`
 	TenantID             string `json:"tenant_id"`
 	ProjectID            string `json:"project_id"`
+}
+
+// RulePage is the page returned by a pager when traversing over a
+// collection of firewall rules.
+type RulePage struct {
+	pagination.LinkedPageBase
+}
+
+// NextPageURL is invoked when a paginated collection of firewall rules has
+// reached the end of a page and the pager seeks to traverse over a new one.
+// In order to do this, it needs to construct the next page's URL.
+func (r RulePage) NextPageURL() (string, error) {
+	var s struct {
+		Links []gophercloud.Link `json:"firewall_rules_links"`
+	}
+	err := r.ExtractInto(&s)
+	if err != nil {
+		return "", err
+	}
+	return gophercloud.ExtractNextURL(s.Links)
+}
+
+// IsEmpty checks whether a RulePage struct is empty.
+func (r RulePage) IsEmpty() (bool, error) {
+	is, err := ExtractRules(r)
+	return len(is) == 0, err
+}
+
+// ExtractRules accepts a Page struct, specifically a RouterPage struct,
+// and extracts the elements into a slice of Router structs. In other words,
+// a generic collection is mapped into a relevant slice.
+func ExtractRules(r pagination.Page) ([]Rule, error) {
+	var s struct {
+		Rules []Rule `json:"firewall_rules"`
+	}
+	err := (r.(RulePage)).ExtractInto(&s)
+	return s.Rules, err
 }
 
 type commonResult struct {
