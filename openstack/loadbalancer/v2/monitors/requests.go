be3ccc5a0a6390e2470fc8ce5bf362776e1c332d
Andrei Ozerov <andrei.ozerov92@gmail.com>
2019-11-30 14:00:41 +0300

    LoadBalancer V2: add monitor List MaxRetriesDown
    Add loadbalancer/v2/monitors.ListOpts.MaxRetriesDown.
    
    Update tests and docs.

--- openstack/loadbalancer/v2/monitors/requests.go
+++ openstack/loadbalancer/v2/monitors/requests.go
@@ -19,24 +19,25 @@ type ListOptsBuilder interface {
 // sort by a particular Monitor attribute. SortDir sets the direction, and is
 // either `asc' or `desc'. Marker and Limit are used for pagination.
 type ListOpts struct {
-	ID            string `q:"id"`
-	Name          string `q:"name"`
-	TenantID      string `q:"tenant_id"`
-	ProjectID     string `q:"project_id"`
-	PoolID        string `q:"pool_id"`
-	Type          string `q:"type"`
-	Delay         int    `q:"delay"`
-	Timeout       int    `q:"timeout"`
-	MaxRetries    int    `q:"max_retries"`
-	HTTPMethod    string `q:"http_method"`
-	URLPath       string `q:"url_path"`
-	ExpectedCodes string `q:"expected_codes"`
-	AdminStateUp  *bool  `q:"admin_state_up"`
-	Status        string `q:"status"`
-	Limit         int    `q:"limit"`
-	Marker        string `q:"marker"`
-	SortKey       string `q:"sort_key"`
-	SortDir       string `q:"sort_dir"`
+	ID             string `q:"id"`
+	Name           string `q:"name"`
+	TenantID       string `q:"tenant_id"`
+	ProjectID      string `q:"project_id"`
+	PoolID         string `q:"pool_id"`
+	Type           string `q:"type"`
+	Delay          int    `q:"delay"`
+	Timeout        int    `q:"timeout"`
+	MaxRetries     int    `q:"max_retries"`
+	MaxRetriesDown int    `q:"max_retries_down"`
+	HTTPMethod     string `q:"http_method"`
+	URLPath        string `q:"url_path"`
+	ExpectedCodes  string `q:"expected_codes"`
+	AdminStateUp   *bool  `q:"admin_state_up"`
+	Status         string `q:"status"`
+	Limit          int    `q:"limit"`
+	Marker         string `q:"marker"`
+	SortKey        string `q:"sort_key"`
+	SortDir        string `q:"sort_dir"`
 }
 
 // ToMonitorListQuery formats a ListOpts into a query string.
