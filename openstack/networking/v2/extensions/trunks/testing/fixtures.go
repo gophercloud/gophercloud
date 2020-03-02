4cbd77a7a46459e4df58e2690ea47cbf8d1d72eb
Lars Lehtonen <lars.lehtonen@gmail.com>
2020-03-02 14:14:42 -0800

    openstack/networking/v2/extensions/trunks/testing: fix dropped error in ExpectedSubportsRemovedTrunk()

--- openstack/networking/v2/extensions/trunks/testing/fixtures.go
+++ openstack/networking/v2/extensions/trunks/testing/fixtures.go
@@ -372,6 +372,9 @@ func ExpectedSubportsAddedTrunk() (exp trunks.Trunk, err error) {
 
 func ExpectedSubportsRemovedTrunk() (exp trunks.Trunk, err error) {
 	trunkUpdatedAt, err := time.Parse(time.RFC3339, "2018-10-03T13:57:27Z")
+	if err != nil {
+		return
+	}
 	expectedTrunks, err := ExpectedTrunkSlice()
 	if err != nil {
 		return
