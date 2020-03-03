058158b02f57d8076aae06a8e740719bcd631b17
kayrus <kayrus@users.noreply.github.com>
2020-03-03 16:36:13 +0100

    Glance V2: Introduce an IDFromName helper in images package (#1879)

--- openstack/imageservice/v2/images/requests.go
+++ openstack/imageservice/v2/images/requests.go
@@ -378,3 +378,39 @@ func (r UpdateImageProperty) ToImagePatchMap() map[string]interface{} {
 
 	return updateMap
 }
+
+// IDFromName is a convienience function that returns an image's ID given its name.
+func IDFromName(client *gophercloud.ServiceClient, name string) (string, error) {
+	count := 0
+	id := ""
+
+	listOpts := ListOpts{
+		Name: name,
+	}
+
+	pages, err := List(client, listOpts).AllPages()
+	if err != nil {
+		return "", err
+	}
+
+	all, err := ExtractImages(pages)
+	if err != nil {
+		return "", err
+	}
+
+	for _, s := range all {
+		if s.Name == name {
+			count++
+			id = s.ID
+		}
+	}
+
+	switch count {
+	case 0:
+		return "", gophercloud.ErrResourceNotFound{Name: name, ResourceType: "image"}
+	case 1:
+		return id, nil
+	default:
+		return "", gophercloud.ErrMultipleResourcesFound{Name: name, Count: count, ResourceType: "image"}
+	}
+}
