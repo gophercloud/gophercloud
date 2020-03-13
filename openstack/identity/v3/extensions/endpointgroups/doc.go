8d66d48df80a822ee2979eac5076bdb38c8dc8aa
Pierre Gaxatte <pierre.gaxatte@corp.ovh.com>
2020-03-13 09:09:45 +0100

    Fix spacing

--- openstack/identity/v3/extensions/endpointgroups/doc.go
+++ openstack/identity/v3/extensions/endpointgroups/doc.go
@@ -4,10 +4,10 @@ and Endpoint associations.
 
 Example to Get an Endpoint Group
 
-    err := endpointgroups.Get(identityClient, endpointGroupID).ExtractErr()
-    if err != nil {
-    	panic(err)
-    }
+	err := endpointgroups.Get(identityClient, endpointGroupID).ExtractErr()
+	if err != nil {
+		panic(err)
+	}
 
 Example to List all Endpoint Groups by name
 
@@ -15,15 +15,15 @@ Example to List all Endpoint Groups by name
 		Name: "mygroup",
 	}
 
-    allPages, err := endpointgroups.List(identityClient, listOpts).AllPages()
-    if err != nil {
-    	panic(err)
-    }
+	allPages, err := endpointgroups.List(identityClient, listOpts).AllPages()
+	if err != nil {
+		panic(err)
+	}
 
 	allGroups, err := endpointgroups.ExtractEndpointGroups(allPages)
-    if err != nil {
-    	panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
 	for _, endpointgroup := range allGroups {
 		fmt.Printf("%+v\n", endpointgroup)
