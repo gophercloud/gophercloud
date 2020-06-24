95b3c5763eae81c6e73e5d312f43ce75ddf8f820
Iury Gregory Melo Ferreira <iurygregory@gmail.com>
2020-06-24 11:21:35 -0300

    Support http_basic auth_strategy for Ironic Inspector (#1986)
    Ironic Inspector introduced a new auth_type http_basic that can
    be used in standalone deployments. The code was introduced by [1]
    
    This commit adds the support to use the new authentication
    type.
    
    [1] https://review.opendev.org/#/c/729463/
    
    Co-authored-by: Iury <iury@r640-u09.dev2.kni.lab.eng.bos.redhat.com>

---
+++ openstack/baremetalintrospection/httpbasic/testing/requests_test.go
@@ -0,0 +1,34 @@
+package testing
+
+import (
+	"encoding/base64"
+	"testing"
+
+	"github.com/gophercloud/gophercloud/openstack/baremetalintrospection/httpbasic"
+	th "github.com/gophercloud/gophercloud/testhelper"
+)
+
+func TestNoAuth(t *testing.T) {
+	httpClient, err := httpbasic.NewBareMetalIntrospectionHTTPBasic(httpbasic.EndpointOpts{
+		IronicInspectorEndpoint:     "http://ironic:5050/v1",
+		IronicInspectorUser:         "myUser",
+		IronicInspectorUserPassword: "myPasswd",
+	})
+	th.AssertNoErr(t, err)
+	encToken := base64.StdEncoding.EncodeToString([]byte("myUser:myPasswd"))
+	headerValue := "Basic " + encToken
+	th.AssertEquals(t, headerValue, httpClient.MoreHeaders["Authorization"])
+
+	errTest1, err := httpbasic.NewBareMetalIntrospectionHTTPBasic(httpbasic.EndpointOpts{
+		IronicInspectorEndpoint: "http://ironic:5050/v1",
+	})
+	_ = errTest1
+	th.AssertEquals(t, "User and Password are required", err.Error())
+
+	errTest2, err := httpbasic.NewBareMetalIntrospectionHTTPBasic(httpbasic.EndpointOpts{
+		IronicInspectorUser:         "myUser",
+		IronicInspectorUserPassword: "myPasswd",
+	})
+	_ = errTest2
+	th.AssertEquals(t, "IronicInspectorEndpoint is required", err.Error())
+}
