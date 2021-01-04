649d6f7f5b65546e725daa8a8fe42b2d7de1c897
Duc Truong <dtruong@blizzard.com>
2021-01-04 15:45:21 -0800

    fix unit test

--- openstack/clustering/v1/clusters/testing/requests_test.go
+++ openstack/clustering/v1/clusters/testing/requests_test.go
@@ -444,12 +444,15 @@ func TestAddNodes(t *testing.T) {
 func TestRemoveNodes(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
+
 	HandleRemoveNodesSuccessfully(t)
+
 	opts := clusters.RemoveNodesOpts{
 		Nodes: []string{"node1", "node2", "node3"},
 	}
-	err := clusters.RemoveNodes(fake.ServiceClient(), "7d85f602-a948-4a30-afd4-e84f47471c15", opts).ExtractErr()
+	result, err := clusters.RemoveNodes(fake.ServiceClient(), "7d85f602-a948-4a30-afd4-e84f47471c15", opts).Extract()
 	th.AssertNoErr(t, err)
+	th.AssertEquals(t, result, "2a0ff107-e789-4660-a122-3816c43af703")
 }
 
 func TestReplaceNodes(t *testing.T) {
