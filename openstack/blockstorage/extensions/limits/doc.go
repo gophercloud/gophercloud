77a5e67cebe1cc3c0e1fdb3a02aa38c68a8afe7e
Joe Topjian <joe@topjian.net>
2021-01-04 18:21:16 -0700

    Update doc.go

--- openstack/blockstorage/extensions/limits/doc.go
+++ openstack/blockstorage/extensions/limits/doc.go
@@ -4,10 +4,10 @@ Package limits shows rate and limit information for a project you authorized for
 Example to Retrieve Limits
 
     limits, err := limits.Get(blockStorageClient).Extract()
-	if err != nil {
-		panic(err)
-	}
+    if err != nil {
+        panic(err)
+    }
 
-	fmt.Printf("%+v\n", limits)
+    fmt.Printf("%+v\n", limits)
 */
 package limits
