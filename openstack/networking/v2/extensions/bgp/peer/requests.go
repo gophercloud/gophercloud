79528c48bd4eaf25acea2e3b3c9a4032873fa138
Jonathan Huang <jonathan.huang@ubisoft.com>
2021-09-09 14:20:29 -0400

    Add Create/Delete BGP Peers

--- openstack/networking/v2/extensions/bgp/peer/requests.go
+++ openstack/networking/v2/extensions/bgp/peer/requests.go
@@ -19,3 +19,40 @@ func Get(c *gophercloud.ServiceClient, id string) (r GetResult) {
 	_, r.Header, r.Err = gophercloud.ParseResponse(resp, err)
 	return
 }
+
+// CreateOptsBuilder allows extensions to add additional parameters to the
+// Create request.
+type CreateOptsBuilder interface {
+	ToPeerCreateMap() (map[string]interface{}, error)
+}
+
+// CreateOpts represents options used to create a network.
+type CreateOpts struct {
+	AuthType string `json:"auth_type"`
+	RemoteAS int    `json:"remote_as"`
+	Name     string `json:"name"`
+	PeerIP   string `json:"peer_ip"`
+}
+
+// ToPeerCreateMap builds a request body from CreateOpts.
+func (opts CreateOpts) ToPeerCreateMap() (map[string]interface{}, error) {
+	return gophercloud.BuildRequestBody(opts, "bgp_peer")
+}
+
+func Create(c *gophercloud.ServiceClient, opts CreateOptsBuilder) (r CreateResult) {
+	b, err := opts.ToPeerCreateMap()
+	if err != nil {
+		r.Err = err
+		return
+	}
+	resp, err := c.Post(createURL(c), b, &r.Body, nil)
+	_, r.Header, r.Err = gophercloud.ParseResponse(resp, err)
+	return
+}
+
+// Delete accepts a unique ID and deletes the bgp Peer associated with it.
+func Delete(c *gophercloud.ServiceClient, PeerID string) (r DeleteResult) {
+	resp, err := c.Delete(deleteURL(c, PeerID), nil)
+	_, r.Header, r.Err = gophercloud.ParseResponse(resp, err)
+	return
+}
--- openstack/networking/v2/extensions/bgp/peer/urls.go
+++ openstack/networking/v2/extensions/bgp/peer/urls.go
@@ -3,14 +3,14 @@ package peer
 import "github.com/gophercloud/gophercloud"
 
 // https://docs.openstack.org/api-ref/network/v2/#list-bgp-peers
-const baseurl = "bgp-peers"
+const urlBase = "bgp-peers"
 
 func resourceURL(c *gophercloud.ServiceClient, id string) string {
-	return c.ServiceURL(baseurl, id)
+	return c.ServiceURL(urlBase, id)
 }
 
 func rootURL(c *gophercloud.ServiceClient) string {
-	return c.ServiceURL(baseurl)
+	return c.ServiceURL(urlBase)
 }
 
 func getURL(c *gophercloud.ServiceClient, id string) string {
@@ -19,4 +19,12 @@ func getURL(c *gophercloud.ServiceClient, id string) string {
 
 func listURL(c *gophercloud.ServiceClient) string {
 	return rootURL(c)
+}
+
+func createURL(c *gophercloud.ServiceClient) string {
+	return rootURL(c)
+}
+
+func deleteURL(c *gophercloud.ServiceClient, id string) string {
+	return c.ServiceURL(urlBase, id)
 }
