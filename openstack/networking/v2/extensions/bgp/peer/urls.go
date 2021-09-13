8234bd2d4b8557771851e4cd8e4cf1abd42b874e
Jonathan Huang <jonathan.huang@ubisoft.com>
2021-09-13 17:27:41 -0400

    Add comment for urls

--- openstack/networking/v2/extensions/bgp/peer/urls.go
+++ openstack/networking/v2/extensions/bgp/peer/urls.go
@@ -2,33 +2,39 @@ package peer
 
 import "github.com/gophercloud/gophercloud"
 
-// https://docs.openstack.org/api-ref/network/v2/#list-bgp-peers
 const urlBase = "bgp-peers"
 
+// return /v2.0/bgp-peers/{bgp-peer-id}
 func resourceURL(c *gophercloud.ServiceClient, id string) string {
 	return c.ServiceURL(urlBase, id)
 }
 
+// return /v2.0/bgp-peers
 func rootURL(c *gophercloud.ServiceClient) string {
 	return c.ServiceURL(urlBase)
 }
 
+// return /v2.0/bgp-peers/{bgp-peer-id}
 func getURL(c *gophercloud.ServiceClient, id string) string {
 	return resourceURL(c, id)
 }
 
+// return /v2.0/bgp-peers
 func listURL(c *gophercloud.ServiceClient) string {
 	return rootURL(c)
 }
 
+// return /v2.0/bgp-peers
 func createURL(c *gophercloud.ServiceClient) string {
 	return rootURL(c)
 }
 
+// return /v2.0/bgp-peers/{bgp-peer-id}
 func deleteURL(c *gophercloud.ServiceClient, id string) string {
 	return resourceURL(c, id)
 }
 
+// return /v2.0/bgp-peers/{bgp-peer-id}
 func updateURL(c *gophercloud.ServiceClient, id string) string {
 	return resourceURL(c, id)
 }
