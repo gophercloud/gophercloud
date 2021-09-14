0259a06f167a33a65a5904160c9bdaeb9c2e963b
Jonathan Huang <jonathan.huang@ubisoft.com>
2021-09-14 14:53:25 -0400

    Add TestDelete

--- openstack/networking/v2/extensions/bgp/speaker/testing/requests_test.go
+++ openstack/networking/v2/extensions/bgp/speaker/testing/requests_test.go
@@ -65,6 +65,21 @@ func TestUpdate(t *testing.T) {
 }
 
 func TestDelete(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	bgpSpeakerID := "ab01ade1-ae62-43c9-8a1f-3c24225b96d8"
+	th.Mux.HandleFunc("/v2.0/bgp-speakers/"+bgpSpeakerID, func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "DELETE")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		th.TestHeader(t, r, "Accept", "application/json")
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNoContent)
+	})
+
+	err := speaker.Delete(fake.ServiceClient(), bgpSpeakerID).ExtractErr()
+	th.AssertNoErr(t, err)
 }
 
 func TestAddBGPPeer(t *testing.T) {
