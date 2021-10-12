db8771c3d9b874f76d3bc7632db3108d9f5f9281
Jonathan <shhgs.efhilt@gmail.com>
2021-10-12 13:06:38 -0400

    Add ListBGPSpeakers (continued) (#2229)
    * Add ListBGPSpeakers
    
    * Update the code per Peer Review
    1. Change BGPSpeakerPage to SinglePageBase
    2. Change the return type of ListBGPSpeakers to ListBGPSpeakersResult
    3. Implement the Extract method of ListBGPSpeakersResult in agents
    4. misc: delete unused stuff for now and change the wording of doc.go
    
    * Rename bgp/speaker to bgp/speakers
    
    * Rename ListBGPSpeakerResult.Extract to ExtractBGPSpeakers
    
    * Change the type of ListBGPSpeakersResult to SinglePageBase
    
    * Fix doc
    
    * Delete an empty line for gofmt
    
    Co-authored-by: Jonathan Huang <jonathan.huang@ubisoft.com>

---
+++ openstack/networking/v2/extensions/bgp/speakers/results.go
@@ -0,0 +1,69 @@
+package speakers
+
+import (
+	// "encoding/json"
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/pagination"
+)
+
+type commonResult struct {
+	gophercloud.Result
+}
+
+// BGPSpeaker BGP Speaker
+type BGPSpeaker struct {
+	// UUID for the bgp speaker
+	ID string `json:"id"`
+
+	// Human-readable name for the bgp speaker. Might not be unique.
+	Name string `json:"name"`
+
+	// TenantID is the project owner of the bgp speaker.
+	TenantID string `json:"tenant_id"`
+
+	// ProjectID is the project owner of the bgp speaker.
+	ProjectID string `json:"project_id"`
+
+	// If the speaker would advertise floating ip host routes
+	AdvertiseFloatingIPHostRoutes bool `json:"advertise_floating_ip_host_routes"`
+
+	// If the speaker would advertise tenant networks
+	AdvertiseTenantNetworks bool `json:"advertise_tenant_networks"`
+
+	// IP version
+	IPVersion int `json:"ip_version"`
+
+	// Local Autonomous System
+	LocalAS int `json:"local_as"`
+
+	// The uuid of the Networks configured with this speaker
+	Networks []string `json:"networks"`
+
+	// The uuid of the BGP Peer Configured with this speaker
+	Peers []string `json:"peers"`
+}
+
+// BGPSpeakerPage is the page returned by a pager when traversing over a
+// collection of bgp speakers.
+type BGPSpeakerPage struct {
+	pagination.SinglePageBase
+}
+
+// IsEmpty checks whether a BGPSpeakerPage struct is empty.
+func (r BGPSpeakerPage) IsEmpty() (bool, error) {
+	is, err := ExtractBGPSpeakers(r)
+	return len(is) == 0, err
+}
+
+// ExtractBGPSpeakers accepts a Page struct, specifically a BGPSpeakerPage struct,
+// and extracts the elements into a slice of BGPSpeaker structs. In other words,
+// a generic collection is mapped into a relevant slice.
+func ExtractBGPSpeakers(r pagination.Page) ([]BGPSpeaker, error) {
+	var s []BGPSpeaker
+	err := ExtractBGPSpeakersInto(r, &s)
+	return s, err
+}
+
+func ExtractBGPSpeakersInto(r pagination.Page, v interface{}) error {
+	return r.(BGPSpeakerPage).Result.ExtractIntoSlicePtr(v, "bgp_speakers")
+}
