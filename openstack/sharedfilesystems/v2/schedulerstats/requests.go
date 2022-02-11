1edc73e7afbf9447cf6bbc0f9d48e67681619bca
Sandro Jäckel <sandro.jaeckel@sap.com>
2022-02-11 14:47:09 +0100

    sharedfilesystems: Add support for `/v2/scheduler-stats/pools{,/details}` api endpoints

---
+++ openstack/sharedfilesystems/v2/schedulerstats/requests.go
@@ -0,0 +1,92 @@
+package schedulerstats
+
+import (
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/pagination"
+)
+
+// ListOptsBuilder allows extensions to add additional parameters to the
+// List request.
+type ListOptsBuilder interface {
+	ToPoolsListQuery() (string, error)
+}
+
+// ListOpts controls the view of data returned (e.g globally or per project).
+type ListOpts struct {
+	// The pool name for the back end.
+	ProjectID string `json:"project_id,omitempty"`
+	// The pool name for the back end.
+	PoolName string `json:"pool_name"`
+	// The host name for the back end.
+	HostName string `json:"host_name"`
+	// The name of the back end.
+	BackendName string `json:"backend_name"`
+	// The capabilities for the storage back end.
+	Capabilities string `json:"capabilities"`
+	// The share type name or UUID. Allows filtering back end pools based on the extra-specs in the share type.
+	ShareType string `json:"share_type,omitempty"`
+}
+
+// ToPoolsListQuery formats a ListOpts into a query string.
+func (opts ListOpts) ToPoolsListQuery() (string, error) {
+	q, err := gophercloud.BuildQueryString(opts)
+	return q.String(), err
+}
+
+// List makes a request against the API to list pool information.
+func List(client *gophercloud.ServiceClient, opts ListOptsBuilder) pagination.Pager {
+	url := poolsListURL(client)
+	if opts != nil {
+		query, err := opts.ToPoolsListQuery()
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		url += query
+	}
+	return pagination.NewPager(client, url, func(r pagination.PageResult) pagination.Page {
+		return PoolPage{pagination.SinglePageBase(r)}
+	})
+}
+
+// ListDetailOptsBuilder allows extensions to add additional parameters to the
+// ListDetail request.
+type ListDetailOptsBuilder interface {
+	ToPoolsListQuery() (string, error)
+}
+
+// ListOpts controls the view of data returned (e.g globally or per project).
+type ListDetailOpts struct {
+	// The pool name for the back end.
+	ProjectID string `json:"project_id,omitempty"`
+	// The pool name for the back end.
+	PoolName string `json:"pool_name"`
+	// The host name for the back end.
+	HostName string `json:"host_name"`
+	// The name of the back end.
+	BackendName string `json:"backend_name"`
+	// The capabilities for the storage back end.
+	Capabilities string `json:"capabilities"`
+	// The share type name or UUID. Allows filtering back end pools based on the extra-specs in the share type.
+	ShareType string `json:"share_type,omitempty"`
+}
+
+// ToPoolsListQuery formats a ListDetailOpts into a query string.
+func (opts ListDetailOpts) ToPoolsListQuery() (string, error) {
+	q, err := gophercloud.BuildQueryString(opts)
+	return q.String(), err
+}
+
+// ListDetail makes a request against the API to list detailed pool information.
+func ListDetail(client *gophercloud.ServiceClient, opts ListDetailOptsBuilder) pagination.Pager {
+	url := poolsListDetailURL(client)
+	if opts != nil {
+		query, err := opts.ToPoolsListQuery()
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		url += query
+	}
+	return pagination.NewPager(client, url, func(r pagination.PageResult) pagination.Page {
+		return PoolPage{pagination.SinglePageBase(r)}
+	})
+}
---
+++ openstack/sharedfilesystems/v2/schedulerstats/results.go
@@ -0,0 +1,116 @@
+package schedulerstats
+
+import (
+	"encoding/json"
+	"math"
+
+	"github.com/gophercloud/gophercloud/pagination"
+)
+
+// Capabilities represents the information of an individual Pool.
+type Capabilities struct {
+	// The following fields should be present in all storage drivers.
+
+	// The quality of service (QoS) support.
+	Qos bool `json:"qos"`
+	// The date and time stamp when the API request was issued.
+	Timestamp string `json:"timestamp"`
+	// The name of the share back end.
+	ShareBackendName string `json:"share_backend_name"`
+	// Share server is usually a storage virtual machine or a lightweight container that is used to export shared file systems.
+	DriverHandlesShareServers bool `json:"driver_handles_share_servers"`
+	// The driver version of the back end.
+	DriverVersion string `json:"driver_version"`
+	// The amount of free capacity for the back end, in GiBs. A valid value is a string, such as unknown, or an integer.
+	FreeCapacityGB float64 `json:"-"`
+	// The storage protocol for the back end. For example, NFS_CIFS, glusterfs, HDFS, etc.
+	StorageProtocol string `json:"storage_protocol"`
+	// The total capacity for the back end, in GiBs. A valid value is a string, such as unknown, or an integer.
+	TotalCapacityGB float64 `json:"-"`
+	// The specification that filters back ends by whether they do or do not support share snapshots.
+	SnapshotSupport bool `json:"snapshot_support"`
+	// The back end replication domain.
+	ReplicationDomain string `json:"replication_domain"`
+	// The name of the vendor for the back end.
+	VendorName string `json:"vendor_name"`
+
+	// The following fields are optional and may have empty values depending
+
+	// on the storage driver in use.
+	ReservedPercentage  int64   `json:"reserved_percentage"`
+	AllocatedCapacityGB float64 `json:"-"`
+}
+
+// Pool represents an individual Pool retrieved from the
+// schedulerstats API.
+type Pool struct {
+	// The name of the back end.
+	Name string `json:"name"`
+	// The name of the back end.
+	Backend string `json:"backend"`
+	// The pool name for the back end.
+	Pool string `json:"pool"`
+	// The host name for the back end.
+	Host string `json:"host"`
+	// The back end capabilities which include qos, total_capacity_gb, etc.
+	Capabilities Capabilities `json:"capabilities,omitempty"`
+}
+
+func (r *Capabilities) UnmarshalJSON(b []byte) error {
+	type tmp Capabilities
+	var s struct {
+		tmp
+		AllocatedCapacityGB interface{} `json:"allocated_capacity_gb"`
+		FreeCapacityGB      interface{} `json:"free_capacity_gb"`
+		TotalCapacityGB     interface{} `json:"total_capacity_gb"`
+	}
+	err := json.Unmarshal(b, &s)
+	if err != nil {
+		return err
+	}
+	*r = Capabilities(s.tmp)
+
+	// Generic function to parse a capacity value which may be a numeric
+	// value, "unknown", or "infinite"
+	parseCapacity := func(capacity interface{}) float64 {
+		if capacity != nil {
+			switch capacity.(type) {
+			case float64:
+				return capacity.(float64)
+			case string:
+				if capacity.(string) == "infinite" {
+					return math.Inf(1)
+				}
+			}
+		}
+		return 0.0
+	}
+
+	r.AllocatedCapacityGB = parseCapacity(s.AllocatedCapacityGB)
+	r.FreeCapacityGB = parseCapacity(s.FreeCapacityGB)
+	r.TotalCapacityGB = parseCapacity(s.TotalCapacityGB)
+
+	return nil
+}
+
+// PoolPage is a single page of all List results.
+type PoolPage struct {
+	pagination.SinglePageBase
+}
+
+// IsEmpty satisfies the IsEmpty method of the Page interface. It returns true
+// if a List contains no results.
+func (page PoolPage) IsEmpty() (bool, error) {
+	va, err := ExtractPools(page)
+	return len(va) == 0, err
+}
+
+// ExtractPools takes a List result and extracts the collection of
+// Pools returned by the API.
+func ExtractPools(p pagination.Page) ([]Pool, error) {
+	var s struct {
+		Pools []Pool `json:"pools"`
+	}
+	err := (p.(PoolPage)).ExtractInto(&s)
+	return s.Pools, err
+}
