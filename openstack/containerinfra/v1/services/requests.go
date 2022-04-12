c3a433e90ef520aece2c7556ee631cfccbb9383c
Mohammed Naser <mnaser@vexxhost.com>
2022-04-12 17:06:41 -0400

    Update requests.go

--- openstack/containerinfra/v1/services/requests.go
+++ openstack/containerinfra/v1/services/requests.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gophercloud/gophercloud/pagination"
 )
 
+// List makes a request against the API to list services.
 func List(client *gophercloud.ServiceClient) pagination.Pager {
 	url := listURL(client)
 	return pagination.NewPager(client, url, func(r pagination.PageResult) pagination.Page {
