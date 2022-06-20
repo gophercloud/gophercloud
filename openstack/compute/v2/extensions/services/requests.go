11863bd438b6daf4926c26f46c49fbb469f4f9e3
吴典秋 <muti_kube@163.com>
2022-06-20 22:54:14 +0800

    fix service delete response ok code

--- openstack/compute/v2/extensions/services/requests.go
+++ openstack/compute/v2/extensions/services/requests.go
@@ -84,7 +84,7 @@ func Update(client *gophercloud.ServiceClient, id string, opts UpdateOpts) (r Up
 // Delete will delete the existing service with the provided ID.
 func Delete(client *gophercloud.ServiceClient, id string) (r DeleteResult) {
 	resp, err := client.Delete(updateURL(client, id), &gophercloud.RequestOpts{
-		OkCodes: []int{200},
+		OkCodes: []int{204},
 	})
 	_, r.Header, r.Err = gophercloud.ParseResponse(resp, err)
 	return
