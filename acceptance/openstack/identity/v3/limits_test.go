fe20aa56c44b3cf9115a9eeea7da8ba84eed1d07
emilmaruszczak <emil.maruszczak@thehutgroup.com>
2022-10-04 10:18:53 +0200

    Add acceptance test

--- acceptance/openstack/identity/v3/limits_test.go
+++ acceptance/openstack/identity/v3/limits_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gophercloud/gophercloud/acceptance/clients"
 	"github.com/gophercloud/gophercloud/acceptance/tools"
 	"github.com/gophercloud/gophercloud/openstack/identity/v3/limits"
+	"github.com/gophercloud/gophercloud/openstack/identity/v3/services"
 	th "github.com/gophercloud/gophercloud/testhelper"
 )
 
@@ -37,4 +38,45 @@ func TestLimitsList(t *testing.T) {
 
 	_, err = limits.ExtractLimits(allPages)
 	th.AssertNoErr(t, err)
+}
+
+func TestCreateLimits(t *testing.T) {
+	limitDescription := tools.RandomString("TESTLIMITS-DESC-", 8)
+	resourceLimit := tools.RandomInt(1, 100)
+	resourceName := "volume"
+
+	clients.RequireAdmin(t)
+
+	client, err := clients.NewIdentityV3Client()
+	th.AssertNoErr(t, err)
+
+	project, err := CreateProject(t, client, nil)
+	th.AssertNoErr(t, err)
+
+	createServiceOpts := &services.CreateOpts{
+		Type:  resourceName,
+		Extra: map[string]interface{}{},
+	}
+
+	service, err := CreateService(t, client, createServiceOpts)
+	th.AssertNoErr(t, err)
+
+	createOpts := limits.BatchCreateOpts{
+		limits.CreateOpts{
+			ServiceID:     service.ID,
+			ProjectID:     project.ID,
+			ResourceName:  resourceName,
+			ResourceLimit: resourceLimit,
+			Description:   limitDescription,
+		},
+	}
+
+	createdLimits, err := limits.BatchCreate(client, createOpts).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertIntGreaterOrEqual(t, 1, len(createdLimits))
+	th.AssertEquals(t, limitDescription, createdLimits[0].Description)
+	th.AssertEquals(t, resourceLimit, createdLimits[0].ResourceLimit)
+	th.AssertEquals(t, resourceName, createdLimits[0].ResourceName)
+	th.AssertEquals(t, service.ID, createdLimits[0].ServiceID)
+	th.AssertEquals(t, project.ID, createdLimits[0].ProjectID)
 }
