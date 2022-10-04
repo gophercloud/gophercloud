aeccb4544483607010c8f4448a504f2f48a68075
emilmaruszczak <emil.maruszczak@thehutgroup.com>
2022-10-04 18:14:27 +0200

    Add acceptance test

---
+++ acceptance/openstack/identity/v3/federation_test.go
@@ -0,0 +1,26 @@
+//go:build acceptance
+// +build acceptance
+
+package v3
+
+import (
+	"testing"
+
+	"github.com/gophercloud/gophercloud/acceptance/clients"
+	"github.com/gophercloud/gophercloud/acceptance/tools"
+	th "github.com/gophercloud/gophercloud/testhelper"
+	"github.com/gophercloud/gophercloud/openstack/identity/v3/extensions/federation"
+)
+
+func TestListMappings(t *testing.T) {
+	client, err := clients.NewIdentityV3Client()
+	th.AssertNoErr(t, err)
+
+  allPages, err := federation.ListMappings(client).AllPages()
+	th.AssertNoErr(t, err)
+
+  mappings, err := federation.ExtractMappings(allPages)
+	th.AssertNoErr(t, err)
+
+	tools.PrintResource(t, mappings)
+}
