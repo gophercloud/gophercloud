8d05a60616ba864ef80cdbf48f403e80d20840e1
Emil Maruszczak <emil.maruszczak@thehutgroup.com>
2023-03-01 21:27:26 +0100

    Check if the mapping was deleted

--- acceptance/openstack/identity/v3/federation_test.go
+++ acceptance/openstack/identity/v3/federation_test.go
@@ -6,6 +6,7 @@ package v3
 import (
 	"testing"
 
+	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/acceptance/clients"
 	"github.com/gophercloud/gophercloud/acceptance/tools"
 	"github.com/gophercloud/gophercloud/openstack/identity/v3/extensions/federation"
@@ -112,4 +113,9 @@ func TestMappingsCRUD(t *testing.T) {
 
 	err = federation.DeleteMapping(client, mappingName).ExtractErr()
 	th.AssertNoErr(t, err)
+
+	resp := federation.GetMapping(client, mappingName)
+	th.AssertErr(t, resp.Err)
+	_, ok := resp.Err.(gophercloud.ErrDefault404)
+	th.AssertEquals(t, true, ok)
 }
