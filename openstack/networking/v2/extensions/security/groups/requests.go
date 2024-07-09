08b233b8cf4628ed0aa703f45cdf26534cc3a62f
kayrus <kay.diam@gmail.com>
2024-07-09 21:05:40 +0200

    [neutron]: introduce Stateful argument for the security groups

--- openstack/networking/v2/extensions/security/groups/requests.go
+++ openstack/networking/v2/extensions/security/groups/requests.go
@@ -5,6 +5,12 @@ import (
 	"github.com/gophercloud/gophercloud/pagination"
 )
 
+// ListOptsBuilder allows extensions to add additional parameters to the
+// List request.
+type ListOptsBuilder interface {
+	ToSecGroupListQuery() (string, error)
+}
+
 // ListOpts allows the filtering and sorting of paginated collections through
 // the API. Filtering is achieved by passing in struct field values that map to
 // the group attributes you want to see returned. SortKey allows you to
@@ -14,6 +20,7 @@ type ListOpts struct {
 	ID          string `q:"id"`
 	Name        string `q:"name"`
 	Description string `q:"description"`
+	Stateful    *bool  `q:"stateful"`
 	TenantID    string `q:"tenant_id"`
 	ProjectID   string `q:"project_id"`
 	Limit       int    `q:"limit"`
@@ -26,16 +33,25 @@ type ListOpts struct {
 	NotTagsAny  string `q:"not-tags-any"`
 }
 
+// ToPortListQuery formats a ListOpts into a query string.
+func (opts ListOpts) ToSecGroupListQuery() (string, error) {
+	q, err := gophercloud.BuildQueryString(opts)
+	return q.String(), err
+}
+
 // List returns a Pager which allows you to iterate over a collection of
 // security groups. It accepts a ListOpts struct, which allows you to filter
 // and sort the returned collection for greater efficiency.
-func List(c *gophercloud.ServiceClient, opts ListOpts) pagination.Pager {
-	q, err := gophercloud.BuildQueryString(&opts)
-	if err != nil {
-		return pagination.Pager{Err: err}
+func List(c *gophercloud.ServiceClient, opts ListOptsBuilder) pagination.Pager {
+	url := rootURL(c)
+	if opts != nil {
+		query, err := opts.ToSecGroupListQuery()
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		url += query
 	}
-	u := rootURL(c) + q.String()
-	return pagination.NewPager(c, u, func(r pagination.PageResult) pagination.Page {
+	return pagination.NewPager(c, url, func(r pagination.PageResult) pagination.Page {
 		return SecGroupPage{pagination.LinkedPageBase{PageResult: r}}
 	})
 }
@@ -61,6 +77,9 @@ type CreateOpts struct {
 
 	// Describes the security group.
 	Description string `json:"description,omitempty"`
+
+	// Stateful indicates if the security group is stateful or stateless.
+	Stateful *bool `json:"stateful,omitempty"`
 }
 
 // ToSecGroupCreateMap builds a request body from CreateOpts.
@@ -95,6 +114,9 @@ type UpdateOpts struct {
 
 	// Describes the security group.
 	Description *string `json:"description,omitempty"`
+
+	// Stateful indicates if the security group is stateful or stateless.
+	Stateful *bool `json:"stateful,omitempty"`
 }
 
 // ToSecGroupUpdateMap builds a request body from UpdateOpts.
