13d2ca4b3977a7f4d95b33f996160f531af7b8d2
Stephen Finucane <stephenfin@redhat.com>
2024-07-16 13:34:29 +0100

    DNM: Add placement checks
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- internal/acceptance/openstack/compute/v2/volumeattach_test.go
+++ internal/acceptance/openstack/compute/v2/volumeattach_test.go
@@ -3,11 +3,13 @@
 package v2
 
 import (
+	"context"
 	"testing"
 
 	"github.com/gophercloud/gophercloud/v2/internal/acceptance/clients"
 	bs "github.com/gophercloud/gophercloud/v2/internal/acceptance/openstack/blockstorage/v2"
 	"github.com/gophercloud/gophercloud/v2/internal/acceptance/tools"
+	"github.com/gophercloud/gophercloud/v2/openstack/placement/v1/resourceproviders"
 	th "github.com/gophercloud/gophercloud/v2/testhelper"
 )
 
@@ -19,6 +21,34 @@ func TestVolumeAttachAttachment(t *testing.T) {
 
 	blockClient, err := clients.NewBlockStorageV3Client()
 	th.AssertNoErr(t, err)
+
+	// TEMP: Check inventory to figure out what's missing
+	placementClient, err := clients.NewPlacementV1Client()
+	th.AssertNoErr(t, err)
+	placementClient.Microversion = "1.37"
+
+	allPages, err := resourceproviders.List(placementClient, resourceproviders.ListOpts{}).AllPages(context.TODO())
+	th.AssertNoErr(t, err)
+
+	allResourceProviders, err := resourceproviders.ExtractResourceProviders(allPages)
+	th.AssertNoErr(t, err)
+
+	for _, r := range allResourceProviders {
+		inv, err := resourceproviders.GetInventories(context.TODO(), placementClient, r.UUID).Extract()
+		th.AssertNoErr(t, err)
+
+		for k, v := range inv.Inventories {
+			t.Logf("resource %s (%s) inventory: %s=%+v", r.UUID, r.Name, k, v)
+		}
+
+		use, err := resourceproviders.GetUsages(context.TODO(), placementClient, r.UUID).Extract()
+		th.AssertNoErr(t, err)
+
+		for k, v := range use.Usages {
+			t.Logf("resource %s (%s) usage: %s=%d", r.UUID, r.Name, k, v)
+		}
+	}
+	// TEMP: Check inventory to figure out what's missing
 
 	server, err := CreateServer(t, client)
 	th.AssertNoErr(t, err)
