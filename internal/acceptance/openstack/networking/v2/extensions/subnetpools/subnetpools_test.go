5449a0d40df03af459da15f43270d3fc2d1f7fd9
Martin André <m.andre@redhat.com>
2025-03-06 13:53:09 +0100

    Improve support for network `standard-attr-revisions` extension
    Consistently add support for the neutron `Resource revision numbers`
    extension [1] for more resources that support it. It adds
    a `RevisionNumber` field to all responses, and allows filtering via the
    `RevisionNumber` list option. It was only partially implemented.
    
    Also support the `If-Match` constraints, and add test for it.
    
    [1] https://docs.openstack.org/api-ref/network/v2/#revisions

--- internal/acceptance/openstack/networking/v2/extensions/subnetpools/subnetpools_test.go
+++ internal/acceptance/openstack/networking/v2/extensions/subnetpools/subnetpools_test.go
@@ -4,6 +4,7 @@ package v2
 
 import (
 	"context"
+	"strings"
 	"testing"
 
 	"github.com/gophercloud/gophercloud/v2/internal/acceptance/clients"
@@ -54,4 +55,64 @@ func TestSubnetPoolsCRUD(t *testing.T) {
 	}
 
 	th.AssertEquals(t, found, true)
+}
+
+func TestSubnetPoolsRevision(t *testing.T) {
+	client, err := clients.NewNetworkV2Client()
+	th.AssertNoErr(t, err)
+
+	// Create a subnetpool
+	subnetPool, err := CreateSubnetPool(t, client)
+	th.AssertNoErr(t, err)
+	defer DeleteSubnetPool(t, client, subnetPool.ID)
+
+	// Store the current revision number.
+	oldRevisionNumber := subnetPool.RevisionNumber
+
+	// Update the subnet pool without revision number.
+	// This should work.
+	newName := tools.RandomString("TESTACC-", 8)
+	newDescription := ""
+	updateOpts := &subnetpools.UpdateOpts{
+		Name:        newName,
+		Description: &newDescription,
+	}
+	subnetPool, err = subnetpools.Update(context.TODO(), client, subnetPool.ID, updateOpts).Extract()
+	th.AssertNoErr(t, err)
+
+	tools.PrintResource(t, subnetPool)
+
+	// This should fail due to an old revision number.
+	newDescription = "new description"
+	updateOpts = &subnetpools.UpdateOpts{
+		Name:           newName,
+		Description:    &newDescription,
+		RevisionNumber: &oldRevisionNumber,
+	}
+	_, err = subnetpools.Update(context.TODO(), client, subnetPool.ID, updateOpts).Extract()
+	th.AssertErr(t, err)
+	if !strings.Contains(err.Error(), "RevisionNumberConstraintFailed") {
+		t.Fatalf("expected to see an error of type RevisionNumberConstraintFailed, but got the following error instead: %v", err)
+	}
+
+	// Reread the subnet pool to show that it did not change.
+	subnetPool, err = subnetpools.Get(context.TODO(), client, subnetPool.ID).Extract()
+	th.AssertNoErr(t, err)
+
+	tools.PrintResource(t, subnetPool)
+
+	// This should work because now we do provide a valid revision number.
+	newDescription = "new description"
+	updateOpts = &subnetpools.UpdateOpts{
+		Name:           newName,
+		Description:    &newDescription,
+		RevisionNumber: &subnetPool.RevisionNumber,
+	}
+	subnetPool, err = subnetpools.Update(context.TODO(), client, subnetPool.ID, updateOpts).Extract()
+	th.AssertNoErr(t, err)
+
+	tools.PrintResource(t, subnetPool)
+
+	th.AssertEquals(t, subnetPool.Name, newName)
+	th.AssertEquals(t, subnetPool.Description, newDescription)
 }
--- openstack/networking/v2/extensions/qos/policies/requests.go
+++ openstack/networking/v2/extensions/qos/policies/requests.go
@@ -2,6 +2,7 @@ package policies
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/gophercloud/gophercloud/v2"
 	"github.com/gophercloud/gophercloud/v2/openstack/networking/v2/networks"
@@ -134,7 +135,6 @@ type ListOpts struct {
 	ProjectID      string `q:"project_id"`
 	Name           string `q:"name"`
 	Description    string `q:"description"`
-	RevisionNumber *int   `q:"revision_number"`
 	IsDefault      *bool  `q:"is_default"`
 	Shared         *bool  `q:"shared"`
 	Limit          int    `q:"limit"`
@@ -145,6 +145,7 @@ type ListOpts struct {
 	TagsAny        string `q:"tags-any"`
 	NotTags        string `q:"not-tags"`
 	NotTagsAny     string `q:"not-tags-any"`
+	RevisionNumber *int   `q:"revision_number"`
 }
 
 // ToPolicyListQuery formats a ListOpts into a query string.
@@ -243,6 +244,11 @@ type UpdateOpts struct {
 
 	// IsDefault indicates if this QoS policy is default policy or not.
 	IsDefault *bool `json:"is_default,omitempty"`
+
+	// RevisionNumber implements extension:standard-attr-revisions. If != "" it
+	// will set revision_number=%s. If the revision number does not match, the
+	// update will fail.
+	RevisionNumber *int `json:"-" h:"If-Match"`
 }
 
 // ToPolicyUpdateMap builds a request body from UpdateOpts.
@@ -258,8 +264,19 @@ func Update(ctx context.Context, c *gophercloud.ServiceClient, policyID string, 
 		r.Err = err
 		return
 	}
+	h, err := gophercloud.BuildHeaders(opts)
+	if err != nil {
+		r.Err = err
+		return
+	}
+	for k := range h {
+		if k == "If-Match" {
+			h[k] = fmt.Sprintf("revision_number=%s", h[k])
+		}
+	}
 	resp, err := c.Put(ctx, updateURL(c, policyID), b, &r.Body, &gophercloud.RequestOpts{
-		OkCodes: []int{200},
+		MoreHeaders: h,
+		OkCodes:     []int{200},
 	})
 	_, r.Header, r.Err = gophercloud.ParseResponse(resp, err)
 	return
