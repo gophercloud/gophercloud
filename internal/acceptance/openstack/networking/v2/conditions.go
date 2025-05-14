75cebf613f39e4a60a58dfa16fcdec22e471cb2a
Stephen Finucane <stephenfin@redhat.com>
2025-05-14 10:37:48 +0100

    tests: Prefer extension check over environment variable
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- internal/acceptance/openstack/networking/v2/conditions.go
+++ internal/acceptance/openstack/networking/v2/conditions.go
@@ -2,7 +2,6 @@ package v2
 
 import (
 	"context"
-	"os"
 	"testing"
 
 	"github.com/gophercloud/gophercloud/v2"
@@ -15,13 +14,5 @@ func RequireNeutronExtension(t *testing.T, client *gophercloud.ServiceClient, ex
 	_, err := extensions.Get(context.TODO(), client, extension).Extract()
 	if err != nil {
 		t.Skipf("this test requires %s Neutron extension", extension)
-	}
-}
-
-// RequirePortForwarding will restrict a test to only be run in environments
-// that support port forwarding
-func RequirePortForwarding(t *testing.T) {
-	if os.Getenv("OS_PORTFORWARDING_ENVIRONMENT") == "" {
-		t.Skip("this test requires support for port forwarding")
 	}
 }
