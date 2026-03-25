7850e65b94d1ed4c4a57313bf297d5ac5d5e56ef
Dominik Danelski <ddanelski@cloudferro.com>
2026-03-25 13:18:31 +0100

    Implement READ operations on Placement traits
    Related: #526
    
    API reference: https://docs.openstack.org/api-ref/placement/#traits
    Code reference:
    https://github.com/openstack/placement/blob/stable/2025.1/placement/handlers/trait.py#L108-L124
    https://github.com/openstack/placement/blob/stable/2025.1/placement/handlers/trait.py#L152-L178
    
    Signed-off-by: Dominik Danelski <ddanelski@cloudferro.com>

---
+++ openstack/placement/v1/traits/doc.go
@@ -0,0 +1,42 @@
+/*
+Package traits lists traits from the OpenStack Placement service.
+
+Traits API requests are available starting from microversion 1.6.
+
+Example to list traits
+
+	placementClient.Microversion = "1.6"
+
+	allPages, err := traits.List(placementClient, traits.ListOpts{}).AllPages(context.TODO())
+	if err != nil {
+		panic(err)
+	}
+
+	allTraits, err := traits.ExtractTraits(allPages)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, t := range allTraits {
+		fmt.Println(t)
+	}
+
+Example to check if a trait exists
+
+	placementClient.Microversion = "1.6"
+
+	traitName := "CUSTOM_HW_FPGA_CLASS1"
+	err := traits.Get(context.TODO(), placementClient, traitName).ExtractErr()
+	if err != nil {
+		if gophercloud.ResponseCodeIs(err, http.StatusNotFound) {
+			// 404 Not Found - The trait does not exist
+			fmt.Println("Trait does not exist.")
+		} else {
+			// Another error occurred
+			panic(err)
+		}
+	} else {
+		fmt.Println("Trait exists!")
+	}
+*/
+package traits
