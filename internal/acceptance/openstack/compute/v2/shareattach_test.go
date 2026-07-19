26e9d92d751313a3db384ac53c42473de53b8e0c
Ilan Zuckerman <izuckerm@redhat.com>
2026-07-19 13:03:41 +0300

    Adding list acceptance tet

--- internal/acceptance/openstack/compute/v2/shareattach_test.go
+++ internal/acceptance/openstack/compute/v2/shareattach_test.go
@@ -10,6 +10,7 @@ import (
 	sfs "github.com/gophercloud/gophercloud/v2/internal/acceptance/openstack/sharedfilesystems/v2"
 	"github.com/gophercloud/gophercloud/v2/internal/acceptance/tools"
 	"github.com/gophercloud/gophercloud/v2/openstack/compute/v2/servers"
+	"github.com/gophercloud/gophercloud/v2/openstack/compute/v2/shareattach"
 	th "github.com/gophercloud/gophercloud/v2/testhelper"
 )
 
@@ -46,4 +47,23 @@ func TestShareAttach(t *testing.T) {
 	tools.PrintResource(t, shareAttachment)
 
 	th.AssertEquals(t, share.ID, shareAttachment.ShareID)
+
+	allPages, err := shareattach.List(client, server.ID).AllPages(context.TODO())
+	th.AssertNoErr(t, err)
+
+	allAttachments, err := shareattach.ExtractShareAttachments(allPages)
+	th.AssertNoErr(t, err)
+
+	tools.PrintResource(t, allAttachments)
+
+	found := false
+	for _, sa := range allAttachments {
+		if sa.ShareID == share.ID {
+			found = true
+			th.AssertEquals(t, shareAttachment.Tag, sa.Tag)
+			th.AssertEquals(t, shareAttachment.Status, sa.Status)
+			break
+		}
+	}
+	th.AssertEquals(t, true, found)
 }
