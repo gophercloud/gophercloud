7629d9580f8c93bc0cb6c31f21ab1c3fd966058b
Adam Harwell <flux.adam@gmail.com>
2026-08-25 14:27:35 -0400

    test(config): verify custom client during v3 auth

--- openstack/config/provider_client_test.go
+++ openstack/config/provider_client_test.go
@@ -11,9 +11,21 @@ import (
 	th "github.com/gophercloud/gophercloud/v2/testhelper"
 )
 
+type roundTripperFunc func(*http.Request) (*http.Response, error)
+
+func (f roundTripperFunc) RoundTrip(r *http.Request) (*http.Response, error) {
+	return f(r)
+}
+
 func TestNewProviderClientV3(t *testing.T) {
 	fakeServer := th.SetupHTTP()
 	defer fakeServer.Teardown()
+
+	transportUsed := false
+	httpClient := http.Client{Transport: roundTripperFunc(func(r *http.Request) (*http.Response, error) {
+		transportUsed = true
+		return http.DefaultTransport.RoundTrip(r)
+	})}
 
 	fakeServer.Mux.HandleFunc("/v3/auth/tokens", func(w http.ResponseWriter, r *http.Request) {
 		th.TestMethod(t, r, http.MethodPost)
@@ -29,8 +41,12 @@ func TestNewProviderClientV3(t *testing.T) {
 			UserID:   "user-id",
 			Password: "password",
 		},
+		WithHTTPClient(httpClient),
 	)
 	th.AssertNoErr(t, err)
+	if !transportUsed {
+		t.Fatal("custom HTTP client was not used for authentication")
+	}
 	if client.Token() != "token" {
 		t.Fatalf("unexpected token: %q", client.Token())
 	}
