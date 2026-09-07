01b1680173963ca7c035395c09546ff1c998aa09
Christopher Barnes <christopher@barnes.biz>
2026-09-07 13:12:33 +0200

    reservation: add host CRUD acceptance test
    Signed-off-by: Christopher Barnes <christopher@barnes.biz>

--- internal/acceptance/openstack/reservation/v1/hosts_test.go
+++ internal/acceptance/openstack/reservation/v1/hosts_test.go
@@ -31,3 +31,76 @@ func TestHostsList(t *testing.T) {
 		tools.PrintResource(t, host.ExtraCapabilities)
 	}
 }
+
+func TestHostsCRUD(t *testing.T) {
+	clients.RequireAdmin(t)
+
+	client, err := clients.NewReservationV1Client()
+	th.AssertNoErr(t, err)
+
+	computeClient, err := clients.NewComputeV2Client()
+	th.AssertNoErr(t, err)
+
+	hypervisorHostname := HypervisorHostname(t, computeClient)
+
+	host, err := CreateHost(t, client, hypervisorHostname)
+	th.AssertNoErr(t, err)
+	defer DeleteHost(t, client, host)
+
+	tools.PrintResource(t, host)
+
+	newHost, err := hosts.Get(context.TODO(), client, host.ID).Extract()
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, host.ID, newHost.ID)
+	th.AssertEquals(t, "a100", newHost.ExtraCapabilities["gpu"])
+
+	allPages, err := hosts.List(client, hosts.ListOpts{}).AllPages(context.TODO())
+	th.AssertNoErr(t, err)
+
+	allHosts, err := hosts.ExtractHosts(allPages)
+	th.AssertNoErr(t, err)
+
+	var found bool
+	for _, h := range allHosts {
+		if h.ID == host.ID {
+			found = true
+		}
+	}
+	th.AssertEquals(t, true, found)
+
+	updateOpts := hosts.UpdateOpts{
+		ExtraCapabilities: map[string]any{"gpu": "h100"},
+	}
+
+	updatedHost, err := hosts.Update(context.TODO(), client, host.ID, updateOpts).Extract()
+	th.AssertNoErr(t, err)
+	tools.PrintResource(t, updatedHost)
+	th.AssertEquals(t, "h100", updatedHost.ExtraCapabilities["gpu"])
+}
+
+// Blazar removes an extra capability when it is updated to null. This requires
+// the 2026.2 release or later.
+func TestHostsRemoveCapability(t *testing.T) {
+	clients.RequireAdmin(t)
+
+	client, err := clients.NewReservationV1Client()
+	th.AssertNoErr(t, err)
+
+	computeClient, err := clients.NewComputeV2Client()
+	th.AssertNoErr(t, err)
+
+	host, err := CreateHost(t, client, HypervisorHostname(t, computeClient))
+	th.AssertNoErr(t, err)
+	defer DeleteHost(t, client, host)
+
+	updateOpts := hosts.UpdateOpts{
+		ExtraCapabilities: map[string]any{"gpu": nil},
+	}
+
+	updatedHost, err := hosts.Update(context.TODO(), client, host.ID, updateOpts).Extract()
+	th.AssertNoErr(t, err)
+	tools.PrintResource(t, updatedHost)
+
+	_, ok := updatedHost.ExtraCapabilities["gpu"]
+	th.AssertEquals(t, false, ok)
+}
