gophercloud/gophercloud

commit 0bffcd86ea35162ad6e94e3a0fc84a8d4cb364aa

baremetal: account for different types of CPU.Frequency
Ironic has two full-featured inspection implementations: via IPA
(in-band) and via Redfish. Unfortunately, the implementations disagreed
on the type of the Frequency field: IPA uses a string, while the Redfish
implementation returns a proper number.

We'll eventually clean it up on the Ironic side, but existing API
versions will have to live with this duality. This patch updates
Gophercloud to handle Frequency as a number of convert it to the current
string format, while also exposing the actual value.

Signed-off-by: Dmitry Tantsur <dtantsur@protonmail.com>

openstack/baremetal/inventory/types.go
@@ -1,16 +1,19 @@
 package inventory
 
+import "encoding/json"
+
 type BootInfoType struct {
 	CurrentBootMode string `json:"current_boot_mode"`
 	PXEInterface    string `json:"pxe_interface"`
 }
 
 type CPUType struct {
-	Architecture string   `json:"architecture"`
-	Count        int      `json:"count"`
-	Flags        []string `json:"flags"`
-	Frequency    string   `json:"frequency"`
-	ModelName    string   `json:"model_name"`
+	Architecture  string      `json:"architecture"`
+	Count         int         `json:"count"`
+	Flags         []string    `json:"flags"`
+	Frequency     string      `json:"-"`
+	ModelName     string      `json:"model_name"`
+	RealFrequency json.Number `json:"frequency"`
 }
 
 type InterfaceType struct {
@@ -68,4 +71,8 @@ type InventoryType struct {
 	Memory       MemoryType       `json:"memory"`
 	SystemVendor SystemVendorType `json:"system_vendor"`
 	Hostname     string           `json:"hostname"`
+}
+
+func (inv *InventoryType) Compat() {
+	inv.CPU.Frequency = string(inv.CPU.RealFrequency)
 }

openstack/baremetal/inventory/testing/types_test.go
@@ -16,6 +16,25 @@ func TestInventory(t *testing.T) {
 		t.Fatalf("Failed to unmarshal inventory: %s", err)
 	}
 
+	output.Compat()
+	th.CheckDeepEquals(t, Inventory, output)
+}
+
+func TestFrequencyAsNumber(t *testing.T) {
+	var output inventory.InventoryType
+	sample := strings.Replace(InventorySample,
+		`"frequency": "2100.084"`,
+		`"frequency": 2100.084`, 1)
+	if sample == InventorySample {
+		t.Fatal("TestFrequencyAsNumber needs updating")
+	}
+
+	err := json.Unmarshal([]byte(sample), &output)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal inventory: %s", err)
+	}
+
+	output.Compat()
 	th.CheckDeepEquals(t, Inventory, output)
 }
 

openstack/baremetal/inventory/testing/fixtures.go
@@ -293,10 +293,11 @@ var Inventory = inventory.InventoryType{
 		PXEInterface:    "52:54:00:4e:3d:30",
 	},
 	CPU: inventory.CPUType{
-		Count:        2,
-		Flags:        []string{"fpu", "mmx", "fxsr", "sse", "sse2"},
-		Frequency:    "2100.084",
-		Architecture: "x86_64",
+		Count:         2,
+		Flags:         []string{"fpu", "mmx", "fxsr", "sse", "sse2"},
+		Frequency:     "2100.084",
+		Architecture:  "x86_64",
+		RealFrequency: "2100.084",
 	},
 	Disks: []inventory.RootDiskType{
 		{

openstack/baremetal/v1/nodes/results.go
@@ -657,6 +657,7 @@ func (pd PluginData) AsStandardData() (result inventory.StandardPluginData, err 
 // AsInspectorData interprets plugin data as coming from ironic-inspector.
 func (pd PluginData) AsInspectorData() (result introspection.Data, err error) {
 	err = json.Unmarshal(pd.RawMessage, &result)
+	result.Inventory.Compat()
 	return
 }
 
@@ -705,6 +706,7 @@ type InventoryResult struct {
 func (r InventoryResult) Extract() (*InventoryData, error) {
 	var data InventoryData
 	err := r.ExtractInto(&data)
+	data.Inventory.Compat()
 	return &data, err
 }
 

openstack/baremetalintrospection/v1/introspection/results.go
@@ -193,6 +193,7 @@ type BaseInterfaceType struct {
 func (r DataResult) Extract() (*Data, error) {
 	var s Data
 	err := r.ExtractInto(&s)
+	s.Inventory.Compat()
 	return &s, err
 }
 

commit c78ad6f7dfdab90e453b8448db483f6e35f7d946

baremetal: also account for Frequency as an empty string
Regression in 0bffcd86ea35162ad6e94e3a0fc84a8d4cb364aa: frequency can
also be an empty string. Unmarshalling json.Number breaks that.
This change adds a custom unmarshall for CPU information.

This needs a clean-up in v3.

Signed-off-by: Dmitry Tantsur <dtantsur@protonmail.com>

openstack/baremetal/inventory/types.go
@@ -1,6 +1,10 @@
 package inventory
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"slices"
+)
 
 type BootInfoType struct {
 	CurrentBootMode string `json:"current_boot_mode"`
@@ -75,4 +79,37 @@ type InventoryType struct {
 
 func (inv *InventoryType) Compat() {
 	inv.CPU.Frequency = string(inv.CPU.RealFrequency)
+}
+
+var emptyFrequency = []string{``, `""`, `null`}
+
+func (t *CPUType) UnmarshalJSON(data []byte) error {
+	if len(data) == 0 || string(data) == "null" {
+		return nil
+	}
+
+	var cpuTypeCompat struct {
+		Architecture string          `json:"architecture"`
+		Count        int             `json:"count"`
+		Flags        []string        `json:"flags"`
+		Frequency    json.RawMessage `json:"frequency"`
+		ModelName    string          `json:"model_name"`
+	}
+	if err := json.Unmarshal(data, &cpuTypeCompat); err != nil {
+		return err
+	}
+
+	// This is the main point of this function: frequency can be an empty string
+	if !slices.Contains(emptyFrequency, string(cpuTypeCompat.Frequency)) {
+		if err := json.Unmarshal(cpuTypeCompat.Frequency, &t.RealFrequency); err != nil {
+			return fmt.Errorf("unable to unmarshal frequency: %w", err)
+		}
+		t.Frequency = string(t.RealFrequency)
+	}
+
+	t.Architecture = cpuTypeCompat.Architecture
+	t.Count = cpuTypeCompat.Count
+	t.Flags = cpuTypeCompat.Flags
+	t.ModelName = cpuTypeCompat.ModelName
+	return nil
 }

openstack/baremetal/inventory/testing/types_test.go
@@ -20,22 +20,53 @@ func TestInventory(t *testing.T) {
 	th.CheckDeepEquals(t, Inventory, output)
 }
 
-func TestFrequencyAsNumber(t *testing.T) {
-	var output inventory.InventoryType
-	sample := strings.Replace(InventorySample,
-		`"frequency": "2100.084"`,
-		`"frequency": 2100.084`, 1)
-	if sample == InventorySample {
-		t.Fatal("TestFrequencyAsNumber needs updating")
+func TestFrequency(t *testing.T) {
+	cases := []struct {
+		Value    string
+		Expected string
+	}{
+		{
+			Value:    `"frequency": "42"`,
+			Expected: "42",
+		},
+		{
+			Value:    `"missing frequency": 42`,
+			Expected: "",
+		},
+		{
+			Value:    `"frequency": 2100.084`,
+			Expected: "2100.084",
+		},
+		{
+			Value:    `"frequency": ""`,
+			Expected: "",
+		},
+		{
+			Value:    `"frequency": null`,
+			Expected: "",
+		},
 	}
 
-	err := json.Unmarshal([]byte(sample), &output)
-	if err != nil {
-		t.Fatalf("Failed to unmarshal inventory: %s", err)
-	}
+	for _, tc := range cases {
+		t.Run(tc.Value, func(t *testing.T) {
+			var output inventory.InventoryType
+			sample := strings.Replace(InventorySample,
+				`"frequency": "2100.084"`, tc.Value, 1)
+			if sample == InventorySample {
+				t.Fatal("TestFrequency needs updating")
+			}
 
-	output.Compat()
-	th.CheckDeepEquals(t, Inventory, output)
+			err := json.Unmarshal([]byte(sample), &output)
+			if err != nil {
+				t.Fatalf("Failed to unmarshal inventory: %s", err)
+			}
+
+			expected := Inventory
+			expected.CPU.Frequency = tc.Expected
+			expected.CPU.RealFrequency = json.Number(tc.Expected)
+			th.CheckDeepEquals(t, expected, output)
+		})
+	}
 }
 
 func TestLLDPTLVErrors(t *testing.T) {